e14e9c752ef40ae679b858a60c5b9a775759e96c
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-17 13:04:18 +0200

    display names of uploaded files

--- website/src/components/FileDragAndDrop.tsx
+++ website/src/components/FileDragAndDrop.tsx
@@ -3,10 +3,20 @@ import { FileUploader } from 'react-drag-drop-files';
 
 const fileTypes = ['JPG', 'PNG', 'GIF', 'GPX'];
 
+function FileDisplay(props: { file: File }) {
+    return <div>{props.file.name}</div>;
+}
+
 export function FileDragAndDrop() {
-    const [file, setFile] = useState(null);
-    const handleChange = (file) => {
-        setFile(file);
+    const [files, setFiles] = useState<File[]>([]);
+    const handleChange = (newFiles) => {
+        setFiles([...files, ...newFiles]);
     };
-    return <FileUploader handleChange={handleChange} name="file" types={fileTypes} />;
+    console.log(files);
+    return (
+        <div>
+            <FileUploader handleChange={handleChange} name="file" types={fileTypes} multiple={true} />
+            {files.length > 0 ? files.map((file) => <FileDisplay file={file} />) : <div>No file selected</div>}
+        </div>
+    );
 }
