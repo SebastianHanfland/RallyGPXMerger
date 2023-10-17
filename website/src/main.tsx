910c360175b518af80ed23ea988325dcc2b01145
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-17 14:20:25 +0200

    Introduce Bootstrap for easy styling

--- website/src/main.tsx
+++ website/src/main.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
