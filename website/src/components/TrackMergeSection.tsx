9d8cac36c5e024cde639a47da2f6018b14a6d2f7
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-17 17:17:45 +0200

    introduce separate package for the logic

--- website/src/components/TrackMergeSection.tsx
+++ website/src/components/TrackMergeSection.tsx
@@ -1,13 +1,16 @@
 import { DummyTable } from './DummyTable.tsx';
 import { Button } from 'react-bootstrap';
 import { FileDownloader } from './FileDownloader.tsx';
+import { calculateMerge } from '../logic/MergeCalculation.ts';
 
 export function TrackMergeSection() {
     return (
         <div className={'m-2 p-2 shadow'} style={{ height: '95%' }}>
             <h4>Restructure files</h4>
             <div style={{ height: '70px' }}>
-                <Button className={'m-2'}>Merge Tracks</Button>
+                <Button className={'m-2'} onClick={calculateMerge}>
+                    Merge Tracks
+                </Button>
                 <FileDownloader name={'test'} content={'1234'} />
                 <Button className={'m-2'}>Show Map</Button>
             </div>
