99112492c1bc09075bf16d616f914498e2b84b4a
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-17 14:15:37 +0200

    clean up template info

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,30 +1,15 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 import { FileDragAndDrop } from './components/FileDragAndDrop.tsx';
 
 export function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
+            <h1>Rally GPX Merger</h1>
             <div>
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo" />
-                </a>
+                <div>Input of Files</div>
+                <div>Restructure files</div>
+                <div>Download target files</div>
             </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
             <FileDragAndDrop />
         </>
     );
