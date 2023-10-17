cc99c6f14be07d3cbda2b98676ee80d5bd232ea6
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-17 13:08:21 +0200

    extract file content to be available in a string

--- website/src/components/FileDragAndDrop.tsx
+++ website/src/components/FileDragAndDrop.tsx
@@ -1,10 +1,22 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FileUploader } from 'react-drag-drop-files';
 
 const fileTypes = ['JPG', 'PNG', 'GIF', 'GPX'];
 
 function FileDisplay(props: { file: File }) {
-    return <div>{props.file.name}</div>;
+    const [fileContent, setFileContent] = useState<string>(null);
+    useEffect(() => {
+        props.file.arrayBuffer().then((content) => {
+            const textContent = new TextDecoder().decode(content);
+            setFileContent(textContent);
+        });
+    }, [props.file]);
+    return (
+        <div>
+            <div>{props.file.name}</div>
+            {fileContent && <div>{fileContent}</div>}
+        </div>
+    );
 }
 
 export function FileDragAndDrop() {
