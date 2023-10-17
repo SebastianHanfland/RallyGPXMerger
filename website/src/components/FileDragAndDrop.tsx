1ce18de8b3b1f64675a26f957b5b304f53901a34
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-17 15:42:54 +0200

    Add table for files and adjust height

--- website/src/components/FileDragAndDrop.tsx
+++ website/src/components/FileDragAndDrop.tsx
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
 import { FileUploader } from 'react-drag-drop-files';
-import { FileDownloader } from './FileDownloader.tsx';
+import { Form, Table } from 'react-bootstrap';
 import GpxParser from 'gpxparser';
 
 const fileTypes = ['JPG', 'PNG', 'GIF', 'GPX'];
 
 function FileDisplay(props: { file: File }) {
     const [fileContent, setFileContent] = useState<string | null>(null);
-    const [showContent, setShowContent] = useState(false);
     useEffect(() => {
         props.file.arrayBuffer().then((content) => {
             const textContent = new TextDecoder().decode(content);
@@ -19,15 +18,20 @@ function FileDisplay(props: { file: File }) {
         });
     }, [props.file]);
 
+    console.log(fileContent);
+
     return (
-        <div>
-            <div>{props.file.name}</div>
-            {fileContent && <FileDownloader content={fileContent} name={'newGpx'} />}
-            <button onClick={() => setShowContent(!showContent)}>
-                {showContent ? 'Hide content' : 'Show content'}
-            </button>
-            {showContent && fileContent && <div>{fileContent}</div>}
-        </div>
+        <tr>
+            <td>
+                <div>{props.file.name}</div>
+            </td>
+            <td>
+                <Form.Control type="text" placeholder="Track name" value={500} />
+            </td>
+            <td>
+                <Form.Control type="text" placeholder="Track name" value={700} />
+            </td>
+        </tr>
     );
 }
 
@@ -38,8 +42,27 @@ export function FileDragAndDrop() {
     };
     return (
         <div>
-            <FileUploader handleChange={handleChange} name="file" types={fileTypes} multiple={true} />
-            {files.length > 0 ? files.map((file) => <FileDisplay file={file} />) : <div>No file selected</div>}
+            <div style={{ height: '70px' }}>
+                <FileUploader handleChange={handleChange} name="file" types={fileTypes} multiple={true} />
+            </div>
+            {files.length > 0 ? (
+                <Table striped bordered hover style={{ width: '100%' }}>
+                    <thead>
+                        <tr>
+                            <th style={{ width: '33%' }}>File name</th>
+                            <th style={{ width: '33%' }}>People</th>
+                            <th style={{ width: '33%' }}>...</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {files.map((file) => (
+                            <FileDisplay file={file} />
+                        ))}
+                    </tbody>
+                </Table>
+            ) : (
+                <div>No file selected</div>
+            )}
         </div>
     );
 }
--- website/src/components/TrackMergeSection.tsx
+++ website/src/components/TrackMergeSection.tsx
@@ -1,9 +1,13 @@
 import { DummyTable } from './DummyTable.tsx';
+import { Button } from 'react-bootstrap';
 
 export function TrackMergeSection() {
     return (
         <div>
             <h4>Restructure files</h4>
+            <div style={{ height: '70px' }}>
+                <Button>Merge Tracks</Button>
+            </div>
             <DummyTable />
         </div>
     );
