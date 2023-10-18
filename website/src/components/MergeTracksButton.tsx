e15b6a19fda36b2746ebb7861d58229103d91ae7
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-18 11:23:20 +0200

    #6 Add a basic fake loading spinner to the button

--- website/src/components/MergeTracksButton.tsx
+++ website/src/components/MergeTracksButton.tsx
@@ -1,13 +1,35 @@
 import { AppDispatch } from '../store/store.ts';
 import { useDispatch } from 'react-redux';
-import { Button } from 'react-bootstrap';
+import { Button, Spinner } from 'react-bootstrap';
 import { calculateMerge } from '../logic/MergeCalculation.ts';
+import { useEffect, useState } from 'react';
 
 export function MergeTracksButton() {
     const dispatch: AppDispatch = useDispatch();
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        if (isLoading) {
+            setTimeout(() => setIsLoading(false), 2000);
+        }
+    }, [isLoading]);
+
     return (
-        <Button className={'m-2'} onClick={() => dispatch(calculateMerge)}>
-            Merge Tracks
+        <Button
+            className={'m-2'}
+            onClick={() => {
+                setIsLoading(true);
+                dispatch(calculateMerge);
+            }}
+            disabled={isLoading}
+        >
+            {isLoading ? (
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner>
+            ) : (
+                <div>Merge Tracks</div>
+            )}
         </Button>
     );
 }
