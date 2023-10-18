beeb982483e2bad5aa3755949102b2b9982f74e7
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-18 08:59:33 +0200

    #3 Introduce react select for interaction

--- website/src/components/MergeTableTrack.tsx
+++ website/src/components/MergeTableTrack.tsx
@@ -2,10 +2,17 @@ import { Button, Form } from 'react-bootstrap';
 import { TrackComposition } from '../store/types.ts';
 import { useDispatch } from 'react-redux';
 import { trackMergeActions } from '../store/trackMerge.reducer.ts';
+import Select from 'react-select';
 
 interface Props {
     track: TrackComposition;
 }
+
+const colourOptions = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+];
 
 export function MergeTableTrack({ track }: Props) {
     const { name, id } = track;
@@ -17,7 +24,15 @@ export function MergeTableTrack({ track }: Props) {
                 <Form.Control type="text" placeholder="Track name" value={name} />
             </td>
             <td>
-                <Form.Control type="text" placeholder="Track name" value={'A_11 + A_12 + 30min'} />
+                {/*<Form.Control type="text" placeholder="Track name" value={'A_11 + A_12 + 30min'} />*/}
+                <Select
+                    defaultValue={[colourOptions[2], colourOptions[3]]}
+                    isMulti
+                    name="colors"
+                    options={colourOptions}
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                />
             </td>
             <td>
                 <Button variant="danger" onClick={() => dispatch(trackMergeActions.removeTrackComposition(id))}>
