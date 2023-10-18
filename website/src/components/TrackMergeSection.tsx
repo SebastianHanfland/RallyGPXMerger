de2e7852e5fac4b630cc817aea47e12e7c68af6d
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-18 12:45:49 +0200

    #22 Introduce dummy for arrival date input

--- website/src/components/TrackMergeSection.tsx
+++ website/src/components/TrackMergeSection.tsx
@@ -1,5 +1,5 @@
 import { MergeTable } from './MergeTable.tsx';
-import { Button } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { CalculatedFilesDownloader } from './CalculatedFilesDownloader.tsx';
 import { MergeTracksButton } from './MergeTracksButton.tsx';
 import { RemoveDataButton } from './RemoveDataButton.tsx';
@@ -12,6 +12,14 @@ export function TrackMergeSection(props: Props) {
         <div className={'m-2 p-2 shadow'} style={{ height: '95%' }}>
             <h4>Restructure files</h4>
             <div style={{ height: '70px' }}>
+                <Button variant={'light'}>
+                    <Form.Control
+                        type="datetime"
+                        placeholder="Arrival Time and Date"
+                        style={{ width: '300px' }}
+                        value={'19.05.2024 14:00'}
+                    />
+                </Button>
                 <MergeTracksButton />
                 <CalculatedFilesDownloader />
                 <Button className={'m-2'} onClick={props.setShowMap}>
