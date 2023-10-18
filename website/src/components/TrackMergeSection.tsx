b31d6b27e9994730393e7a010cf938e0e3c22711
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-18 09:42:50 +0200

    #20 create a thunk as a basis to start with the logic

--- website/src/components/TrackMergeSection.tsx
+++ website/src/components/TrackMergeSection.tsx
@@ -2,13 +2,16 @@ import { MergeTable } from './MergeTable.tsx';
 import { Button } from 'react-bootstrap';
 import { FileDownloader } from './FileDownloader.tsx';
 import { calculateMerge } from '../logic/MergeCalculation.ts';
+import { useDispatch } from 'react-redux';
+import { AppDispatch } from '../store/store.ts';
 
 export function TrackMergeSection() {
+    const dispatch: AppDispatch = useDispatch();
     return (
         <div className={'m-2 p-2 shadow'} style={{ height: '95%' }}>
             <h4>Restructure files</h4>
             <div style={{ height: '70px' }}>
-                <Button className={'m-2'} onClick={calculateMerge}>
+                <Button className={'m-2'} onClick={() => dispatch(calculateMerge)}>
                     Merge Tracks
                 </Button>
                 <FileDownloader name={'test'} content={'1234'} />
--- website/src/logic/MergeCalculation.ts
+++ website/src/logic/MergeCalculation.ts
@@ -1,1 +1,11 @@
-export function calculateMerge() {}
+import { State } from '../store/types.ts';
+import { AppDispatch } from '../store/store.ts';
+import { getGpxSegments } from '../store/gpxSegments.reducer.ts';
+import { getTrackCompositions } from '../store/trackMerge.reducer.ts';
+
+export function calculateMerge(dispatch: AppDispatch, getState: () => State) {
+    const gpxSegments = getGpxSegments(getState());
+    const trackCompositions = getTrackCompositions(getState());
+
+    console.log({ gpxSegments, trackCompositions, dispatch });
+}
