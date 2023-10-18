77fdbaaf04cc046e3fa6a5646b7e6f0474079333
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-18 09:56:13 +0200

    #5 create dummy calculated tracks

--- website/src/logic/MergeCalculation.ts
+++ website/src/logic/MergeCalculation.ts
@@ -2,10 +2,19 @@ import { State } from '../store/types.ts';
 import { AppDispatch } from '../store/store.ts';
 import { getGpxSegments } from '../store/gpxSegments.reducer.ts';
 import { getTrackCompositions } from '../store/trackMerge.reducer.ts';
+import { calculatedTracksActions } from '../store/calculatedTracks.reducer.ts';
 
 export function calculateMerge(dispatch: AppDispatch, getState: () => State) {
     const gpxSegments = getGpxSegments(getState());
     const trackCompositions = getTrackCompositions(getState());
 
     console.log({ gpxSegments, trackCompositions, dispatch });
+
+    const createCalculatedTracks = trackCompositions.map((track) => ({
+        id: track.id,
+        filename: `${track.name}.gpx`,
+        content: '1234',
+    }));
+
+    dispatch(calculatedTracksActions.setCalculatedTracks(createCalculatedTracks));
 }
