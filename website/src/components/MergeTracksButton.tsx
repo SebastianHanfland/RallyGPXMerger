6fc5aa5b8c29d830c238c782a0a203f5efd99ec5
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-19 09:02:29 +0200

    #20 Make arrival date mandatory in the UI with disabling the button

--- website/src/components/MergeTracksButton.tsx
+++ website/src/components/MergeTracksButton.tsx
@@ -1,12 +1,14 @@
 import { AppDispatch } from '../store/store.ts';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button, Spinner } from 'react-bootstrap';
 import { calculateMerge } from '../logic/MergeCalculation.ts';
 import { useEffect, useState } from 'react';
+import { getArrivalDateTime } from '../store/trackMerge.reducer.ts';
 
 export function MergeTracksButton() {
     const dispatch: AppDispatch = useDispatch();
     const [isLoading, setIsLoading] = useState(false);
+    const arrivalDate = useSelector(getArrivalDateTime);
 
     useEffect(() => {
         if (isLoading) {
@@ -21,7 +23,7 @@ export function MergeTracksButton() {
                 setIsLoading(true);
                 dispatch(calculateMerge);
             }}
-            disabled={isLoading}
+            disabled={isLoading || !arrivalDate}
         >
             {isLoading ? (
                 <Spinner animation="border" role="status">
