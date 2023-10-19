8c6874d2175488cf6d1d8d789a323f7ece0caaf8
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-19 09:48:23 +0200

    #20 solve test and thus introduce a structure for the merge problem

--- website/src/logic/primitive/primitiveSolver.ts
+++ website/src/logic/primitive/primitiveSolver.ts
@@ -1,6 +1,19 @@
 import { GpxMergeLogic } from '../types.ts';
+import { mergeGpxs } from '../gpxMerger.ts';
 
 export const mergeTracks: GpxMergeLogic = (gpxSegments, trackCompositions, arrivalDateTime) => {
-    // console.log(arrivalDateTime, trackCompositions, gpxSegments);
-    return trackCompositions.map((track) => ({ id: track.id, content: '', filename: track.name! }));
+    return trackCompositions.map((track) => {
+        let trackContent: string;
+
+        track.segmentIds.forEach((segmentId) => {
+            const gpxSegment = gpxSegments.find((segment) => segment.id === segmentId);
+            if (!trackContent) {
+                trackContent = gpxSegment!.content;
+            } else {
+                trackContent = mergeGpxs(trackContent, gpxSegment!.content);
+            }
+        });
+
+        return { id: track.id, content: trackContent!, filename: track.name! };
+    });
 };
