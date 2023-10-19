5f6e92ee4f9398a88dbef28a83a872ed60e9da3c
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-19 11:55:44 +0200

    #23 copy paste primitive solver

---
+++ website/src/logic/withPeoples/withPeoplesSolver.ts
@@ -0,0 +1,28 @@
+import { GpxMergeLogic } from '../types.ts';
+import { letTimeInGpxEndAt } from '../gpxTimeShifter.ts';
+import { getStartTimeOfGpxTrack } from '../startTimeExtractor.ts';
+import { shiftEndDate } from '../shiftEndDate.ts';
+import { mergeGpxSegmentContents, resolveGpxSegments } from '../helper/solvingHelper.ts';
+
+export const mergeAndAdjustTimes: GpxMergeLogic = (gpxSegments, trackCompositions, arrivalDateTime) => {
+    return trackCompositions.map((track) => {
+        const gpxSegmentContents = resolveGpxSegments(track, gpxSegments);
+
+        let shiftedGpxContents: string[] = [];
+        let endDate = arrivalDateTime;
+
+        gpxSegmentContents.reverse().forEach((content) => {
+            if (typeof content === 'string') {
+                const shiftedContent = letTimeInGpxEndAt(content, endDate);
+                endDate = getStartTimeOfGpxTrack(shiftedContent);
+                shiftedGpxContents = [shiftedContent, ...shiftedGpxContents];
+            } else {
+                endDate = shiftEndDate(endDate, content.minutes);
+            }
+        });
+
+        const trackContent = mergeGpxSegmentContents(shiftedGpxContents);
+
+        return { id: track.id, content: trackContent, filename: track.name! };
+    });
+};
