8695ef96c4fb671a6cc0cd9cafc793e9ee4fad79
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-19 09:27:31 +0200

    #20 Solve test

--- website/src/logic/primitive/primitiveSolver.ts
+++ website/src/logic/primitive/primitiveSolver.ts
@@ -2,5 +2,5 @@ import { GpxMergeLogic } from '../types.ts';
 
 export const mergeTracks: GpxMergeLogic = (gpxSegments, trackCompositions, arrivalDateTime) => {
     console.log(arrivalDateTime, trackCompositions, gpxSegments);
-    return [];
+    return trackCompositions.map((track) => ({ id: track.id, content: '', filename: track.name! }));
 };
