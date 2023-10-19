5dda4b27b9ee812bd8d72bf312bfc49cc65e913a
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-19 09:43:14 +0200

    #20 create next test including mocking

--- website/src/logic/primitive/__tests__/primitiveSolver.test.ts
+++ website/src/logic/primitive/__tests__/primitiveSolver.test.ts
@@ -1,13 +1,17 @@
 import { mergeTracks } from '../primitiveSolver.ts';
 import { CalculatedTrack, GpxSegment, TrackComposition } from '../../../store/types.ts';
+import { mergeGpxs } from '../../gpxMerger.ts';
+import { Mock } from 'vitest';
+
+vi.mock('../../gpxMerger.ts');
 
 describe('primitiveSolver', () => {
     it('merge A1 and AB to A and B1 and AB to B', () => {
         // given
         const gpxSegments: GpxSegment[] = [
-            { id: '1', filename: 'A1', content: '', peopleCountStart: 1, peopleCountEnd: 1 },
-            { id: '2', filename: 'B1', content: '', peopleCountStart: 1, peopleCountEnd: 1 },
-            { id: '3', filename: 'AB', content: '', peopleCountStart: 2, peopleCountEnd: 2 },
+            { id: '1', filename: 'A1', content: 'cA1', peopleCountStart: 1, peopleCountEnd: 1 },
+            { id: '2', filename: 'B1', content: 'cB1', peopleCountStart: 1, peopleCountEnd: 1 },
+            { id: '3', filename: 'AB', content: 'cAB', peopleCountStart: 2, peopleCountEnd: 2 },
         ];
         const trackCompositions: TrackComposition[] = [
             { id: '1', name: 'A', segmentIds: ['1', '3'] },
@@ -16,9 +20,18 @@ describe('primitiveSolver', () => {
         const arrivalDateTime = '2023-10-17T22:00:00.000Z';
 
         const expectedCalculatedTracks: CalculatedTrack[] = [
-            { id: '1', filename: 'A', content: '' },
-            { id: '2', filename: 'B', content: '' },
+            { id: '1', filename: 'A', content: 'cA' },
+            { id: '2', filename: 'B', content: 'cB' },
         ];
+        (mergeGpxs as Mock).mockImplementation((a: string, b: string) => {
+            if (a === 'cA1' && b === 'cAB') {
+                return 'cA';
+            }
+            if (a === 'cB1' && b === 'cAB') {
+                return 'cB';
+            }
+            expect({ a, b }).toBeUndefined();
+        });
 
         // when
         const calculatedTracks = mergeTracks(gpxSegments, trackCompositions, arrivalDateTime);
--- website/src/logic/primitive/primitiveSolver.ts
+++ website/src/logic/primitive/primitiveSolver.ts
@@ -1,6 +1,6 @@
 import { GpxMergeLogic } from '../types.ts';
 
 export const mergeTracks: GpxMergeLogic = (gpxSegments, trackCompositions, arrivalDateTime) => {
-    console.log(arrivalDateTime, trackCompositions, gpxSegments);
+    // console.log(arrivalDateTime, trackCompositions, gpxSegments);
     return trackCompositions.map((track) => ({ id: track.id, content: '', filename: track.name! }));
 };
