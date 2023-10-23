0c567e4021cb6f30bb1561f53614dcad43aa782b
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-23 14:05:44 +0200

    #25 remove dummy function

--- website/src/logic/MergeCalculation.ts
+++ website/src/logic/MergeCalculation.ts
@@ -7,16 +7,7 @@ import { mergeAndAdjustTimes } from './primitive/primitiveSolver.ts';
 import { GpxMergeLogic } from './types.ts';
 import { mergeAndDelayAndAdjustTimes } from './withPeoples/withPeoplesSolver.ts';
 
-const dummy: GpxMergeLogic = (gpxSegments, trackCompositions, arrivalDateTime) => {
-    console.log(gpxSegments, arrivalDateTime);
-    return trackCompositions.map((track) => ({
-        id: track.id,
-        filename: `${track.name}.gpx`,
-        content: '1234',
-    }));
-};
-
-const solveFunction: GpxMergeLogic[] = [dummy, mergeAndAdjustTimes, mergeAndDelayAndAdjustTimes];
+const solveFunctions: GpxMergeLogic[] = [mergeAndAdjustTimes, mergeAndDelayAndAdjustTimes];
 
 export function calculateMerge(dispatch: AppDispatch, getState: () => State) {
     const gpxSegments = getGpxSegments(getState());
@@ -27,7 +18,7 @@ export function calculateMerge(dispatch: AppDispatch, getState: () => State) {
         return;
     }
 
-    const calculatedTracks = solveFunction[1](gpxSegments, trackCompositions, arrivalDateTime);
+    const calculatedTracks = solveFunctions[0](gpxSegments, trackCompositions, arrivalDateTime);
 
     dispatch(calculatedTracksActions.setCalculatedTracks(calculatedTracks));
 }
