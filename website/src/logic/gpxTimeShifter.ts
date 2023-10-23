c26c75e6e5c21958c33f1461f637b97bc8402db5
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-23 10:05:18 +0200

    #13 remove unused import

--- website/src/logic/gpxTimeShifter.ts
+++ website/src/logic/gpxTimeShifter.ts
@@ -1,12 +1,11 @@
-import date from 'date-and-time';
 /**
  * Assumptions:
  * - for trackpoints with timestamps: calculating "new" times is just a matter of adding or subtracting a delta
  * - delta is duration between desired start-time (likely end time) and recorded time of first trackpoint
  * - for trackpoints without timestamps: not implemented
  *
- * @param first the track to join the second to
- * @param second this gets joined at the end of the first
+ * @param parsedGpx the track to join the second to
+ * @param endTime this gets joined at the end of the first
  *
  */
 export function letTimeInGpxEndAt(parsedGpx: string, endTime: string): string {
