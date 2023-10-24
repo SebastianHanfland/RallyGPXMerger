244560ba262503f5a3ae295ee7ceed872a333072
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-24 15:33:31 +0200

    #18 connect source selection to redux

--- website/src/components/map/SourceSelection.tsx
+++ website/src/components/map/SourceSelection.tsx
@@ -1,12 +1,29 @@
 import { Form } from 'react-bootstrap';
-
+import { useDispatch, useSelector } from 'react-redux';
+import { getCurrenMapSource, mapActions } from '../../store/map.reducer.ts';
 export function SourceSelection() {
+    const mapSource = useSelector(getCurrenMapSource);
+    const dispatch = useDispatch();
     return (
         <Form.Group>
             <h5>Source selection</h5>
             <Form>
-                <Form.Check type={'radio'} className={'m-3'} label={'GPX Segments'}></Form.Check>
-                <Form.Check type={'radio'} className={'m-3'} label={'Calculated Tracks'}></Form.Check>
+                <Form.Check
+                    type={'radio'}
+                    id={'segments'}
+                    className={'m-3'}
+                    label={'GPX Segments'}
+                    checked={mapSource === 'segments'}
+                    onClick={() => dispatch(mapActions.setSource('segments'))}
+                ></Form.Check>
+                <Form.Check
+                    type={'radio'}
+                    id={'tracks'}
+                    className={'m-3'}
+                    label={'Calculated Tracks'}
+                    checked={mapSource === 'tracks'}
+                    onClick={() => dispatch(mapActions.setSource('tracks'))}
+                ></Form.Check>
             </Form>
         </Form.Group>
     );
