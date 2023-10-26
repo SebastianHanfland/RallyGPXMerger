39b064e53c571b12fef083f917f0fd2934082c9d
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-26 19:42:18 +0200

    #28 Add a confirmation modal to the remove a single gpx segment

--- website/src/components/RemoveFileButton.tsx
+++ website/src/components/RemoveFileButton.tsx
@@ -2,6 +2,8 @@ import { useDispatch } from 'react-redux';
 import { Button } from 'react-bootstrap';
 import { gpxSegmentsActions } from '../store/gpxSegments.reducer.ts';
 import { trackMergeActions } from '../store/trackMerge.reducer.ts';
+import { ConfirmationModal } from './ConfirmationModal.tsx';
+import { useState } from 'react';
 
 interface Props {
     id: string;
@@ -10,17 +12,29 @@ interface Props {
 
 export function RemoveFileButton({ id, name }: Props) {
     const dispatch = useDispatch();
+    const [showModal, setShowModal] = useState(false);
+    const removeGpxSegment = () => {
+        dispatch(gpxSegmentsActions.removeGpxSegment(id));
+        dispatch(trackMergeActions.removeGpxSegment(id));
+    };
     return (
-        <Button
-            variant="danger"
-            title={`Remove file "${name}" and all references`}
-            className={'m-1'}
-            onClick={() => {
-                dispatch(gpxSegmentsActions.removeGpxSegment(id));
-                dispatch(trackMergeActions.removeGpxSegment(id));
-            }}
-        >
-            x
-        </Button>
+        <>
+            <Button
+                variant="danger"
+                title={`Remove file "${name}" and all references`}
+                className={'m-1'}
+                onClick={() => setShowModal(true)}
+            >
+                x
+            </Button>
+            {showModal && (
+                <ConfirmationModal
+                    onConfirm={removeGpxSegment}
+                    closeModal={() => setShowModal(false)}
+                    title={'Removing Gpx Segment'}
+                    body={`Do you really want to remove the file "${name}" and all its references?`}
+                />
+            )}
+        </>
     );
 }
