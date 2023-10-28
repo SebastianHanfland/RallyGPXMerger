df2e57fb5f8ebefd0f167c7be3537e8c5f027f9e
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-28 18:35:00 +0200

    #30 Add opacity for snakes

--- website/src/components/map/trackMarkerDisplayHook.ts
+++ website/src/components/map/trackMarkerDisplayHook.ts
@@ -28,7 +28,7 @@ export function addTrackToMap(points: { lat: number; lng: number }[], trackId: s
         icon: bikeIcon,
         title: 'None',
     });
-    const trackSnake = L.polyline(points, { weight: 10, color: getColorFromUuid(trackId) });
+    const trackSnake = L.polyline(points, { weight: 10, color: getColorFromUuid(trackId), opacity: 0.6 });
     trackMarker.addTo(routeLayer);
     trackSnake.addTo(routeLayer);
 }
