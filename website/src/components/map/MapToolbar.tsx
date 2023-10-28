6af69c0c96a2eadb410cc0f5520caf7bc03785c5
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-28 19:02:02 +0200

    #30 add icons for the time slider

--- website/src/components/map/MapToolbar.tsx
+++ website/src/components/map/MapToolbar.tsx
@@ -5,8 +5,11 @@ export function MapToolbar() {
     return (
         <div className={'m-2 p-2 shadow'} style={{ height: '95%', overflow: 'auto' }}>
             <h4>Map settings</h4>
+            <hr />
             <TimeSlider />
+            <hr />
             <SourceSelection />
+            <hr />
         </div>
     );
 }
--- website/src/components/map/TimeSlider.tsx
+++ website/src/components/map/TimeSlider.tsx
@@ -5,6 +5,9 @@ import { MAX_SLIDER_TIME } from '../../store/types.ts';
 import { getCurrentTimeStamp } from './trackSimulationReader.ts';
 import { formatDate } from '../../logic/dateUtil.ts';
 import { useEffect, useState } from 'react';
+import play from '../../assets/play.svg';
+import fast from '../../assets/fast.svg';
+import stop from '../../assets/stop.svg';
 
 let interval: NodeJS.Timeout | undefined;
 
@@ -40,14 +43,29 @@ export function TimeSlider() {
                 value={mapTime}
                 onChange={(event) => dispatch(mapActions.setCurrentTime(Number(event.target.value)))}
             />
-            <Button variant={'success'} className={'m-1'} onClick={() => setPlaySpeed(1)}>
-                Play
+            <Button
+                variant={'success'}
+                className={'m-1'}
+                onClick={() => setPlaySpeed(1)}
+                title={'Play the rally simulation'}
+            >
+                <img src={play} className="m-1" alt="open file" />
             </Button>
-            <Button variant={'warning'} className={'m-1'} onClick={() => setPlaySpeed(5)}>
-                Fast
+            <Button
+                variant={'warning'}
+                className={'m-1'}
+                onClick={() => setPlaySpeed(5)}
+                title={'Play the rally simulation fast'}
+            >
+                <img src={fast} className="m-1" alt="open file" />
             </Button>
-            <Button variant={'danger'} className={'m-1'} onClick={() => setPlaySpeed(undefined)}>
-                Stop
+            <Button
+                variant={'danger'}
+                className={'m-1'}
+                onClick={() => setPlaySpeed(undefined)}
+                title={'Stop the rally simulation'}
+            >
+                <img src={stop} className="m-1" alt="open file" />
             </Button>
         </Form.Group>
     );
