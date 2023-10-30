bc017b475baa2832553d36b90e8f81ebacf9f33e
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-30 07:26:24 +0100

    #33 add tests for gpxSegments reducer

---
+++ website/src/store/__tests__/gpxSegment.reducer.test.ts
@@ -0,0 +1,53 @@
+import { createStore } from '../store.ts';
+import { getGpxSegments, gpxSegmentsActions } from '../gpxSegments.reducer.ts';
+import { GpxSegment } from '../types.ts';
+
+describe('Gpx segment reducer test', () => {
+    const dummyGpxSegment1: GpxSegment = { content: 'a', id: '1', filename: 'b' };
+    const dummyGpxSegment2: GpxSegment = { content: 'b', id: '2', filename: 'c' };
+
+    it('should set and remove gpx segments', () => {
+        // given
+        const store = createStore();
+        expect(getGpxSegments(store.getState())).toEqual([]);
+
+        // when & then
+        store.dispatch(gpxSegmentsActions.addGpxSegments([dummyGpxSegment1, dummyGpxSegment2]));
+        expect(getGpxSegments(store.getState())).toEqual([dummyGpxSegment1, dummyGpxSegment2]);
+
+        // when & then
+        store.dispatch(gpxSegmentsActions.removeGpxSegment(dummyGpxSegment1.id));
+        expect(getGpxSegments(store.getState())).toEqual([dummyGpxSegment2]);
+
+        // when & then
+        store.dispatch(gpxSegmentsActions.clearGpxSegments());
+        expect(getGpxSegments(store.getState())).toEqual([]);
+    });
+
+    it('should set people counter', () => {
+        // given
+        const store = createStore();
+        store.dispatch(gpxSegmentsActions.addGpxSegments([dummyGpxSegment1, dummyGpxSegment2]));
+
+        // when
+        store.dispatch(gpxSegmentsActions.setPeopleCountEnd({ id: '2', count: 500 }));
+
+        // then
+        expect(getGpxSegments(store.getState())).toEqual([
+            dummyGpxSegment1,
+            { ...dummyGpxSegment2, peopleCountEnd: 500 },
+        ]);
+    });
+
+    it('should change file content', () => {
+        // given
+        const store = createStore();
+        store.dispatch(gpxSegmentsActions.addGpxSegments([dummyGpxSegment1, dummyGpxSegment2]));
+
+        // when
+        store.dispatch(gpxSegmentsActions.changeGpxSegmentContent({ id: '2', newContent: 'newC' }));
+
+        // then
+        expect(getGpxSegments(store.getState())).toEqual([dummyGpxSegment1, { ...dummyGpxSegment2, content: 'newC' }]);
+    });
+});
