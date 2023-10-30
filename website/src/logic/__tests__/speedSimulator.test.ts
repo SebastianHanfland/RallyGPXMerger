d83f6c340b57742b0274f4fe6195dcfc51e40bb3
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-10-30 22:14:20 +0100

    #34 add helper function to calculate the speed ourselves Based on https://github.com/gpxstudio/gpxstudio.github.io/blob/master/js/trace.js#L2300-L2319

---
+++ website/src/logic/__tests__/speedSimulator.test.ts
@@ -0,0 +1,54 @@
+import { generateTimeData } from '../speedSimulator.ts';
+import { Point } from 'gpxparser';
+
+describe('speedSimulator', () => {
+    it('should map an empty point list into an empty point list', () => {
+        // when
+        const points = generateTimeData('2020-02-02T20:20:20.000Z', 12, []);
+
+        // then
+        expect(points).toEqual([]);
+    });
+
+    function stringifyDate(point: Point) {
+        return { ...point, time: point.time.toISOString() };
+    }
+
+    it('should set the first time stamp to the start value', () => {
+        // when
+        const points = generateTimeData('2020-02-02T20:20:20.000Z', 12, [{ lat: 1, lon: 1, ele: 1 }]);
+
+        // then
+        expect(points.map(stringifyDate)).toEqual([{ lat: 1, lon: 1, ele: 1, time: '2020-02-02T20:20:20.000Z' }]);
+    });
+
+    it('should set second timestamp', () => {
+        // when
+        const points = generateTimeData('2020-02-02T20:20:20.000Z', 12, [
+            { lat: 1, lon: 1, ele: 1 },
+            { lat: 1, lon: 1.0001, ele: 1 },
+        ]);
+
+        // then
+        expect(points.map(stringifyDate)).toEqual([
+            { lat: 1, lon: 1, ele: 1, time: '2020-02-02T20:20:20.000Z' },
+            { lat: 1, lon: 1.0001, ele: 1, time: '2020-02-02T20:20:23.335Z' },
+        ]);
+    });
+
+    it('should take elevation into account', () => {
+        // when
+        const points = generateTimeData('2020-02-02T20:20:20.000Z', 12, [
+            { lat: 1, lon: 1, ele: 1 },
+            { lat: 1, lon: 1.0001, ele: 3 },
+            { lat: 1, lon: 1.0002, ele: 5 },
+        ]);
+
+        // then
+        expect(points.map(stringifyDate)).toEqual([
+            { lat: 1, lon: 1, ele: 1, time: '2020-02-02T20:20:20.000Z' },
+            { lat: 1, lon: 1.0001, ele: 3, time: '2020-02-02T20:20:23.834Z' },
+            { lat: 1, lon: 1.0002, ele: 5, time: '2020-02-02T20:20:28.228Z' },
+        ]);
+    });
+});
---
+++ website/src/logic/speedSimulator.ts
@@ -0,0 +1,46 @@
+import geoDistance from 'geo-distance-helper';
+import { GeoPoint } from 'geo-distance-helper/dist/geoDistance';
+import { Point } from 'gpxparser';
+import date from 'date-and-time';
+
+function slopeFactor(slope: number): number {
+    const max_slope = 100;
+    slope = Math.max(-max_slope, Math.min(max_slope, slope));
+
+    if (slope < -30) {
+        return 1.5;
+    } else if (slope < 0) {
+        return 1 + ((2 * 0.7) / 13) * slope + (0.7 / Math.pow(13, 2)) * Math.pow(slope, 2);
+    } else if (slope <= 20) {
+        return 1 + Math.pow(slope / 7, 2);
+    } else {
+        return 10;
+    }
+}
+
+const toLatLng = ({ lat, lon }: Omit<Point, 'time'>): GeoPoint => ({ lat, lng: lon });
+
+export function generateTimeData(start: string, avg: number, points: Omit<Point, 'time'>[]): Point[] {
+    const alpha = 0.15;
+    let last_speed = avg;
+    let previousPoint: Point | undefined = undefined;
+    return points.map((point, index, points) => {
+        if (index === 0) {
+            previousPoint = { ...point, time: new Date(start) };
+            return { ...point, time: new Date(start) };
+        }
+        const a = previousPoint as Point;
+        const b = points[index];
+        const distInKm = geoDistance(toLatLng(b), toLatLng(a)) as number;
+
+        const slope = distInKm == 0 ? 0 : ((b.ele - a.ele) / (1000 * distInKm)) * 100;
+        const slope_factor = slopeFactor(slope);
+
+        const speed = alpha * (avg / slope_factor) + (1 - alpha) * last_speed;
+        const time = new Date(date.addSeconds(a.time, (60 * 60 * distInKm) / speed));
+        last_speed = speed;
+
+        previousPoint = { ...point, time };
+        return { ...point, time };
+    });
+}
