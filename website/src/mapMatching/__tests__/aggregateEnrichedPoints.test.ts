632d355abbcb457e3872e9c810b0e25293c193cd
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-11-07 09:59:33 +0100

    #42 add and fix tests

--- website/src/mapMatching/__tests__/aggregateEnrichedPoints.test.ts
+++ website/src/mapMatching/__tests__/aggregateEnrichedPoints.test.ts
@@ -20,14 +20,41 @@ describe('aggregateEnrichedPoints', () => {
             ];
 
             // when
-            const aggregatedPoints = aggregateEnrichedPoints(longList);
+            const aggregatedPoints = aggregateEnrichedPoints(longList, 0);
 
             // then
             expect(aggregatedPoints).toEqual([
                 {
                     streetName: 'Street A',
                     from: '2023-10-22T06:23:46Z',
-                    to: '2023-10-22T06:23:46Z',
+                    to: '2023-10-22T06:23:46.000Z',
+                    pointFrom: { lat: 1, lon: 2 },
+                    pointTo: { lat: 1, lon: 2 },
+                },
+            ]);
+        });
+
+        it('should take participants into account ', () => {
+            // given
+            const longList: EnrichedPoints[] = [
+                {
+                    street: 'Street A',
+                    time: '2023-10-22T06:23:46Z',
+                    lat: 1,
+                    lon: 2,
+                    ele: 3,
+                } as unknown as EnrichedPoints,
+            ];
+
+            // when
+            const aggregatedPoints = aggregateEnrichedPoints(longList, 100);
+
+            // then
+            expect(aggregatedPoints).toEqual([
+                {
+                    streetName: 'Street A',
+                    from: '2023-10-22T06:23:46Z',
+                    to: '2023-10-22T06:24:06.000Z',
                     pointFrom: { lat: 1, lon: 2 },
                     pointTo: { lat: 1, lon: 2 },
                 },
@@ -42,14 +69,14 @@ describe('aggregateEnrichedPoints', () => {
             ];
 
             // when
-            const aggregatedPoints = aggregateEnrichedPoints(longList);
+            const aggregatedPoints = aggregateEnrichedPoints(longList, 0);
 
             // then
             expect(aggregatedPoints).toEqual([
                 {
                     streetName: 'Street A',
                     from: '2023-10-22T06:23:46Z',
-                    to: '2023-10-22T06:26:46Z',
+                    to: '2023-10-22T06:26:46.000Z',
                     pointFrom: { lat: 1, lon: 2 },
                     pointTo: { lat: 4, lon: 5 },
                 },
@@ -71,35 +98,35 @@ describe('aggregateEnrichedPoints', () => {
             ];
 
             // when
-            const aggregatedPoints = aggregateEnrichedPoints(longList);
+            const aggregatedPoints = aggregateEnrichedPoints(longList, 0);
 
             // then
             expect(aggregatedPoints).toEqual([
                 {
                     streetName: 'Street A',
                     from: '2023-10-22T06:23:46Z',
-                    to: '2023-10-22T06:26:46Z',
+                    to: '2023-10-22T06:26:46.000Z',
                     pointFrom: { lat: 1, lon: 2 },
                     pointTo: { lat: 4, lon: 5 },
                 },
                 {
                     from: '2023-10-22T06:36:46Z',
                     streetName: 'Friedberger Straße, B 300, B 2',
-                    to: '2023-10-22T08:30:46Z',
+                    to: '2023-10-22T08:30:46.000Z',
                     pointFrom: { lat: 6, lon: 7 },
                     pointTo: { lat: 8, lon: 9 },
                 },
                 {
                     from: '2023-10-22T08:35:46Z',
                     streetName: 'Meringer Straße, B 2',
-                    to: '2023-10-22T08:40:46Z',
+                    to: '2023-10-22T08:40:46.000Z',
                     pointFrom: { lat: 10, lon: 11 },
                     pointTo: { lat: 12, lon: 13 },
                 },
                 {
                     from: '2023-10-22T08:46:46Z',
                     streetName: 'Street B',
-                    to: '2023-10-22T09:00:46Z',
+                    to: '2023-10-22T09:00:46.000Z',
                     pointFrom: { lat: 14, lon: 15 },
                     pointTo: { lat: 16, lon: 17 },
                 },
