f7870fc718cc3e1f60bc7741f59c594b3a275bf6
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-11-08 19:51:23 +0100

    #44 copy reducer to be able to not store the request counter in the local storage, so that a reload fixes the counters

---
+++ website/src/store/geoCodingRequests.reducer.ts
@@ -0,0 +1,100 @@
+import { createSlice, PayloadAction, Reducer } from '@reduxjs/toolkit';
+import { GeoCodingState, ResolvedPositions, ResolvedPostCodes, State } from './types';
+import { storage } from './storage.ts';
+import { TrackStreetInfo } from '../mapMatching/types.ts';
+
+const initialState: GeoCodingState = {
+    requestCounter: 0,
+    postCodeRequestCounter: 0,
+    requestDoneCounter: 0,
+    postCodeRequestDoneCounter: 0,
+    isLoadingData: false,
+};
+
+const geoCodingSlice = createSlice({
+    name: 'geoCoding',
+    initialState: storage.load()?.geoCoding ?? initialState,
+    reducers: {
+        setGeoApifyKey: (state: GeoCodingState, action: PayloadAction<string>) => {
+            state.geoApifyKey = action.payload;
+        },
+        setBigDataCloudKey: (state: GeoCodingState, action: PayloadAction<string>) => {
+            state.bigDataCloudKey = action.payload;
+        },
+        saveResolvedPositions: (state: GeoCodingState, action: PayloadAction<ResolvedPositions>) => {
+            if (!state.resolvedPositions) {
+                state.resolvedPositions = {};
+            }
+            const resolvedPositions = state.resolvedPositions;
+            Object.entries(action.payload).forEach(([key, value]) => {
+                if (resolvedPositions[key] === null) {
+                    resolvedPositions[key] = value;
+                }
+                if (resolvedPositions[key] === undefined) {
+                    resolvedPositions[key] = value;
+                }
+            });
+            state.resolvedPositions = resolvedPositions;
+        },
+        saveResolvedPostCodes: (state: GeoCodingState, action: PayloadAction<ResolvedPostCodes>) => {
+            if (!state.resolvedPostCodes) {
+                state.resolvedPostCodes = {};
+            }
+            const resolvedPostCodes = state.resolvedPostCodes;
+            Object.entries(action.payload).forEach(([key, value]) => {
+                if (resolvedPostCodes[key] === undefined) {
+                    resolvedPostCodes[key] = value;
+                }
+            });
+            state.resolvedPostCodes = resolvedPostCodes;
+        },
+        setTrackStreetInfos: (state: GeoCodingState, action: PayloadAction<TrackStreetInfo[]>) => {
+            state.trackStreetInfos = action.payload;
+        },
+        increaseActiveRequestCounter: (state: GeoCodingState) => {
+            state.requestCounter += 1;
+        },
+        decreaseActiveRequestCounter: (state: GeoCodingState) => {
+            state.requestCounter -= 1;
+        },
+        increaseRequestDoneCounter: (state: GeoCodingState) => {
+            state.requestDoneCounter += 1;
+        },
+        resetRequestDoneCounter: (state: GeoCodingState) => {
+            state.requestDoneCounter = 0;
+        },
+        setNumberOfRequiredRequests: (state: GeoCodingState, action: PayloadAction<number>) => {
+            state.numberOfRequiredRequests = action.payload;
+        },
+        increaseActivePostCodeRequestCounter: (state: GeoCodingState) => {
+            state.postCodeRequestCounter += 1;
+        },
+        decreaseActivePostCodeRequestCounter: (state: GeoCodingState) => {
+            state.postCodeRequestCounter -= 1;
+        },
+        increasePostCodeRequestDoneCounter: (state: GeoCodingState) => {
+            state.postCodeRequestDoneCounter += 1;
+        },
+        resetPostCodeRequestDoneCounter: (state: GeoCodingState) => {
+            state.postCodeRequestDoneCounter = 0;
+        },
+        setIsLoadingData: (state: GeoCodingState, action: PayloadAction<boolean>) => {
+            state.isLoadingData = action.payload;
+        },
+    },
+});
+
+export const geoCodingActions = geoCodingSlice.actions;
+export const geoCodingReducer: Reducer<GeoCodingState> = geoCodingSlice.reducer;
+const getBase = (state: State) => state.geoCoding;
+export const getGeoApifyKey = (state: State) => getBase(state).geoApifyKey;
+export const getBigDataCloudKey = (state: State) => getBase(state).bigDataCloudKey;
+export const getResolvedPositions = (state: State) => getBase(state).resolvedPositions ?? {};
+export const getResolvedPostCodes = (state: State) => getBase(state).resolvedPostCodes ?? {};
+export const getTrackStreetInfos = (state: State) => getBase(state).trackStreetInfos ?? [];
+export const getNumberOfRequiredRequests = (state: State) => getBase(state).numberOfRequiredRequests;
+export const getNumberOfRequestsDone = (state: State) => getBase(state).requestDoneCounter;
+export const getNumberOfPostCodeRequestsDone = (state: State) => getBase(state).postCodeRequestDoneCounter;
+export const getNumberOfRequestsRunning = (state: State) => getBase(state).requestCounter;
+export const getNumberOfPostCodeRequestsRunning = (state: State) => getBase(state).postCodeRequestCounter;
+export const getIsLoadingGeoData = (state: State) => getBase(state).isLoadingData;
