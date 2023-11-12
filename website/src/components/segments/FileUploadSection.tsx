d64c64397415b492b02f0064575498db7df26acb
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-11-12 14:26:43 +0100

    #45 Add hint to use brouter

--- website/src/components/segments/FileUploadSection.tsx
+++ website/src/components/segments/FileUploadSection.tsx
@@ -4,6 +4,12 @@ export function FileUploadSection() {
     return (
         <div className={'m-2 p-2 shadow'} style={{ height: '95%', overflow: 'auto' }}>
             <h4>Upload GPX Segments</h4>
+            <p>
+                You can create GPX segments with{' '}
+                <a href={'http://brouter.de/brouter-web'} target={'_blank'} referrerPolicy={'no-referrer'}>
+                    brouter
+                </a>
+            </p>
             <GpxSegments />
         </div>
     );
