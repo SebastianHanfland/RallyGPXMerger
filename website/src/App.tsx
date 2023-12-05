737549871c4581db8643be6f452d7238ea53d7e5
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-12-05 12:31:48 +0100

    #39 Introduce a router as base for iframing

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -7,15 +7,32 @@ import { store } from './store/store.ts';
 import { AppFooter } from './components/Footer.tsx';
 import { AppHeader } from './components/Header.tsx';
 import { ErrorBoundary } from './components/ErrorBoundary.tsx';
+import { BrowserRouter } from 'react-router-dom';
+import { Switch, Route } from 'react-router';
+
+function RallyPlaner() {
+    return (
+        <Provider store={store}>
+            <AppHeader />
+            <MergeAndMap />
+            <AppFooter />
+        </Provider>
+    );
+}
+
+function RallyVersionControl() {
+    return <div>Version Control</div>;
+}
 
 export function App() {
     return (
         <ErrorBoundary>
-            <Provider store={store}>
-                <AppHeader />
-                <MergeAndMap />
-                <AppFooter />
-            </Provider>
+            <BrowserRouter basename={'/RallyGPXMerger'}>
+                <Switch>
+                    <Route path={'/versions'} component={RallyVersionControl} />
+                    <Route path={'/*'} component={RallyPlaner} />
+                </Switch>
+            </BrowserRouter>
         </ErrorBoundary>
     );
 }
