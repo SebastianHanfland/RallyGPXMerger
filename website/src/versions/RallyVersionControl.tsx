d147dfda6a5d79da5f763f5738beb63912026c82
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-12-06 18:00:50 +0100

    #39 proof of concept for reading an uploaded zip file

--- website/src/versions/RallyVersionControl.tsx
+++ website/src/versions/RallyVersionControl.tsx
@@ -1,3 +1,21 @@
+import rally1 from '/rally1.zip?url';
+import { useEffect } from 'react';
+import JSZip from 'jszip';
+
 export function RallyVersionControl() {
-    return <div>Version Control</div>;
+    useEffect(() => {
+        const zip = new JSZip();
+        const file = zip.file(rally1);
+        console.log(file, rally1);
+        fetch(rally1)
+            .then((res) => res.blob())
+            .then((blob) => {
+                zip.loadAsync(blob).then((zipContent) => {
+                    Object.entries(zipContent.files).map(([filename, content]) => {
+                        content.async('text').then((text) => console.log(filename, text));
+                    });
+                });
+            });
+    }, []);
+    return null;
 }
