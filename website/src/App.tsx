044f22c8f567822759056c8074c72fe9228fec5c
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-12-08 09:12:17 +0100

    #39 do not use routing

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -2,20 +2,12 @@ import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import { ErrorBoundary } from './components/ErrorBoundary.tsx';
-import { BrowserRouter } from 'react-router-dom';
-import { Route, Switch } from 'react-router';
 import { RallyVersionControl } from './versions/RallyVersionControl.tsx';
 import { RallyPlaner } from './RallyPlaner.tsx';
 
 export function App() {
-    return (
-        <ErrorBoundary>
-            <BrowserRouter basename={'/RallyGPXMerger'}>
-                <Switch>
-                    <Route path={'/versions'} component={RallyVersionControl} />
-                    <Route path={'/*'} component={RallyPlaner} />
-                </Switch>
-            </BrowserRouter>
-        </ErrorBoundary>
-    );
+    const urlParams = window.location.search;
+    const hasExternalUrl = urlParams.includes('?url=');
+
+    return <ErrorBoundary>{hasExternalUrl ? <RallyVersionControl /> : <RallyPlaner />}</ErrorBoundary>;
 }
