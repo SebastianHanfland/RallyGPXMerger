d0c840ce10a691f315825be237e47b5711db4131
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-12-15 05:01:32 +0100

    #60 add tests to specify new behavior

--- website/src/logic/helper/__tests__/peopleDelayCounter.test.ts
+++ website/src/logic/helper/__tests__/peopleDelayCounter.test.ts
@@ -100,5 +100,36 @@ describe('peopleDelayCounter', () => {
                 expect(number).toEqual(0);
             });
         });
+
+        describe('Two branched meet and then another', () => {
+            const twoBranches = [
+                { id: '1', segmentIds: ['A1', 'AB', 'ABC'], peopleCount: 200 },
+                { id: '2', segmentIds: ['B1', 'AB', 'ABC'], peopleCount: 150 },
+                { id: '3', segmentIds: ['C1', 'C2', 'ABC'], peopleCount: 300 },
+            ];
+            it('the longest branch should reach first', () => {
+                // when
+                const number = sumUpAllPeopleWithHigherPriority2(twoBranches, '1');
+
+                // then
+                expect(number).toEqual(0);
+            });
+
+            it('earlier matched branches should stay together and are evaluated together', () => {
+                // when
+                const number = sumUpAllPeopleWithHigherPriority2(twoBranches, '2');
+
+                // then
+                expect(number).toEqual(200);
+            });
+
+            it('later added branches are compared against the combined strength', () => {
+                // when
+                const number = sumUpAllPeopleWithHigherPriority2(twoBranches, '3');
+
+                // then
+                expect(number).toEqual(350);
+            });
+        });
     });
 });
