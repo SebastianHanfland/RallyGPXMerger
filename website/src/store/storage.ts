875e6fdb672f3632ac8f9ba6000d2507e16398a6
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-12-15 16:42:17 +0100

    #67 make a more complicated storage...

--- website/src/store/storage.ts
+++ website/src/store/storage.ts
@@ -6,7 +6,12 @@ const stateKey = `gpxMerger.state`;
 
 const save = (value: State) => {
     try {
-        localStorage.setItem(stateKey, JSON.stringify(value));
+        const { gpxSegments, calculatedTracks, geoCoding, trackMerge, map } = value;
+        localStorage.setItem(stateKey + '.gpxSegments', JSON.stringify(gpxSegments));
+        localStorage.setItem(stateKey + '.map', JSON.stringify(map));
+        localStorage.setItem(stateKey + '.trackMerge', JSON.stringify(trackMerge));
+        localStorage.setItem(stateKey + '.geoCoding', JSON.stringify(geoCoding));
+        localStorage.setItem(stateKey + '.calculatedTracks', JSON.stringify(calculatedTracks));
     } catch (error) {
         console.log(error);
     }
@@ -14,9 +19,41 @@ const save = (value: State) => {
 
 const load = (): State | undefined => {
     try {
+        let gpxSegments = undefined;
+        let map = undefined;
+        let trackMerge = undefined;
+        let geoCoding = undefined;
+        let calculatedTracks = undefined;
+
+        const gpxSegmentsStringified = localStorage.getItem(stateKey + '.gpxSegments');
+        if (gpxSegmentsStringified) {
+            gpxSegments = JSON.parse(gpxSegmentsStringified);
+        }
+        const mapStringified = localStorage.getItem(stateKey + '.map');
+        if (mapStringified) {
+            map = JSON.parse(mapStringified);
+        }
+        const trackMergeStringified = localStorage.getItem(stateKey + '.trackMerge');
+        if (trackMergeStringified) {
+            trackMerge = JSON.parse(trackMergeStringified);
+        }
+        const geoCodingStringified = localStorage.getItem(stateKey + '.geoCoding');
+        if (geoCodingStringified) {
+            geoCoding = JSON.parse(geoCodingStringified);
+        }
+        const calculatedTracksStringified = localStorage.getItem(stateKey + '.calculatedTracks');
+        if (calculatedTracksStringified) {
+            calculatedTracks = JSON.parse(calculatedTracksStringified);
+        }
         const item = localStorage.getItem(stateKey);
         if (item) {
-            return JSON.parse(item);
+            return {
+                gpxSegments,
+                map,
+                trackMerge,
+                geoCoding,
+                calculatedTracks,
+            } as State;
         }
     } catch (error) {
         console.log(error);
