46e8ce58ff547447c7e890c7fb45b4241e53d4cb
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-12-19 10:41:34 +0100

    #71 introduce german header in blocked streets

--- website/src/components/streets/streetsCsvCreator.ts
+++ website/src/components/streets/streetsCsvCreator.ts
@@ -1,9 +1,24 @@
 import { BlockedStreetInfo } from '../../mapMatching/types.ts';
 import { formatTimeOnly } from '../../utils/dateUtil.ts';
+import { getLanguage } from '../../language.ts';
+
+const englishHeader = 'Post code;District;Street;Blocked from;Blocked until;';
+const germanHeader = 'PLZ;Bezirk;Straße;Blockiert von;Blockiert bis;';
+
+const getHeader = () => {
+    const language = getLanguage();
+    switch (language) {
+        case 'de':
+            return germanHeader;
+        case 'en':
+            return englishHeader;
+    }
+};
 
 export function convertStreetInfoToCsv(blockedStreets: BlockedStreetInfo[]): string {
     return (
-        'Post code;District;Street;Blocked from;Blocked until;\n' +
+        getHeader() +
+        '\n' +
         blockedStreets
             .map(
                 ({ postCode, district, streetName, frontArrival, backPassage }) =>
