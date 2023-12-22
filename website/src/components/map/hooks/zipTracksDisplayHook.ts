a39d4d20df7886adbcbc84e706b9ab4afac11bb3
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-12-22 11:33:18 +0100

    #53 Only display selected track

--- website/src/components/map/hooks/zipTracksDisplayHook.ts
+++ website/src/components/map/hooks/zipTracksDisplayHook.ts
@@ -3,22 +3,27 @@ import { MutableRefObject, useEffect } from 'react';
 import { LayerGroup } from 'leaflet';
 import { addTracksToLayer } from './addTrackToMap.ts';
 import { getShowCalculatedTracks, getShowMapMarker } from '../../../store/map.reducer.ts';
-import { getSelectedVersions, getZipTracks } from '../../../store/zipTracks.reducer.ts';
+import { getSelectedTracks, getSelectedVersions, getZipTracks } from '../../../store/zipTracks.reducer.ts';
 
 export function zipTracksDisplayHook(calculatedTracksLayer: MutableRefObject<LayerGroup | null>) {
     const zipTracks = useSelector(getZipTracks);
     const showTracks = useSelector(getShowCalculatedTracks);
     const showMarker = useSelector(getShowMapMarker);
     const selectedVersions = useSelector(getSelectedVersions);
+    const selectedTracks = useSelector(getSelectedTracks);
 
     // eslint-disable-next-line react-hooks/rules-of-hooks
     useEffect(() => {
         const tracks = selectedVersions.flatMap((version) => {
-            return zipTracks[version] ?? [];
+            const tracksOfVersion = zipTracks[version] ?? [];
+            if ((selectedTracks[version]?.length ?? 0) === 0) {
+                return tracksOfVersion;
+            }
+            return tracksOfVersion.filter((track) => selectedTracks[version]?.includes(track.id));
         });
         addTracksToLayer(calculatedTracksLayer, tracks, showTracks, {
             showMarker,
             opacity: 0.7,
         });
-    }, [zipTracks, zipTracks.length, selectedVersions, showMarker]);
+    }, [zipTracks, zipTracks.length, selectedTracks, selectedVersions, showMarker]);
 }
