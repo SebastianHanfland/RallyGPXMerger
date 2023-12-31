db9fe09bc830403aaa69d1db0dac5c851e352ab5
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2023-12-31 13:50:16 +0100

    Add version 3

--- website/src/versions/versionLinks.ts
+++ website/src/versions/versionLinks.ts
@@ -9,4 +9,9 @@ export const versions = [
         url: 'https://sebastianhanfland.github.io/RallyGPXMerger/variante2_001.zip',
         color: 'red',
     },
+    {
+        name: 'Variante 3',
+        url: 'https://sebastianhanfland.github.io/RallyGPXMerger/variante3_001.zip',
+        color: 'green',
+    },
 ];
