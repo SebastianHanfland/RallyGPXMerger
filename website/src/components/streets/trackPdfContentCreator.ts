e3aee56c6c7ed0033d60e830974ef44c7a5298b5
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-01-11 08:08:21 +0100

    #97 split code into separate functions

--- website/src/components/streets/trackPdfContentCreator.ts
+++ website/src/components/streets/trackPdfContentCreator.ts
@@ -3,7 +3,7 @@ import { formatTimeOnly, getTimeDifferenceInSeconds } from '../../utils/dateUtil
 import { getLanguage } from '../../language.ts';
 import geoDistance from 'geo-distance-helper';
 import { toLatLng } from '../../logic/speedSimulator.ts';
-import { streetInfoHeaderLength } from './StreetFilesjsPdfDownloader.tsx';
+import { ContentTable } from 'pdfmake/interfaces';
 
 export function formatNumber(numberToFormat: number, maximumFractionDigits = 2) {
     const language = getLanguage();
@@ -66,40 +66,39 @@ function getAdditionalInfo(
 }
 
 export function convertTrackInfoToPdfContent(track: TrackStreetInfo): string {
-    return (
-        getHeader(track) +
-        track.wayPoints
-            .map(
-                ({
-                    streetName,
-                    postCode,
-                    district,
-                    frontArrival,
-                    frontPassage,
-                    backArrival,
-                    pointFrom,
-                    pointTo,
-                    type,
-                    nodeTracks,
-                    breakLength,
-                }) =>
-                    `${streetName}${getAdditionalInfo(type, nodeTracks, breakLength)};` +
-                    `${postCode ?? ''};` +
-                    `${district ?? ''};` +
-                    `${formatNumber(geoDistance(toLatLng(pointFrom), toLatLng(pointTo)) as number, 2)};` +
-                    `${formatNumber(getTimeDifferenceInSeconds(frontPassage, frontArrival) / 60, 1)};` +
-                    `${formatNumber(getTimeDifferenceInSeconds(backArrival, frontArrival) / 60, 1)};` +
-                    `${formatTimeOnly(frontArrival)};` +
-                    `${formatTimeOnly(frontPassage)};` +
-                    `${formatTimeOnly(backArrival)}`
-            )
-            .join('\n')
-    );
+    return track.wayPoints
+        .map(
+            ({
+                streetName,
+                postCode,
+                district,
+                frontArrival,
+                frontPassage,
+                backArrival,
+                pointFrom,
+                pointTo,
+                type,
+                nodeTracks,
+                breakLength,
+            }) =>
+                `${streetName}${getAdditionalInfo(type, nodeTracks, breakLength)};` +
+                `${postCode ?? ''};` +
+                `${district ?? ''};` +
+                `${formatNumber(geoDistance(toLatLng(pointFrom), toLatLng(pointTo)) as number, 2)};` +
+                `${formatNumber(getTimeDifferenceInSeconds(frontPassage, frontArrival) / 60, 1)};` +
+                `${formatNumber(getTimeDifferenceInSeconds(backArrival, frontArrival) / 60, 1)};` +
+                `${formatTimeOnly(frontArrival)};` +
+                `${formatTimeOnly(frontPassage)};` +
+                `${formatTimeOnly(backArrival)}`
+        )
+        .join('\n');
 }
 
-export function createStreetTable(trackStreets: TrackStreetInfo) {
-    const trackInfo = convertTrackInfoToPdfContent(trackStreets).replaceAll('Wahlkreis', '').split('\n');
-    const body: string[][] = trackInfo.slice(streetInfoHeaderLength).map((row) => row.split(';'));
+export function createStreetTable(trackStreets: TrackStreetInfo): ContentTable {
+    const streetInfo = convertTrackInfoToPdfContent(trackStreets)
+        .replaceAll('Wahlkreis', '')
+        .split('\n')
+        .map((row) => row.split(';'));
     return {
         layout: 'lightHorizontalLines', // optional
         table: {
@@ -107,20 +106,22 @@ export function createStreetTable(trackStreets: TrackStreetInfo) {
             // you can declare how many rows should be treated as headers
             headerRows: 1,
             widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto'],
-            body: body,
+            body: streetInfo,
         },
     };
 }
 
-export function createInfoTable(trackStreets: TrackStreetInfo) {
-    const trackInfo = convertTrackInfoToPdfContent(trackStreets).replaceAll('Wahlkreis', '').split('\n');
-    const infoBody: string[][] = trackInfo.slice(0, streetInfoHeaderLength).map((row) => row.split(';'));
+export function createInfoTable(trackStreets: TrackStreetInfo): ContentTable {
+    const trackInfo = getHeader(trackStreets)
+        .replaceAll('Wahlkreis', '')
+        .split('\n')
+        .map((row) => row.split(';'));
     return {
         layout: 'lightHorizontalLines', // optional
         table: {
             widths: ['auto', 'auto'],
 
-            body: infoBody,
+            body: trackInfo,
         },
     };
 }
