babc0b64bae18651ce95585336f73e5aadef65f0
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-01-11 08:12:32 +0100

    #97 reuse code from csv creator

--- website/src/components/streets/trackPdfContentCreator.ts
+++ website/src/components/streets/trackPdfContentCreator.ts
@@ -1,55 +1,10 @@
 import { TrackStreetInfo, TrackWayPointType } from '../../mapMatching/types.ts';
 import { formatTimeOnly, getTimeDifferenceInSeconds } from '../../utils/dateUtil.ts';
-import { getLanguage } from '../../language.ts';
 import geoDistance from 'geo-distance-helper';
 import { toLatLng } from '../../logic/speedSimulator.ts';
 import { ContentTable } from 'pdfmake/interfaces';
-
-export function formatNumber(numberToFormat: number, maximumFractionDigits = 2) {
-    const language = getLanguage();
-    return Intl.NumberFormat(language, { maximumFractionDigits: maximumFractionDigits }).format(numberToFormat);
-}
-
-const germanHeader = (trackInfo: TrackStreetInfo): string => {
-    const duration = getTimeDifferenceInSeconds(trackInfo.arrivalBack, trackInfo.startFront) / 60;
-    const durationString = `Dauer in min;${formatNumber(duration)}\n`;
-    const distance = `Strecke in km;${formatNumber(trackInfo.distanceInKm)}\n`;
-    const averageSpeed = `Durchschnittsgeschwindigkeit in km/h;${formatNumber(
-        (trackInfo.distanceInKm / duration) * 60
-    )}\n`;
-
-    const times = `Start;${formatTimeOnly(trackInfo.startFront)}\nAnkunft der Ersten;${formatTimeOnly(
-        trackInfo.arrivalFront
-    )}\nAnkunft der Letzten;${formatTimeOnly(trackInfo.arrivalBack)}\n`;
-    const peopleCount = `Geschätzte TeilnehmerInnen:;${trackInfo.peopleCount ?? ''}\n`;
-    const tableHeaders = `Straße;PLZ;Bezirk;Länge in km;Dauer in min;Blockiert in min;Ankunft des Zugs auf der Straße;Ankunft des Zugs am Ende;Straße blockiert bis\n`;
-
-    return `${times}${durationString}${distance}${averageSpeed}${peopleCount}${tableHeaders}`;
-};
-const englishHeader = (trackInfo: TrackStreetInfo): string => {
-    const duration = getTimeDifferenceInSeconds(trackInfo.arrivalBack, trackInfo.startFront) / 60;
-    const durationString = `Duration in min;${duration.toFixed(2)}\n`;
-    const distance = `Distance in km;${trackInfo.distanceInKm.toFixed(2)}\n`;
-    const averageSpeed = `Average speed in km/h;${((trackInfo.distanceInKm / duration) * 60).toFixed(2)}\n`;
-
-    const times = `Start;${formatTimeOnly(trackInfo.startFront)}\nArrival of front;${formatTimeOnly(
-        trackInfo.arrivalFront
-    )}\nArrival of back;${formatTimeOnly(trackInfo.arrivalBack)}\n`;
-    const peopleCount = `People on track:;${trackInfo.peopleCount ?? ''}\n`;
-    const tableHeaders = `Street;Post code;District;Length in km;Duration in min;Blockage in min;Arrival of front;Passage of front;Arrival of back\n`;
-
-    return `${times}${durationString}${distance}${averageSpeed}${peopleCount}${tableHeaders}`;
-};
-
-export const getHeader = (trackInfo: TrackStreetInfo): string => {
-    const language = getLanguage();
-    switch (language) {
-        case 'de':
-            return germanHeader(trackInfo);
-        case 'en':
-            return englishHeader(trackInfo);
-    }
-};
+import { streetInfoHeaderLength } from './StreetFilesjsPdfDownloader.tsx';
+import { formatNumber, getHeader } from './trackCsvCreator.ts';
 
 function getAdditionalInfo(
     type: TrackWayPointType | undefined,
@@ -99,6 +54,7 @@ export function createStreetTable(trackStreets: TrackStreetInfo): ContentTable {
         .replaceAll('Wahlkreis', '')
         .split('\n')
         .map((row) => row.split(';'));
+
     return {
         layout: 'lightHorizontalLines', // optional
         table: {
@@ -115,6 +71,7 @@ export function createInfoTable(trackStreets: TrackStreetInfo): ContentTable {
     const trackInfo = getHeader(trackStreets)
         .replaceAll('Wahlkreis', '')
         .split('\n')
+        .slice(0, streetInfoHeaderLength)
         .map((row) => row.split(';'));
     return {
         layout: 'lightHorizontalLines', // optional
