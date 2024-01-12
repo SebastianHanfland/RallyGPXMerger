2db337de3df84236285801b58c39b99a5cb3c4ee
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-01-12 15:20:42 +0100

    #97 use google maps for displaying break places

--- website/src/components/streets/trackPdfContentCreator.ts
+++ website/src/components/streets/trackPdfContentCreator.ts
@@ -21,7 +21,8 @@ function getAdditionalInfo(
 
 export function getLink(waypoint: { pointFrom: { lat: number; lon: number }; pointTo: { lat: number; lon: number } }) {
     if (waypoint.pointTo.lat === waypoint.pointFrom.lat && waypoint.pointTo.lon === waypoint.pointFrom.lon) {
-        return `https://www.openstreetmap.org/#map=17/${waypoint.pointTo.lat}/${waypoint.pointTo.lon}`;
+        // return `https://www.openstreetmap.org/#map=17/${waypoint.pointTo.lat}/${waypoint.pointTo.lon}`;
+        return `https://www.google.com/maps/search/${waypoint.pointTo.lat},${waypoint.pointTo.lon}`;
     }
     return `https://www.openstreetmap.org/directions?engine=fossgis_osrm_car&route=${waypoint.pointFrom.lat}%2C${waypoint.pointFrom.lon}%3B${waypoint.pointTo.lat}%2C${waypoint.pointTo.lon}`;
 }
