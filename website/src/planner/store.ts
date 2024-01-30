b42f6b385fcb2508f75ad86f07cc29f9d078e6dd
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-01-30 09:06:59 +0100

    #104 move reducers into stores

--- website/src/planner/store.ts
+++ website/src/planner/store.ts
@@ -1,8 +1,28 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import { Action, combineReducers, configureStore, Reducer, ThunkAction } from '@reduxjs/toolkit';
 import reduceReducer from 'reduce-reducers';
-import { rootReducer, storingReducer } from '../store/reducers.ts';
 import { State } from '../store/types.ts';
+import { gpxSegmentsReducer } from '../store/gpxSegments.reducer.ts';
+import { trackMergeReducer } from '../store/trackMerge.reducer.ts';
+import { calculatedTracksReducer } from '../store/calculatedTracks.reducer.ts';
+import { mapReducer } from '../store/map.reducer.ts';
+import { geoCodingReducer } from '../store/geoCoding.reducer.ts';
+import { geoCodingRequestsReducer } from '../store/geoCodingRequests.reducer.ts';
+import { storage } from '../store/storage.ts';
 
+const rootReducer: Reducer = combineReducers({
+    gpxSegments: gpxSegmentsReducer,
+    trackMerge: trackMergeReducer,
+    calculatedTracks: calculatedTracksReducer,
+    map: mapReducer,
+    geoCoding: geoCodingReducer,
+    geoCodingRequests: geoCodingRequestsReducer,
+});
+const storingReducer: Reducer = (state: State) => {
+    if (state) {
+        storage.save(state);
+    }
+    return state;
+};
 export const createStore = () =>
     configureStore({
         reducer: reduceReducer(rootReducer, storingReducer),
--- website/src/versions/store.ts
+++ website/src/versions/store.ts
@@ -1,5 +1,11 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { iFrameReducer } from '../store/reducers.ts';
+import { combineReducers, configureStore, Reducer } from '@reduxjs/toolkit';
+import { zipTracksReducer } from '../store/zipTracks.reducer.ts';
+import { mapReducer } from '../store/map.reducer.ts';
+
+const iFrameReducer: Reducer = combineReducers({
+    zipTracks: zipTracksReducer,
+    map: mapReducer,
+});
 
 const createIframeStore = () =>
     configureStore({
