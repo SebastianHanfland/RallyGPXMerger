047f29d4b19fe8c4811364e03488d29ceafde1ac
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-01 15:49:08 +0100

    #100 also resolve positions

--- website/src/planner/wizard/simpleRallyThunk.ts
+++ website/src/planner/wizard/simpleRallyThunk.ts
@@ -5,14 +5,17 @@ import { trackMergeActions } from '../store/trackMerge.reducer.ts';
 import { v4 as uuidv4 } from 'uuid';
 import { calculateMerge } from '../logic/merge/MergeCalculation.ts';
 import { AppDispatch } from '../store/store.ts';
+import { resolvePositions } from '../logic/resolving/resolveStreetAndPostcodeInfo.ts';
 
 export const simpleRallyThunk = (dispatch: AppDispatch, getState: () => State) => {
     const gpxSegments = getGpxSegments(getState());
     gpxSegments.forEach((segment) => {
-        trackMergeActions.addTrackComposition({ id: uuidv4(), segmentIds: [segment.id], name: segment.filename });
+        dispatch(
+            trackMergeActions.addTrackComposition({ id: uuidv4(), segmentIds: [segment.id], name: segment.filename })
+        );
     });
-    dispatch(calculateMerge);
-    // Maybe even the street resolving
-    // Offer document for download
-    dispatch(layoutActions.selectSection('gps'));
+    setTimeout(() => {
+        dispatch(calculateMerge).then(() => dispatch(resolvePositions));
+        dispatch(layoutActions.selectSection('streets'));
+    }, 10);
 };
