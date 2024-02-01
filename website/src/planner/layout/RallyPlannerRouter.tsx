0494f1993919fa59501790bc769eb48356afd3ca
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-01 12:14:51 +0100

    #100 use a fallback to go to gps when arrival date is present

--- website/src/planner/layout/RallyPlannerRouter.tsx
+++ website/src/planner/layout/RallyPlannerRouter.tsx
@@ -3,15 +3,26 @@ import { AppFooter } from './Footer.tsx';
 import { MergeAndMap } from './MergeAndMap.tsx';
 import { AppHeader } from './Header.tsx';
 import { WizardParameters } from '../wizard/WizardParameters.tsx';
-import { useSelector } from 'react-redux';
-import { getSelectionSection } from '../store/layout.reducer.ts';
+import { useDispatch, useSelector } from 'react-redux';
+import { getSelectionSection, layoutActions } from '../store/layout.reducer.ts';
 import { WizardSegments } from '../wizard/WizardSegments.tsx';
 import { Container } from 'react-bootstrap';
 import { getArrivalDateTime } from '../store/trackMerge.reducer.ts';
+import { useEffect } from 'react';
+import { Sections } from './types.ts';
 
 export const RallyPlannerRouter = () => {
     const selectedSection = useSelector(getSelectionSection);
     const arrivalDateTime = useSelector(getArrivalDateTime);
+
+    const dispatch = useDispatch();
+    const setSelectedSection = (section: Sections) => dispatch(layoutActions.selectSection(section));
+
+    useEffect(() => {
+        if (selectedSection === 'start' && !!arrivalDateTime) {
+            setSelectedSection('gps');
+        }
+    }, [arrivalDateTime, selectedSection]);
 
     const isFreshStart = selectedSection === 'start' && !arrivalDateTime;
     if (isFreshStart) {
