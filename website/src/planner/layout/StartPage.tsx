be408d9a46198c8e486710ddb3fa62363fdbac2b
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-01 10:19:10 +0100

    #100 improve the new start screen, with icons and visual effects

--- website/src/planner/layout/StartPage.tsx
+++ website/src/planner/layout/StartPage.tsx
@@ -1,7 +1,13 @@
-import { Card, Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { Sections } from './types.ts';
+import stars from '../../assets/stars.svg';
+import fileUp from '../../assets/file-up.svg';
+import info from '../../assets/info.svg';
 
-const cardStyle = { style: { minWidth: '18rem', minHeight: '25rem', cursor: 'pointer' }, className: 'shadow m-2' };
+const cardStyle = {
+    style: { minWidth: '18rem', minHeight: '25rem', cursor: 'pointer' },
+    className: 'startPageCard shadow m-2',
+};
 
 interface Props {
     setSelectedSection: (section: Sections) => void;
@@ -10,29 +16,38 @@ interface Props {
 export const StartPage = ({ setSelectedSection }: Props) => {
     return (
         <Container>
-            <h3 className={'m-5'}>Rally GPX Merger</h3>
-            <h5>Choose your situation</h5>
+            <h3 className={'mb-5'}>Rally GPX Merger</h3>
+            <h5 className={'mb-5'}>Choose your situation:</h5>
             <Row>
                 <Col>
                     <Card {...cardStyle} onClick={() => setSelectedSection('gps')}>
                         <Card.Body>
                             <Card.Title>Start a new planning</Card.Title>
-                            <Card.Text>Plan a moving demonstration with multiple branches</Card.Text>
+                            <Card.Text style={{ minHeight: '3rem' }}>
+                                Plan a moving demonstration with multiple branches
+                            </Card.Text>
+                            <img
+                                src={stars}
+                                className="m-1"
+                                alt="start new planning"
+                                style={{ height: '10rem', width: '10rem' }}
+                                color={'#ffffff'}
+                            />
                         </Card.Body>
-                        <ListGroup className="list-group-flush">
-                            <ListGroup.Item>Set parameters</ListGroup.Item>
-                            <ListGroup.Item>Upload Gpx segments</ListGroup.Item>
-                            <ListGroup.Item>Group segments into tracks</ListGroup.Item>
-                            <ListGroup.Item>Resolve Street data</ListGroup.Item>
-                            <ListGroup.Item>Download documents</ListGroup.Item>
-                        </ListGroup>
                     </Card>
                 </Col>
                 <Col>
                     <Card {...cardStyle} onClick={() => setSelectedSection('importExport')}>
                         <Card.Body>
                             <Card.Title>Load an existing planning</Card.Title>
-                            <Card.Text>Load an existing planning via json file</Card.Text>
+                            <Card.Text style={{ minHeight: '3rem' }}>Load an existing planning via json file</Card.Text>
+                            <img
+                                src={fileUp}
+                                className="m-1"
+                                alt="open existing planning"
+                                style={{ height: '10rem', width: '10rem' }}
+                                color={'#ffffff'}
+                            />
                         </Card.Body>
                     </Card>
                 </Col>
@@ -40,7 +55,16 @@ export const StartPage = ({ setSelectedSection }: Props) => {
                     <Card {...cardStyle} onClick={() => setSelectedSection('gps')}>
                         <Card.Body>
                             <Card.Title>Learn how to do a planning</Card.Title>
-                            <Card.Text>Go through a sample planning of a demonstration with help</Card.Text>
+                            <Card.Text style={{ minHeight: '3rem' }}>
+                                Go through a sample planning of a demonstration with help
+                            </Card.Text>
+                            <img
+                                src={info}
+                                className="m-1"
+                                alt="open existing planning"
+                                style={{ height: '10rem', width: '10rem' }}
+                                color={'#ffffff'}
+                            />
                         </Card.Body>
                     </Card>
                 </Col>
