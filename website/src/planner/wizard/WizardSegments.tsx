3a2df684d8b021d6ed6625c06bc4ceee74669e1b
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-01 12:28:35 +0100

    #100 lead the user a bit better

--- website/src/planner/wizard/WizardSegments.tsx
+++ website/src/planner/wizard/WizardSegments.tsx
@@ -1,13 +1,15 @@
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import { WizardHeader } from './WizardHeader.tsx';
 import { layoutActions } from '../store/layout.reducer.ts';
 import { Sections } from '../layout/types.ts';
 import { GpxSegments } from '../segments/GpxSegments.tsx';
+import { getGpxSegments } from '../store/gpxSegments.reducer.ts';
 
 export const WizardSegments = () => {
     const dispatch = useDispatch();
     const setSelectedSection = (section: Sections) => dispatch(layoutActions.selectSection(section));
+    const gpxSegments = useSelector(getGpxSegments);
 
     return (
         <Container>
@@ -15,11 +17,17 @@ export const WizardSegments = () => {
             <h5>Upload single segments here, which will be used to construct different tracks</h5>
             <Row>
                 <Col>
-                    <Button className={'my-4'} onClick={() => setSelectedSection('gps')}>
-                        Continue
-                    </Button>
+                    {gpxSegments.length > 5 && (
+                        <Button className={'my-4'} onClick={() => setSelectedSection('gps')}>
+                            Continue
+                        </Button>
+                    )}
                     <GpxSegments noFilter={true} />
-                    <Button className={'my-4'} onClick={() => setSelectedSection('gps')}>
+                    <Button
+                        className={'my-4'}
+                        onClick={() => setSelectedSection('gps')}
+                        disabled={gpxSegments.length === 0}
+                    >
                         Continue
                     </Button>
                 </Col>
