887287bf0c3781ea473d842813837a62c9368a41
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-02 12:20:18 +0100

    #106 Dummy for a dashboard

--- website/src/planner/layout/Dashboard.tsx
+++ website/src/planner/layout/Dashboard.tsx
@@ -1,6 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getShowDashboard, layoutActions } from '../store/layout.reducer.ts';
-import { Offcanvas } from 'react-bootstrap';
+import { Card, Col, Container, Offcanvas, Row } from 'react-bootstrap';
+
+function DashboardCard({ text, done, canBeDone }: { text: string; done: boolean; canBeDone: boolean }) {
+    return (
+        <Card
+            style={{ cursor: 'pointer', backgroundColor: done ? 'lightgreen' : canBeDone ? undefined : 'lightsalmon' }}
+            className={'startPageCard shadow m-2'}
+        >
+            {text}
+        </Card>
+    );
+}
 
 export function Dashboard() {
     const show = useSelector(getShowDashboard);
@@ -12,9 +23,45 @@ export function Dashboard() {
                 <Offcanvas.Title>Dashboard</Offcanvas.Title>
             </Offcanvas.Header>
             <Offcanvas.Body>
-                {/*<Col xl={6}>*/}
-                Hello world
-                {/*</Col>*/}
+                <Container>
+                    <Row>
+                        <Col>
+                            <DashboardCard text={'Settings'} done={true} canBeDone={true} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DashboardCard text={'Segments'} done={true} canBeDone={true} />
+                        </Col>
+
+                        <Col>
+                            <DashboardCard text={'Tracks'} done={true} canBeDone={true} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DashboardCard text={'Tracks Merging'} done={false} canBeDone={true} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DashboardCard text={'Street info'} done={false} canBeDone={false} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DashboardCard text={'Street grouping'} done={false} canBeDone={false} />
+                        </Col>
+                        <Col>
+                            <DashboardCard text={'Post codes'} done={false} canBeDone={false} />
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col>
+                            <DashboardCard text={'Documents'} done={false} canBeDone={false} />
+                        </Col>
+                    </Row>
+                </Container>
             </Offcanvas.Body>
         </Offcanvas>
     );
