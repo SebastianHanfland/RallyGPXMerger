ed8b99f96a2f702e801d5c790eed70162a2ab1d9
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-02 11:35:43 +0100

    #87 allow replacement for a multiple files

--- website/src/planner/segments/fileReplaceThunk.ts
+++ website/src/planner/segments/fileReplaceThunk.ts
@@ -1,9 +1,11 @@
 import { Dispatch } from '@reduxjs/toolkit';
 import { State } from '../store/types.ts';
 import { getReplaceProcess, gpxSegmentsActions } from '../store/gpxSegments.reducer.ts';
+import { getTrackCompositions, trackMergeActions } from '../store/trackMerge.reducer.ts';
 
 export const executeGpxSegmentReplacement = (dispatch: Dispatch, getState: () => State) => {
     const replaceProcess = getReplaceProcess(getState());
+    const trackCompositions = getTrackCompositions(getState());
 
     if (!replaceProcess || replaceProcess.replacementSegments.length === 0) {
         return;
@@ -15,6 +17,17 @@ export const executeGpxSegmentReplacement = (dispatch: Dispatch, getState: () =>
         dispatch(gpxSegmentsActions.changeGpxSegmentContent(payload));
         dispatch(gpxSegmentsActions.setFilename({ id: targetSegment, filename: replacementSegments[0].filename }));
     } else {
+        const replacementIds = replacementSegments.map((segment) => segment.id);
+        trackCompositions.forEach((track) => {
+            if (track.segmentIds.includes(targetSegment)) {
+                const newSegments = track.segmentIds.flatMap((segmentId) =>
+                    segmentId === targetSegment ? [...replacementIds] : segmentId
+                );
+                dispatch(trackMergeActions.setSegments({ id: track.id, segments: newSegments }));
+            }
+        });
+        dispatch(gpxSegmentsActions.addGpxSegments(replacementSegments));
+        dispatch(gpxSegmentsActions.removeGpxSegment(targetSegment));
     }
 
     dispatch(gpxSegmentsActions.setReplaceProcess(undefined));
