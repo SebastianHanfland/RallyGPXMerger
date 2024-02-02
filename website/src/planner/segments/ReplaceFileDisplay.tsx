fd15974630ff4bd960403015100ad8d0835d3532
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-02 11:19:28 +0100

    #87 allow changing of file name

--- website/src/planner/segments/ReplaceFileDisplay.tsx
+++ website/src/planner/segments/ReplaceFileDisplay.tsx
@@ -1,12 +1,13 @@
 import { Form } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
-import { gpxSegmentsActions } from '../store/gpxSegments.reducer.ts';
+import { useDispatch, useSelector } from 'react-redux';
+import { getReplaceProcess, gpxSegmentsActions } from '../store/gpxSegments.reducer.ts';
 import { GpxSegment } from '../../common/types.ts';
 import { RemoveReplaceFileButton } from './RemoveReplaceFileButton.tsx';
 
 export function ReplaceFileDisplay({ gpxSegment }: { gpxSegment: GpxSegment }) {
     const { id, filename } = gpxSegment;
     const dispatch = useDispatch();
+    const replacementProcess = useSelector(getReplaceProcess);
 
     return (
         <tr>
@@ -15,7 +16,19 @@ export function ReplaceFileDisplay({ gpxSegment }: { gpxSegment: GpxSegment }) {
                     type="text"
                     placeholder="File name"
                     value={filename}
-                    onChange={(value) => dispatch(gpxSegmentsActions.setFilename({ id, filename: value.target.value }))}
+                    onChange={(value) => {
+                        if (!replacementProcess) {
+                            return;
+                        }
+                        dispatch(
+                            gpxSegmentsActions.setReplaceProcess({
+                                ...replacementProcess,
+                                replacementSegments: replacementProcess.replacementSegments.map((segment) =>
+                                    segment.id === id ? { ...segment, filename: value.target.value } : segment
+                                ),
+                            })
+                        );
+                    }}
                 />
             </td>
             <td>
