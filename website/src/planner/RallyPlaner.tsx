b2e9e1ce97fa7b0804cd56d1a920498a031041b2
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-05 18:32:37 +0100

    #106 try out a side component

--- website/src/planner/RallyPlaner.tsx
+++ website/src/planner/RallyPlaner.tsx
@@ -1,13 +1,46 @@
 import '../App.css';
-import { Provider } from 'react-redux';
+import { Provider, useDispatch } from 'react-redux';
 import { store } from './store/store.ts';
 import { RallyPlannerRouter } from './layout/RallyPlannerRouter.tsx';
 import { Dashboard } from './layout/dashboard/Dashboard.tsx';
+import { CSSProperties } from 'react';
+import { layoutActions } from './store/layout.reducer.ts';
+import info from '../assets/info.svg';
+
+const style: CSSProperties = {
+    position: 'fixed',
+    width: '30px',
+    height: '100vh',
+    left: 0,
+    right: 0,
+    overflowY: 'scroll',
+    top: 0,
+    zIndex: 10,
+    backgroundColor: '#0d6efd',
+    cursor: 'pointer',
+};
+
+function FloatingInfoButton() {
+    const dispatch = useDispatch();
+    return (
+        <div style={style} className={'shadow'} onClick={() => dispatch(layoutActions.setShowDashboard(true))}>
+            <img
+                src={info}
+                className={'m-1'}
+                style={{ height: '20px', width: '20px', position: 'fixed', top: '48vh', left: 0 }}
+                alt="help"
+            />
+        </div>
+    );
+}
 
 export function RallyPlaner() {
     return (
         <Provider store={store}>
-            <RallyPlannerRouter />
+            <div style={{ left: '30px', position: 'fixed' }}>
+                <RallyPlannerRouter />
+            </div>
+            <FloatingInfoButton />
             <Dashboard />
         </Provider>
     );
