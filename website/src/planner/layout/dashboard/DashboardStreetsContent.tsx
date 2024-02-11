dd43e870df2066f9f6e86df0f21a8c4e18aa472a
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-11 10:11:06 +0100

    #107 translate * <p>

--- website/src/planner/layout/dashboard/DashboardStreetsContent.tsx
+++ website/src/planner/layout/dashboard/DashboardStreetsContent.tsx
@@ -1,11 +1,14 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { layoutActions } from '../../store/layout.reducer.ts';
 import { getEnrichedTrackStreetInfos } from '../../logic/resolving/selectors/getEnrichedTrackStreetInfos.ts';
-import { Button, Spinner } from 'react-bootstrap';
+import { Button, ProgressBar, Spinner } from 'react-bootstrap';
 import { resolvePositions } from '../../logic/resolving/resolveStreetAndPostcodeInfo.ts';
 import { AppDispatch } from '../../store/store.ts';
 import { calculateTrackStreetInfos } from '../../logic/resolving/aggregate/calculateTrackStreetInfos.ts';
-import { addPostCodeToStreetInfos } from '../../logic/resolving/postcode/postCodeResolver.ts';
+import {
+    addPostCodeToStreetInfos,
+    getPostCodeRequestProgress,
+} from '../../logic/resolving/postcode/postCodeResolver.ts';
 import {
     getIsAggregating,
     getIsLoadingGeoData,
@@ -17,6 +20,7 @@ import { geoCodingActions, getResolvedPositions, getResolvedPostCodes } from '..
 import { Done } from './Done.tsx';
 import { Warning } from './Warning.tsx';
 import { FormattedMessage } from 'react-intl';
+import { getRequestProgress } from '../../logic/resolving/selectors/requestEstimator.ts';
 
 export function StreetStatus(props: { done: boolean; loading: boolean }) {
     if (props.loading) {
@@ -41,6 +45,9 @@ export function DashboardStreetsContent() {
     const isLoading = useSelector(getIsLoadingGeoData);
     const isAggregating = useSelector(getIsAggregating);
 
+    const requestProgress = useSelector(getRequestProgress);
+    const postCodeProgress = useSelector(getPostCodeRequestProgress);
+
     const streetsDone = !runningRequests && hasStreetInfo;
     const postCodesDone = !runningPostCodeRequests && hasPostCodeInfo;
 
@@ -54,6 +61,7 @@ export function DashboardStreetsContent() {
                 </b>
                 <Button
                     size={'sm'}
+                    variant={'success'}
                     onClick={(event) => {
                         event.stopPropagation();
                         dispatch(layoutActions.selectSection('streets'));
@@ -61,7 +69,7 @@ export function DashboardStreetsContent() {
                     }}
                     disabled={ongoingRequests}
                 >
-                    <FormattedMessage id={'msg.triggerAll'} />
+                    <FormattedMessage id={'msg.triggerAll'} /> *
                 </Button>
             </div>
             <div className={'d-flex justify-content-between m-1'}>
@@ -74,10 +82,15 @@ export function DashboardStreetsContent() {
                         dispatch(resolveStreetNames);
                     }}
                 >
-                    <FormattedMessage id={'msg.streetNames'} />
+                    <FormattedMessage id={'msg.streetNames'} /> *
                 </Button>
                 <StreetStatus done={streetsDone} loading={runningRequests} />
             </div>
+            {requestProgress !== undefined && (
+                <div className={'m-2'}>
+                    <ProgressBar now={requestProgress} label={`${requestProgress?.toFixed(0)}%`}></ProgressBar>
+                </div>
+            )}
             <div className={'d-flex justify-content-between m-1'}>
                 <Button
                     size={'sm'}
@@ -103,10 +116,26 @@ export function DashboardStreetsContent() {
                     }}
                     disabled={ongoingRequests}
                 >
-                    <FormattedMessage id={'msg.postCodes'} />
+                    <FormattedMessage id={'msg.postCodes'} /> *
                 </Button>
                 <StreetStatus done={postCodesDone} loading={runningPostCodeRequests} />
             </div>
+            {postCodeProgress !== undefined && (
+                <div className={'m-2'}>
+                    <ProgressBar now={postCodeProgress} label={`${postCodeProgress?.toFixed(0)}%`}></ProgressBar>
+                </div>
+            )}
+            <p className={'m-1'} style={{ fontSize: '12px' }}>
+                *<FormattedMessage id={'msg.iAgree.part1'} />{' '}
+                <a href={'https://www.geoapify.com/'} target={'_blank'} referrerPolicy={'no-referrer'}>
+                    geoapify
+                </a>{' '}
+                <FormattedMessage id={'msg.iAgree.part2'} />{' '}
+                <a href={'https://www.bigdatacloud.com/'} target={'_blank'} referrerPolicy={'no-referrer'}>
+                    BigDataCloud
+                </a>
+                <FormattedMessage id={'msg.iAgree.part3'} />
+            </p>
         </div>
     );
 }
--- website/src/planner/streets/TrackDataOverview.tsx
+++ website/src/planner/streets/TrackDataOverview.tsx
@@ -1,39 +1,22 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { Button, ProgressBar, Spinner } from 'react-bootstrap';
 import { AppDispatch } from '../store/store.ts';
 import { getGpxSegments } from '../store/gpxSegments.reducer.ts';
 import { useEffect } from 'react';
-import {
-    estimateRequestsForStreetResolving,
-    getRequestProgress,
-} from '../logic/resolving/selectors/requestEstimator.ts';
-import {
-    getIsLoadingGeoData,
-    getNumberOfPostCodeRequestsRunning,
-    getNumberOfRequestsRunning,
-} from '../store/geoCodingRequests.reducer.ts';
-import { addPostCodeToStreetInfos, getPostCodeRequestProgress } from '../logic/resolving/postcode/postCodeResolver.ts';
+import { estimateRequestsForStreetResolving } from '../logic/resolving/selectors/requestEstimator.ts';
 import { TrackDataOverviewTable } from './TrackDataOverviewTable.tsx';
-import { geoCodingActions, getResolvedPostCodes } from '../store/geoCoding.reducer.ts';
-import { calculateTrackStreetInfos } from '../logic/resolving/aggregate/calculateTrackStreetInfos.ts';
-import { resolvePositions } from '../logic/resolving/resolveStreetAndPostcodeInfo.ts';
 import { FormattedMessage } from 'react-intl';
+import { DashboardStreetsContent } from '../layout/dashboard/DashboardStreetsContent.tsx';
+import { getCalculatedTracks } from '../store/calculatedTracks.reducer.ts';
 
 export function TrackDataOverview() {
     const gpxSegments = useSelector(getGpxSegments);
-    const requestProgress = useSelector(getRequestProgress);
-    const runningRequests = useSelector(getNumberOfRequestsRunning) > 0;
-    const runningPostCodeRequests = useSelector(getNumberOfPostCodeRequestsRunning) > 0;
-    const isLoading = useSelector(getIsLoadingGeoData);
-    const postCodeProgress = useSelector(getPostCodeRequestProgress);
-    const hasNoPostCodes = Object.keys(useSelector(getResolvedPostCodes)).length === 0;
+    const calculatedTracks = useSelector(getCalculatedTracks);
     const dispatch: AppDispatch = useDispatch();
 
     useEffect(() => {
         dispatch(estimateRequestsForStreetResolving);
-    }, [gpxSegments.length]);
+    }, [gpxSegments.length, calculatedTracks.length]);
 
-    const ongoingRequests = runningRequests || runningPostCodeRequests || isLoading;
     return (
         <div className={'m-2 p-2'}>
             <h4>
@@ -41,62 +24,7 @@ export function TrackDataOverview() {
             </h4>
             <TrackDataOverviewTable />
 
-            <div className={'mb-2'}>
-                <Button onClick={() => dispatch(calculateTrackStreetInfos)}>
-                    Recalculate the streets without fetching
-                </Button>
-            </div>
-            <Button
-                variant={'success'}
-                onClick={() => dispatch(resolvePositions)}
-                disabled={ongoingRequests}
-                title={'Fetching the street information from geoapify and BigDataCloud'}
-            >
-                {ongoingRequests ? (
-                    <span>
-                        <Spinner size={'sm'} />
-                        {runningRequests && <span>Fetching street information</span>}
-                        {runningPostCodeRequests && <span>Fetching post codes</span>}
-                        {isLoading && !runningRequests && !runningPostCodeRequests && <span>Grouping Streets</span>}
-                    </span>
-                ) : (
-                    <span>Fetch the street information and post codes*</span>
-                )}
-            </Button>
-            <p className={'m-1'} style={{ fontSize: '12px' }}>
-                * with clicking, I agree to send data to{' '}
-                <a href={'https://www.geoapify.com/'} target={'_blank'} referrerPolicy={'no-referrer'}>
-                    geoapify
-                </a>{' '}
-                and{' '}
-                <a href={'https://www.bigdatacloud.com/'} target={'_blank'} referrerPolicy={'no-referrer'}>
-                    BigDataCloud
-                </a>
-            </p>
-            <hr />
-            <h5>Street information</h5>
-            {requestProgress !== undefined && (
-                <div className={'m-2'}>
-                    <ProgressBar now={requestProgress} label={`${requestProgress?.toFixed(0)}%`}></ProgressBar>
-                </div>
-            )}
-            <h5>Post codes</h5>
-            <Button variant={'success'} onClick={() => dispatch(addPostCodeToStreetInfos)} disabled={ongoingRequests}>
-                Fetch Postcodes
-            </Button>
-            <Button
-                className={'mx-1'}
-                variant={'danger'}
-                onClick={() => dispatch(geoCodingActions.clearPostCodesAndDistricts())}
-                disabled={hasNoPostCodes || ongoingRequests}
-            >
-                Clear Postcodes
-            </Button>
-            {postCodeProgress !== undefined && (
-                <div className={'m-2'}>
-                    <ProgressBar now={postCodeProgress} label={`${postCodeProgress?.toFixed(0)}%`}></ProgressBar>
-                </div>
-            )}
+            <DashboardStreetsContent />
         </div>
     );
 }
