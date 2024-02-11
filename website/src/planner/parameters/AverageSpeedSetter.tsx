93051e2fdd4b4e7db02753caf738bd8937ad38b4
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-11 09:30:24 +0100

    #107 finish settings section

--- website/src/planner/parameters/AverageSpeedSetter.tsx
+++ website/src/planner/parameters/AverageSpeedSetter.tsx
@@ -1,13 +1,16 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getAverageSpeedInKmH, trackMergeActions } from '../store/trackMerge.reducer.ts';
 import { Form } from 'react-bootstrap';
+import { FormattedMessage } from 'react-intl';
 
 export function AverageSpeedSetter() {
     const dispatch = useDispatch();
     const averageSpeed = useSelector(getAverageSpeedInKmH);
     return (
         <div className={'d-inline-block'}>
-            <h5 className="form-label m-3">Average speed of the rally</h5>
+            <h5 className="form-label m-3">
+                <FormattedMessage id={'msg.averageSpeed.title'} />
+            </h5>
             <div className={'d-flex'}>
                 <span className={'mx-4'}>3&nbsp;km/h</span>
                 <Form.Range
@@ -20,7 +23,7 @@ export function AverageSpeedSetter() {
                 <span className={'mx-4'}>20&nbsp;km/h</span>
             </div>
             <h6 className="form-label m-3">
-                {'Average speed: '}
+                <FormattedMessage id={'msg.averageSpeed'} />
                 <span className={'bg-info p-1'}>{averageSpeed.toFixed(1) + '\xa0km/h'}</span>
             </h6>
         </div>
--- website/src/planner/parameters/ParticipantsDelaySetter.tsx
+++ website/src/planner/parameters/ParticipantsDelaySetter.tsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getAverageSpeedInKmH, getParticipantsDelay, trackMergeActions } from '../store/trackMerge.reducer.ts';
 import { Form } from 'react-bootstrap';
+import { FormattedMessage } from 'react-intl';
 
 export function ParticipantsDelaySetter() {
     const dispatch = useDispatch();
@@ -8,9 +9,13 @@ export function ParticipantsDelaySetter() {
     const participantsDelay = useSelector(getParticipantsDelay);
     return (
         <div className={'d-inline-block'}>
-            <h5 className="form-label m-3">Expansion of participants</h5>
+            <h5 className="form-label m-3">
+                <FormattedMessage id={'msg.participantsDelay.title'} />
+            </h5>
             <div className={'d-flex'}>
-                <span className={'mx-4'}>None</span>
+                <span className={'mx-4'}>
+                    <FormattedMessage id={'msg.none'} />
+                </span>
                 <Form.Range
                     min={0}
                     max={1.0}
@@ -18,14 +23,16 @@ export function ParticipantsDelaySetter() {
                     value={participantsDelay}
                     onChange={(event) => dispatch(trackMergeActions.setParticipantsDelays(Number(event.target.value)))}
                 />
-                <span className={'mx-4'}>A&nbsp;lot</span>
+                <span className={'mx-4'} style={{ whiteSpace: 'nowrap' }}>
+                    <FormattedMessage id={'msg.aLot'} />
+                </span>
             </div>
             <h6 className="form-label m-3">
-                {'Delay of one participant: '}
+                <FormattedMessage id={'msg.participantsDelay'} />
                 <span className={'bg-info p-1'}>{participantsDelay.toFixed(2) + '\xa0s'}</span>
             </h6>
             <h6 className="form-label m-3">
-                {`Length of 100 people (based on ${averageSpeed.toFixed(1)} km/h): `}
+                <FormattedMessage id={'msg.participantsLength'} values={{ speed: averageSpeed.toFixed(1) }} />
                 <span className={'bg-info p-1'}>
                     {Math.round((averageSpeed / 3.6) * 100 * participantsDelay) + '\xa0m'}
                 </span>
