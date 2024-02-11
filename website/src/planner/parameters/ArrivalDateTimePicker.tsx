a6d2b4b45d028ae06a1ef47bf677392eaa552c2a
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-11 09:12:18 +0100

    #107 translate settings part 1

--- website/src/planner/parameters/ArrivalDateTimePicker.tsx
+++ website/src/planner/parameters/ArrivalDateTimePicker.tsx
@@ -2,14 +2,19 @@ import DatePicker from 'react-datepicker';
 import { useDispatch, useSelector } from 'react-redux';
 import { getArrivalDateTime, trackMergeActions } from '../store/trackMerge.reducer.ts';
 import 'react-datepicker/dist/react-datepicker.css';
+import { FormattedMessage } from 'react-intl';
 
 export function ArrivalDateTimePicker() {
     const dispatch = useDispatch();
     const arrivalDateTime = useSelector(getArrivalDateTime);
     return (
         <div className={'d-inline-block'}>
-            <h5 className="form-label m-3">Arrival of tracks:</h5>
-            <p>Time when the first people should arrive at the end of the track.</p>
+            <h5 className="form-label m-3">
+                <FormattedMessage id={'msg.arrivalDate.title'} />
+            </h5>
+            <p>
+                <FormattedMessage id={'msg.arrivalDate.hint'} />
+            </p>
             <DatePicker
                 className={'form-control'}
                 dateFormat={'dd.MM.yyyy HH:mm'}
--- website/src/planner/parameters/PlanningLabel.tsx
+++ website/src/planner/parameters/PlanningLabel.tsx
@@ -1,17 +1,23 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getPlanningLabel, trackMergeActions } from '../store/trackMerge.reducer.ts';
 import { Form } from 'react-bootstrap';
+import { FormattedMessage, useIntl } from 'react-intl';
 
 export function PlanningLabel() {
+    const intl = useIntl();
     const dispatch = useDispatch();
     const planningLabel = useSelector(getPlanningLabel);
     return (
         <div className={'d-inline-block'}>
-            <h5 className="form-label m-3">Label of current planning:</h5>
-            <p>This can be a version string that appears in the file names and the documents</p>
+            <h5 className="form-label m-3">
+                <FormattedMessage id={'msg.label.title'} />
+            </h5>
+            <p>
+                <FormattedMessage id={'msg.label.hint'} />
+            </p>
             <Form.Control
                 type="text"
-                placeholder="Label for planning"
+                placeholder={intl.formatMessage({ id: 'msg.label.title' })}
                 value={planningLabel}
                 onChange={(value) => dispatch(trackMergeActions.setPlanningLabel(value.target.value))}
             />
