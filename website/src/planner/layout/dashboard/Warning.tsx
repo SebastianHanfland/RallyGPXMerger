853ea1ddaa9d13aaf9f02b21ad188066ef6b94f6
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-16 07:18:03 +0100

    #114 introduce a new dialog which leads the user

--- website/src/planner/layout/dashboard/Warning.tsx
+++ website/src/planner/layout/dashboard/Warning.tsx
@@ -1,5 +1,5 @@
 import warning from '../../../assets/warning.svg';
 
-export function Warning() {
-    return <img src={warning} className="m-1" alt="warning" style={{ width: '20px', height: '20px' }} />;
+export function Warning({ size }: { size?: number }) {
+    return <img src={warning} className="m-1" alt="warning" style={{ width: `${size}px`, height: `${size}px` }} />;
 }
