aae96dbcddaf95f5cdb6b6c9b7364d079f793dbf
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-02-20 07:02:07 +0100

    #0 bring title to own row when screen is small

--- website/src/planner/layout/Header.tsx
+++ website/src/planner/layout/Header.tsx
@@ -7,25 +7,29 @@ import { SectionNavigation } from './SectionNavigation.tsx';
 
 export const AppHeader = () => {
     return (
-        <Row>
-            <div className="footer-copyright text-center py-3">
-                <ButtonToolbar className="justify-content-between" aria-label="Toolbar with Button groups">
-                    <SectionNavigation />
-                    <h1 className={'d-none d-lg-block'}>
-                        <FormattedMessage id={'msg.appName'} />
-                    </h1>
-                    <h3 className={'d-lg-none'}>
-                        <FormattedMessage id={'msg.appName'} />
-                    </h3>
-                    <ButtonToolbar aria-label="Toolbar with Button groups" className={'m-0'}>
-                        <ButtonGroup aria-label="help-buttons" className={'m-0'}>
-                            <HelpButton />
-                            <RemoveDataButton />
-                            <LanguageSelection />
-                        </ButtonGroup>
+        <>
+            <Row className={'d-lg-none'}>
+                <h3 className={'d-lg-none'}>
+                    <FormattedMessage id={'msg.appName'} />
+                </h3>
+            </Row>
+            <Row>
+                <div className="footer-copyright text-center py-3">
+                    <ButtonToolbar className="justify-content-between" aria-label="Toolbar with Button groups">
+                        <SectionNavigation />
+                        <h1 className={'d-none d-lg-block'}>
+                            <FormattedMessage id={'msg.appName'} />
+                        </h1>
+                        <ButtonToolbar aria-label="Toolbar with Button groups" className={'m-0'}>
+                            <ButtonGroup aria-label="help-buttons" className={'m-0'}>
+                                <HelpButton />
+                                <RemoveDataButton />
+                                <LanguageSelection />
+                            </ButtonGroup>
+                        </ButtonToolbar>
                     </ButtonToolbar>
-                </ButtonToolbar>
-            </div>
-        </Row>
+                </div>
+            </Row>
+        </>
     );
 };
