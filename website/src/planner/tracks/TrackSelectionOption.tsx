c65992c69cdcebd1cb69baefba3b130a170b0794
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-03-20 04:21:33 +0100

    #126: maybe make the hovering a bit smoother

--- website/src/planner/tracks/TrackSelectionOption.tsx
+++ website/src/planner/tracks/TrackSelectionOption.tsx
@@ -19,27 +19,30 @@ export function TrackSelectionOption({ segmentId, segmentName, trackId }: Props)
         <div
             onMouseEnter={() => dispatch(mapActions.setHighlightedSegmentId(segmentId))}
             onMouseLeave={() => dispatch(mapActions.setHighlightedSegmentId(undefined))}
-            className={'d-flex justify-content-between'}
-            style={{
-                border: '1px solid transparent',
-                borderColor: 'black',
-                cursor: 'pointer',
-                margin: '1px',
-                backgroundColor: getColorFromUuid(segmentId),
-            }}
-            key={segmentId}
         >
-            <div className={'m-1'} title={segmentName}>
-                {segmentName}
-            </div>
-            <Button
-                variant="danger"
-                size={'sm'}
-                onClick={() => dispatch(trackMergeActions.removeSegmentFromTrack({ id: trackId, segmentId }))}
-                title={intl.formatMessage({ id: 'msg.removeTrackSegment' }, { segmentName })}
+            <div
+                className={'d-flex justify-content-between'}
+                style={{
+                    border: '1px solid transparent',
+                    borderColor: 'black',
+                    cursor: 'pointer',
+                    margin: '1px',
+                    backgroundColor: getColorFromUuid(segmentId),
+                }}
+                key={segmentId}
             >
-                X
-            </Button>
+                <div className={'m-1'} title={segmentName}>
+                    {segmentName}
+                </div>
+                <Button
+                    variant="danger"
+                    size={'sm'}
+                    onClick={() => dispatch(trackMergeActions.removeSegmentFromTrack({ id: trackId, segmentId }))}
+                    title={intl.formatMessage({ id: 'msg.removeTrackSegment' }, { segmentName })}
+                >
+                    X
+                </Button>
+            </div>
         </div>
     );
 }
