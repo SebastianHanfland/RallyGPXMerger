847ae8df173a2fa64bdffac9f670be6c5f6e60b8
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-03-21 22:06:14 +0100

    #128: create dummy points in redux

--- website/src/planner/map/points/PointsOfInterestModal.tsx
+++ website/src/planner/map/points/PointsOfInterestModal.tsx
@@ -3,14 +3,32 @@ import Button from 'react-bootstrap/Button';
 import { FormattedMessage } from 'react-intl';
 import { useDispatch, useSelector } from 'react-redux';
 import { getContextMenuPoint, pointsActions } from '../../store/points.reducer.ts';
+import { v4 as uuidv4 } from 'uuid';
+import { PointOfInterestType } from '../../store/types.ts';
 
 export function PointsOfInterestModal() {
     const dispatch = useDispatch();
-    const hasMarkedPoint = !!useSelector(getContextMenuPoint);
+    const markedPoint = useSelector(getContextMenuPoint);
+    const hasMarkedPoint = !!markedPoint;
     const closeModal = () => {
         dispatch(pointsActions.setContextMenuPoint(undefined));
     };
-    const savePoint = () => {};
+    const savePoint = () => {
+        if (!markedPoint) {
+            return;
+        }
+        dispatch(
+            pointsActions.addPoint({
+                ...markedPoint,
+                id: uuidv4(),
+                title: 'Here',
+                description: 'Descr',
+                radiusInM: 100,
+                type: PointOfInterestType.IMPEDIMENT,
+            })
+        );
+        dispatch(pointsActions.setContextMenuPoint(undefined));
+    };
     return (
         <Modal show={hasMarkedPoint} onHide={closeModal} backdrop="static" keyboard={false}>
             <Modal.Header closeButton>
