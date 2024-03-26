294897aeae83dca5a2a2478d1519b18599125f4f
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-03-26 02:48:29 +0100

    #133: use flipped flag in assembling the information for the tracks

--- website/src/planner/logic/merge/helper/enrichGpxSegmentsWithTimeStamps.ts
+++ website/src/planner/logic/merge/helper/enrichGpxSegmentsWithTimeStamps.ts
@@ -13,7 +13,11 @@ export function enrichGpxSegmentsWithTimeStamps(
         let nextStartDate = '2020-10-10T10:00:00.000Z';
         gpxContent.tracks.forEach((track) => {
             const usedSpeed = (segmentSpeeds[segment.id] ?? 0) > 0 ? segmentSpeeds[segment.id]! : averageSpeed;
-            track.points = generateTimeData(nextStartDate, usedSpeed, track.points);
+            track.points = generateTimeData(
+                nextStartDate,
+                usedSpeed,
+                segment.flipped ? track.points.reverse() : track.points
+            );
             nextStartDate = track.points[track.points.length - 1].time.toISOString();
         });
         return {
