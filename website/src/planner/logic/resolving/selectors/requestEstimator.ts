23b2d6183d7748f811312259f38f3b1f5a9b4578
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-03-29 11:35:33 +0100

    #136: try to read current resolving status via segments

--- website/src/planner/logic/resolving/selectors/requestEstimator.ts
+++ website/src/planner/logic/resolving/selectors/requestEstimator.ts
@@ -3,16 +3,11 @@ import { State } from '../../../store/types.ts';
 import { getGpxSegments } from '../../../store/gpxSegments.reducer.ts';
 import { SimpleGPX } from '../../../../utils/SimpleGPX.ts';
 import { splitListIntoSections } from '../helper/splitPointsService.ts';
-import {
-    geoCodingRequestsActions,
-    getNumberOfRequestsDone,
-    getNumberOfRequestsRunning,
-    getNumberOfRequiredRequests,
-} from '../../../store/geoCodingRequests.reducer.ts';
+import { geoCodingRequestsActions } from '../../../store/geoCodingRequests.reducer.ts';
+import { GpxSegment } from '../../../../common/types.ts';
 
-export const estimateRequestsForStreetResolving = (dispatch: Dispatch, getState: () => State) => {
+function getNumberOfRequestsForSegments(gpxSegments: GpxSegment[]) {
     let counter = 0;
-    const gpxSegments = getGpxSegments(getState()).filter((segment) => !segment.streetsResolved);
     gpxSegments.forEach((segment) => {
         const gpx = SimpleGPX.fromString(segment.content);
         gpx.tracks.forEach((track) => {
@@ -22,23 +17,23 @@ export const estimateRequestsForStreetResolving = (dispatch: Dispatch, getState:
             });
         });
     });
+    return counter;
+}
+
+export const estimateRequestsForStreetResolving = (dispatch: Dispatch, getState: () => State) => {
+    const gpxSegments = getGpxSegments(getState()).filter((segment) => !segment.streetsResolved);
+    const counter = getNumberOfRequestsForSegments(gpxSegments);
     dispatch(geoCodingRequestsActions.setNumberOfRequiredRequests(counter));
 };
 
 export const getRequestProgress = (state: State): undefined | number => {
-    const numberOfRequiredRequests = getNumberOfRequiredRequests(state);
-    const numberOfRequestsDone = getNumberOfRequestsDone(state);
-    const numberOfRequestsRunning = getNumberOfRequestsRunning(state);
+    const gpxSegments = getGpxSegments(state);
+    const allRequests = getNumberOfRequestsForSegments(gpxSegments);
+    const resolvedRequests = getNumberOfRequestsForSegments(gpxSegments.filter((segment) => segment.streetsResolved));
 
-    if (numberOfRequestsRunning === 0 && numberOfRequiredRequests === 0) {
+    if (allRequests === 0) {
         return 100;
     }
-    if (!!numberOfRequiredRequests && numberOfRequestsDone === numberOfRequiredRequests) {
-        return 100;
-    }
-    if (numberOfRequestsRunning === 0 || !numberOfRequiredRequests) {
-        return 0;
-    }
 
-    return (numberOfRequestsDone / numberOfRequiredRequests) * 100;
+    return (resolvedRequests / allRequests) * 100;
 };
--- website/src/planner/logic/resolving/streets/mapMatchingStreetResolver.ts
+++ website/src/planner/logic/resolving/streets/mapMatchingStreetResolver.ts
@@ -6,7 +6,6 @@ import { SimpleGPX } from '../../../../utils/SimpleGPX.ts';
 import { Point } from 'gpxparser';
 import { splitListIntoSections } from '../helper/splitPointsService.ts';
 import { AppDispatch } from '../../../store/store.ts';
-import { geoCodingRequestsActions } from '../../../store/geoCodingRequests.reducer.ts';
 
 function toGeoApifyMapMatchingBody(points: Point[]): GeoApifyMapMatching {
     return {
@@ -21,20 +20,15 @@ function toGeoApifyMapMatchingBody(points: Point[]): GeoApifyMapMatching {
 export function resolveStreetNames(dispatch: AppDispatch, getState: () => State) {
     const geoApifyKey = getGeoApifyKey(getState()) || '9785fab54f7e463fa8f04543b4b9852b';
     let counter = 0;
-    dispatch(geoCodingRequestsActions.resetRequestDoneCounter());
-
     const gpxSegments = getGpxSegments(getState()).filter((segment) => !segment.streetsResolved);
 
     gpxSegments.forEach((segment) => {
         SimpleGPX.fromString(segment.content).tracks.forEach((track) => {
             splitListIntoSections(track.points, 1000).forEach((points) => {
-                dispatch(geoCodingRequestsActions.increaseActiveRequestCounter());
                 setTimeout(() => {
                     geoApifyFetchMapMatching(geoApifyKey)(toGeoApifyMapMatchingBody(points)).then(
                         (resolvedPositions) => {
                             dispatch(geoCodingActions.saveResolvedPositions(resolvedPositions));
-                            dispatch(geoCodingRequestsActions.decreaseActiveRequestCounter());
-                            dispatch(geoCodingRequestsActions.increaseRequestDoneCounter());
                         }
                     );
                 }, 5000 * counter);
