549472cf0a5c662b885b4874911d72073a5f4542
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-03-30 13:15:42 +0100

    #140: try out minHeight

--- website/src/versions/trackInfo/TrackInformationModalBody.tsx
+++ website/src/versions/trackInfo/TrackInformationModalBody.tsx
@@ -4,7 +4,7 @@ import { Button, Card, Col, Row } from 'react-bootstrap';
 import download from '../../assets/file-down.svg';
 
 const cardStyle = {
-    style: { width: '180x', height: '180px', cursor: 'pointer' },
+    style: { width: '180x', height: '180px', minWidth: '180x', minHeight: '180px', cursor: 'pointer' },
     className: 'startPageCard shadow mb-2 p-2 text-center',
 };
 
