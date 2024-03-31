b22d7f91dcc94bf6ec0ef81c556fddeed9470caf
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-03-31 09:27:35 +0200

    #141: specify tests for break situations. Breaks should not interact with node finding

--- website/src/planner/logic/merge/helper/__tests__/nodeFinder.test.ts
+++ website/src/planner/logic/merge/helper/__tests__/nodeFinder.test.ts
@@ -43,6 +43,78 @@ describe('Node finder', () => {
         expect(nodesOfTracks).toEqual(expectedTrackNodes);
     });
 
+    describe('node finding regardless of breaks', () => {
+        const getA = (segmentIds: string[]) => ({ id: '1', name: 'A', segmentIds, peopleCount: 200 });
+        const getB = (segmentIds: string[]) => ({ id: '2', name: 'B', segmentIds, peopleCount: 300 });
+
+        const break20_1 = `20${BREAK_IDENTIFIER}1`;
+        const break15_1 = `15${BREAK_IDENTIFIER}1`;
+        const break5_1 = `5${BREAK_IDENTIFIER}1`;
+        const break5_2 = `5${BREAK_IDENTIFIER}2`;
+        const testCases = [
+            ...[
+                { beforeA: [], beforeB: [] },
+                { beforeA: [break20_1], beforeB: [] },
+                { beforeA: [], beforeB: [break15_1] },
+                { beforeA: [break20_1], beforeB: [break15_1] },
+            ].flatMap(({ beforeA, beforeB }) => [
+                {
+                    description: `should find nodes when no breaks are present after merge ${beforeA} ${beforeB}`,
+                    tracks: [getA(['0', ...beforeA, '1', '3']), getB(['2', ...beforeB, '1', '3'])],
+                },
+                {
+                    description: `even if one track has a break on the shared path ${beforeA} ${beforeB}`,
+                    tracks: [getA(['0', ...beforeA, '1', break5_1, '3']), getB(['2', ...beforeB, '1', '3'])],
+                },
+                {
+                    description: `even if the other track has a break on the shared path ${beforeA} ${beforeB}`,
+                    tracks: [getA(['0', ...beforeA, '1', '3']), getB(['2', ...beforeB, '1', break5_1, '3'])],
+                },
+                {
+                    description: `even if both tracks have a break on the shared path ${beforeA} ${beforeB}`,
+                    tracks: [getA(['0', ...beforeA, '1', break5_1, '3']), getB(['2', ...beforeB, '1', break5_1, '3'])],
+                },
+                {
+                    description: `even if both tracks have a different id break on the shared path ${beforeA} ${beforeB}`,
+                    tracks: [getA(['0', ...beforeA, '1', break5_1, '3']), getB(['2', ...beforeB, '1', break5_2, '3'])],
+                },
+                {
+                    description: `even if both tracks have a different length break on the shared path ${beforeA} ${beforeB}`,
+                    tracks: [getA(['0', ...beforeA, '1', break5_1, '3']), getB(['2', ...beforeB, '1', break15_1, '3'])],
+                },
+            ]),
+        ];
+
+        testCases.forEach(({ tracks, description }) =>
+            it(description, () => {
+                // when
+                // then
+                const expectedTrackNodes: TrackNode[] = [
+                    {
+                        segmentIdAfterNode: '1',
+                        segmentsBeforeNode: [
+                            { amount: 200, segmentId: '0', trackId: '1' },
+                            { amount: 300, segmentId: '2', trackId: '2' },
+                        ],
+                    },
+                    {
+                        segmentIdAfterNode: '3',
+                        segmentsBeforeNode: [
+                            { amount: 200, segmentId: '1', trackId: '1' },
+                            { amount: 300, segmentId: '1', trackId: '2' },
+                        ],
+                    },
+                ];
+
+                // when
+                const nodesOfTracks = listAllNodesOfTracks(tracks);
+
+                // then
+                expect(nodesOfTracks).toEqual(expectedTrackNodes);
+            })
+        );
+    });
+
     it('should find two nodes, (first two branches then another)', () => {
         // given
         const trackCompositions: TrackComposition[] = [
