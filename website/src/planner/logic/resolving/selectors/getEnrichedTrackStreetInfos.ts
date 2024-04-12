b4d003b8025e981f0067dbbdd4639c5e5487fc79
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-04-12 08:37:52 +0200

    #156: filter out 0km pieces

--- website/src/planner/logic/resolving/selectors/getEnrichedTrackStreetInfos.ts
+++ website/src/planner/logic/resolving/selectors/getEnrichedTrackStreetInfos.ts
@@ -1,5 +1,5 @@
 import { getResolvedDistricts, getResolvedPostCodes, getTrackStreetInfos } from '../../../store/geoCoding.reducer.ts';
-import { TrackStreetInfo } from '../types.ts';
+import { TrackStreetInfo, TrackWayPointType } from '../types.ts';
 import { createSelector } from '@reduxjs/toolkit';
 import { getWayPointKey } from '../helper/pointKeys.ts';
 
@@ -10,14 +10,18 @@ export const getEnrichedTrackStreetInfos = createSelector(
     (trackStreetInfos, resolvedPostCodes, resolvedDistricts): TrackStreetInfo[] => {
         return trackStreetInfos.map((info) => ({
             ...info,
-            wayPoints: info.wayPoints.map((wayPoint) => {
-                const postCodeKey = getWayPointKey(wayPoint).postCodeKey;
-                return {
-                    ...wayPoint,
-                    postCode: resolvedPostCodes[postCodeKey],
-                    district: resolvedDistricts[postCodeKey],
-                };
-            }),
+            wayPoints: info.wayPoints
+                .map((wayPoint) => {
+                    const postCodeKey = getWayPointKey(wayPoint).postCodeKey;
+                    return {
+                        ...wayPoint,
+                        postCode: resolvedPostCodes[postCodeKey],
+                        district: resolvedDistricts[postCodeKey],
+                    };
+                })
+                .filter(
+                    (wayPoint) => (wayPoint.distanceInKm ?? 0) > 0.001 && wayPoint.type !== TrackWayPointType.Break
+                ),
         }));
     }
 );
