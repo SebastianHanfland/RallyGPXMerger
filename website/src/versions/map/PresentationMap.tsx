97d4b366a8aa30ea7d8c2af784b1bc1429258822
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-04-18 15:19:07 +0200

    #157: only do location on mobile browsers

--- website/src/versions/map/PresentationMap.tsx
+++ website/src/versions/map/PresentationMap.tsx
@@ -27,7 +27,7 @@ export const PresentationMap = () => {
             const noSingleScroll = { tap: !L.Browser.mobile, dragging: !L.Browser.mobile };
             myMap = L.map('mapid', isInIframe ? noSingleScroll : undefined).setView(Munich, startZoom);
             L.tileLayer(tileUrlTemplate, getOptions()).addTo(myMap);
-            if (isLive) {
+            if (isLive && L.Browser.mobile) {
                 const locate = L.control.locate({ initialZoomLevel: startZoom });
                 locate.addTo(myMap);
                 locate.start();
