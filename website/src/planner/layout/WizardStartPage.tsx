3ca7f8ed662464625f8fd5f3e3a3cd2d6f1638bc
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-04-30 10:16:34 +0200

    #160: extract card component for other start cards

--- website/src/planner/layout/WizardStartPage.tsx
+++ website/src/planner/layout/WizardStartPage.tsx
@@ -1,4 +1,4 @@
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { Sections } from './types.ts';
 import stars from '../../assets/stars.svg';
 import fileUp from '../../assets/file-up.svg';
@@ -13,13 +13,6 @@ import { AppFooter } from './Footer.tsx';
 import { importHook } from '../io/importHook.ts';
 import { FormattedMessage } from 'react-intl';
 import { WizardStartPageCard } from './WizardStartPageCard.tsx';
-
-const cardStyle = {
-    style: { minWidth: '18rem', minHeight: '30rem', cursor: 'pointer' },
-    className: 'startPageCard shadow m-2',
-};
-
-const imageStyle = { height: '10rem', width: '10rem' };
 
 export const WizardStartPage = () => {
     const dispatch = useDispatch();
@@ -42,53 +35,32 @@ export const WizardStartPage = () => {
                     />
                 </Col>
                 <Col>
-                    <Card {...cardStyle} onClick={importButtonClicked}>
-                        <Card.Body>
-                            <img
-                                src={fileUp}
-                                className="m-1"
-                                alt="open existing plan"
-                                style={imageStyle}
-                                color={'#ffffff'}
-                            />
-                            <Card.Title className={'mt-3'}>
-                                <FormattedMessage id={'msg.loadPlan'} />
-                            </Card.Title>
-                            <Card.Text style={{ minHeight: '3rem' }}>
-                                <FormattedMessage id={'msg.loadPlan.hint'} />
-                            </Card.Text>
-                            <input
-                                type="file"
-                                name="file"
-                                onChange={changeHandler}
-                                ref={uploadInput}
-                                hidden={true}
-                                accept={'application/json'}
-                            />
-                        </Card.Body>
-                    </Card>
+                    <WizardStartPageCard
+                        icon={fileUp}
+                        onClick={importButtonClicked}
+                        title={<FormattedMessage id={'msg.loadPlan'} />}
+                        text={<FormattedMessage id={'msg.loadPlan.hint'} />}
+                    />
+                    <input
+                        type="file"
+                        name="file"
+                        onChange={changeHandler}
+                        ref={uploadInput}
+                        hidden={true}
+                        accept={'application/json'}
+                    />
                 </Col>
                 <Col>
-                    <Card
-                        {...cardStyle}
+                    <WizardStartPageCard
+                        icon={info}
                         onClick={() => {
                             loadSampleData(dispatch);
                             dispatch(layoutActions.setShowDashboard(true));
                             setSelectedSection('gps');
                         }}
-                    >
-                        <Card.Body>
-                            <img
-                                src={info}
-                                className="m-1"
-                                alt="open existing plan"
-                                style={imageStyle}
-                                color={'#ffffff'}
-                            />
-                            <Card.Title className={'mt-3'}>
-                                <FormattedMessage id={'msg.loadSample'} />
-                            </Card.Title>
-                            <Card.Text style={{ minHeight: '3rem' }}>
+                        title={<FormattedMessage id={'msg.loadSample'} />}
+                        text={
+                            <>
                                 <FormattedMessage id={'msg.loadSample.hint'} />:
                                 <ul>
                                     <li>
@@ -98,33 +70,17 @@ export const WizardStartPage = () => {
                                         <FormattedMessage id={'msg.loadSample.ex2'} />:
                                     </li>
                                 </ul>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                            </>
+                        }
+                    />
                 </Col>
                 <Col>
-                    <Card
-                        {...cardStyle}
-                        onClick={() => {
-                            setSelectedSection('wizard-versions');
-                        }}
-                    >
-                        <Card.Body>
-                            <img
-                                src={versionSvg}
-                                className="m-1"
-                                alt="open existing plan"
-                                style={imageStyle}
-                                color={'#ffffff'}
-                            />
-                            <Card.Title className={'mt-3'}>
-                                <FormattedMessage id={'msg.versions'} />
-                            </Card.Title>
-                            <Card.Text style={{ minHeight: '3rem' }}>
-                                <FormattedMessage id={'msg.versions.hint'} />
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                    <WizardStartPageCard
+                        icon={versionSvg}
+                        onClick={() => setSelectedSection('wizard-versions')}
+                        title={<FormattedMessage id={'msg.versions'} />}
+                        text={<FormattedMessage id={'msg.versions.hint'} />}
+                    />
                 </Col>
             </Row>
             <DirectlyToPlannerButton />
