d0fbfaf9454dcfc833ee0497c15333b582ab98c1
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-04-30 10:50:32 +0200

    #160: Introduce empty planer of wizard

--- website/src/language.ts
+++ website/src/language.ts
@@ -1,12 +1,13 @@
 export type SupportedLanguages = 'de' | 'en';
 
-let language: SupportedLanguages = 'de';
+export const getInitialLanguage = (): SupportedLanguages => {
+    console.log('Evaluating language', window.navigator.language);
+    return window.navigator.language.includes('de') ? 'de' : 'en';
+};
+
+let language: SupportedLanguages = getInitialLanguage();
 
 export const getLanguage = () => language;
 export const setLanguage = (newLanguage: SupportedLanguages) => {
     language = newLanguage;
-};
-
-export const getInitialLanguage = (): SupportedLanguages => {
-    return window.navigator.language.includes('de') ? 'de' : 'en';
 };
