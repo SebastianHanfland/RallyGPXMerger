9c7a5e32e38fd4aa309b614b0746673611b6a61a
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-05-06 09:02:41 +0200

    #160: only show edit when there is an active planning and allow to navigate there and back

--- website/src/planner/layout/RallyPlannerRouter.tsx
+++ website/src/planner/layout/RallyPlannerRouter.tsx
@@ -3,13 +3,10 @@ import { AppFooter } from './Footer.tsx';
 import { MergeAndMap } from './MergeAndMap.tsx';
 import { AppHeader } from './Header.tsx';
 import { WizardParameters } from '../wizard/WizardParameters.tsx';
-import { useDispatch, useSelector } from 'react-redux';
-import { getSelectionSection, layoutActions } from '../store/layout.reducer.ts';
+import { useSelector } from 'react-redux';
+import { getSelectionSection } from '../store/layout.reducer.ts';
 import { WizardSegments } from '../wizard/WizardSegments.tsx';
 import { Container } from 'react-bootstrap';
-import { getArrivalDateTime } from '../store/trackMerge.reducer.ts';
-import { useEffect } from 'react';
-import { Sections } from './types.ts';
 import { WizardsComplexity } from '../wizard/WizardComplexity.tsx';
 import { WizardTracks } from '../wizard/WizardTracks.tsx';
 import { WizardVersions } from '../wizard/WizardVersions.tsx';
@@ -18,19 +15,8 @@ import { FloatingInfoButton } from '../FloatingInfoButton.tsx';
 
 export const RallyPlannerRouter = () => {
     const selectedSection = useSelector(getSelectionSection);
-    const arrivalDateTime = useSelector(getArrivalDateTime);
 
-    const dispatch = useDispatch();
-    const setSelectedSection = (section: Sections) => dispatch(layoutActions.selectSection(section));
-
-    useEffect(() => {
-        if (selectedSection === 'menu' && !!arrivalDateTime) {
-            setSelectedSection('gps');
-        }
-    }, [arrivalDateTime, selectedSection]);
-
-    const isFreshStart = selectedSection === 'menu' && !arrivalDateTime;
-    if (isFreshStart) {
+    if (selectedSection === 'menu') {
         return <WizardStartPage />;
     }
 
