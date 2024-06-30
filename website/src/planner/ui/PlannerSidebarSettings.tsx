779e3535a013b00cb5ae5a78f83afa866be886be
Sebastian Hanfland <sebastian.hanfland@dwd.de>
2024-06-30 07:43:24 +0200

    #167: Add api keys to settings

--- website/src/planner/ui/PlannerSidebarSettings.tsx
+++ website/src/planner/ui/PlannerSidebarSettings.tsx
@@ -1,9 +1,22 @@
 import { TrackMergeParameters } from '../parameters/TrackMergeParameters.tsx';
+import { ApiKeyInput } from '../streets/ApiKeyInput.tsx';
+import { Accordion } from 'react-bootstrap';
+import { FormattedMessage } from 'react-intl';
 
 export const PlannerSidebarSettings = () => {
     return (
         <div>
             <TrackMergeParameters />
+            <Accordion>
+                <Accordion.Item eventKey="0">
+                    <Accordion.Header>
+                        <FormattedMessage id={'msg.ownApiKey'} />
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        <ApiKeyInput />
+                    </Accordion.Body>
+                </Accordion.Item>
+            </Accordion>
         </div>
     );
 };
