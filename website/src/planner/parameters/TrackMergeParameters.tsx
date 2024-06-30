6bcf064d56f600ec35d35457a44b45b9cda08226
Sebastian Hanfland <sebastian.hanfland@dwd.de>
2024-06-30 06:45:40 +0200

    #167: Move label naming

--- website/src/planner/parameters/TrackMergeParameters.tsx
+++ website/src/planner/parameters/TrackMergeParameters.tsx
@@ -1,14 +1,11 @@
 import { ArrivalDateTimePicker } from './ArrivalDateTimePicker.tsx';
 import { ParticipantsDelaySetter } from './ParticipantsDelaySetter.tsx';
 import { AverageSpeedSetter } from './AverageSpeedSetter.tsx';
-import { PlanningLabel } from './PlanningLabel.tsx';
 
 export function TrackMergeParameters() {
     return (
         <div className={'text-center'}>
             <ArrivalDateTimePicker />
-            <hr />
-            <PlanningLabel />
             <hr />
             <ParticipantsDelaySetter />
             <hr />
