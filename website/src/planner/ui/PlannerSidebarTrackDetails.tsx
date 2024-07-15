3d422cc26f3531ada57cfe557fda9e8f101770d1
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-07-15 06:04:49 +0200

    #167: add other Infos also to the track screen

--- website/src/planner/ui/PlannerSidebarTrackDetails.tsx
+++ website/src/planner/ui/PlannerSidebarTrackDetails.tsx
@@ -3,15 +3,22 @@ import { FormattedMessage } from 'react-intl';
 import { TrackComposition } from '../store/types.ts';
 import { TrackSegmentSelection } from '../tracks/TrackSegmentSelection.tsx';
 import { PlannerSidebarTrackFormDetails } from './PlannerSidebarTrackFormDetails.tsx';
+import { PlannerSidebarTrackInfo } from './PlannerSidebarTrackInfo.tsx';
+import { useSelector } from 'react-redux';
+import { getEnrichedTrackStreetInfos } from '../logic/resolving/selectors/getEnrichedTrackStreetInfos.ts';
 
 export const PlannerSidebarTrackDetails = ({ track }: { track: TrackComposition }) => {
     const { name } = track;
+    const trackInfos = useSelector(getEnrichedTrackStreetInfos);
+    const matchedTrackInfo = trackInfos.find((trackInfo) => trackInfo.id === track.id);
+    const distanceInfo = matchedTrackInfo?.distanceInKm ? ` (${matchedTrackInfo.distanceInKm.toFixed(2)} km)` : '';
     return (
         <div className={'m-2'}>
             <h4>
-                <span className={'mx-2'}>{name}</span>
+                <span className={'mx-2'}>{`${name}${distanceInfo}`}</span>
                 <TrackButtonsCell track={track} />
             </h4>
+            <PlannerSidebarTrackInfo trackInfo={matchedTrackInfo} />
             <PlannerSidebarTrackFormDetails track={track} />
 
             <div style={{ width: '100%' }} className={'my-2'}>
