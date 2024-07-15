3a992bf24c8e1482013f20ba2600db2cf8b54f26
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-07-15 06:04:41 +0200

    #167: add toolip and open the sidebar when clicked on warning

--- website/src/planner/ui/DefaultArrivalDateWarning.tsx
+++ website/src/planner/ui/DefaultArrivalDateWarning.tsx
@@ -1,12 +1,13 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useIntl } from 'react-intl';
 import { getArrivalDateTime, getHasDefaultArrivalDateTime } from '../store/trackMerge.reducer.ts';
 import { CSSProperties } from 'react';
 import { formatDate } from '../../utils/dateUtil.ts';
+import { layoutActions } from '../store/layout.reducer.ts';
 
 const defaultTimeWarning: CSSProperties = {
     position: 'fixed',
-    width: '500px',
+    width: '650px',
     height: '30px',
     borderRadius: '2px',
     left: 80,
@@ -22,13 +23,21 @@ export function DefaultArrivalDateWarning() {
     const intl = useIntl();
     const hasDefaultTime = useSelector(getHasDefaultArrivalDateTime);
     const arrivalDateTime = useSelector(getArrivalDateTime);
+    const dispatch = useDispatch();
 
     if (!hasDefaultTime || !arrivalDateTime) {
         return null;
     }
 
     return (
-        <div style={defaultTimeWarning} className={'shadow'}>
+        <div
+            style={defaultTimeWarning}
+            className={'shadow'}
+            onClick={() => {
+                dispatch(layoutActions.setIsSidebarOpen(true));
+            }}
+            title={intl.formatMessage({ id: 'msg.setArrivalDateTime' })}
+        >
             {`(!) ${intl.formatMessage({ id: 'msg.defaultArrivalTime' })}: ${formatDate(arrivalDateTime)}`}
         </div>
     );
