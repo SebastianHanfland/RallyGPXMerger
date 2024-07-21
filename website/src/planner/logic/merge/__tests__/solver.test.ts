96f4c727cb2051fb6932ef62538b75473caadfb8
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-07-21 08:51:34 +0200

    #167: fix tests for now

--- website/src/planner/logic/merge/__tests__/solver.test.ts
+++ website/src/planner/logic/merge/__tests__/solver.test.ts
@@ -4,7 +4,8 @@ import { Mock } from 'vitest';
 
 import { assembleTrackFromSegments } from '../helper/assembleTrackFromSegments.ts';
 import { setParticipantsDelay } from '../../../store/trackMerge.reducer.ts';
-import { GpxSegment } from '../../../../common/types.ts';
+import { ResolvedGpxSegment } from '../../../../common/types.ts';
+import { SimpleGPX } from '../../../../utils/SimpleGPX.ts';
 
 vi.mock('../../utils/SimpleGPX.ts');
 vi.mock('../helper/assembleTrackFromSegments.ts');
@@ -17,10 +18,10 @@ describe('with Peoples Solver', () => {
     it('merge A1 and AB to A and B1 and AB to B - Shifting time based on people on track', () => {
         // given
         setParticipantsDelay(0.2);
-        const gpxSegments: GpxSegment[] = [
-            { id: '1', filename: 'A1', content: 'cA1' },
-            { id: '2', filename: 'B1', content: 'cB1' },
-            { id: '3', filename: 'AB', content: 'cAB' },
+        const gpxSegments: ResolvedGpxSegment[] = [
+            { id: '1', filename: 'A1', content: 'cA1' as unknown as SimpleGPX },
+            { id: '2', filename: 'B1', content: 'cB1' as unknown as SimpleGPX },
+            { id: '3', filename: 'AB', content: 'cAB' as unknown as SimpleGPX },
         ];
         const trackCompositions: TrackComposition[] = [
             { id: '1', name: 'A', segmentIds: ['1', '3'], peopleCount: 2000 },
@@ -54,12 +55,12 @@ describe('with Peoples Solver', () => {
     it('should merge A + B -> AB and AB + C -> ABC', () => {
         // given
         setParticipantsDelay(0.2);
-        const gpxSegments: GpxSegment[] = [
-            { id: '1', filename: 'A1', content: 'cA1' },
-            { id: '2', filename: 'B1', content: 'cB1' },
-            { id: '3', filename: 'AB', content: 'cAB' },
-            { id: '4', filename: 'ABC', content: 'cABA' },
-            { id: '5', filename: 'C1', content: 'cC1' },
+        const gpxSegments: ResolvedGpxSegment[] = [
+            { id: '1', filename: 'A1', content: 'cA1' as unknown as SimpleGPX },
+            { id: '2', filename: 'B1', content: 'cB1' as unknown as SimpleGPX },
+            { id: '3', filename: 'AB', content: 'cAB' as unknown as SimpleGPX },
+            { id: '4', filename: 'ABC', content: 'cABA' as unknown as SimpleGPX },
+            { id: '5', filename: 'C1', content: 'cC1' as unknown as SimpleGPX },
         ];
         const trackCompositions: TrackComposition[] = [
             { id: '1', name: 'A', segmentIds: ['1', '3', '4'], peopleCount: 3000 },
