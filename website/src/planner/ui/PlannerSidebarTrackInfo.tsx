3cf2156ef851456b2a7b3edbbc3ab00b5ea129b6
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-08-03 11:58:58 +0200

    #167: use a table for better display

--- website/src/planner/ui/PlannerSidebarTrackInfo.tsx
+++ website/src/planner/ui/PlannerSidebarTrackInfo.tsx
@@ -1,5 +1,5 @@
 import { TrackStreetInfo } from '../logic/resolving/types.ts';
-import { Col, Row } from 'react-bootstrap';
+import { Row } from 'react-bootstrap';
 import { formatTimeOnly, getTimeDifferenceInSeconds } from '../../utils/dateUtil.ts';
 import { useIntl } from 'react-intl';
 
@@ -16,24 +16,30 @@ export const PlannerSidebarTrackInfo = ({ trackInfo }: Props) => {
     const average = (distanceInKm / getTimeDifferenceInSeconds(arrivalFront, startFront)) * 60 * 60;
 
     const duration = (getTimeDifferenceInSeconds(arrivalBack, startFront) / 60 / 60).toFixed(1) + ' h';
+
     return (
         <div className={'mb-3'}>
             <Row>
-                <Col>
-                    {intl.formatMessage({ id: 'msg.start' })}: {formatTimeOnly(startFront)}
-                </Col>
-                <Col>
-                    {intl.formatMessage({ id: 'msg.arrival' })}: {formatTimeOnly(arrivalFront)}
-                </Col>
-                <Col>
-                    {intl.formatMessage({ id: 'msg.end' })}: {formatTimeOnly(arrivalBack)}
-                </Col>
-                <Col>
-                    {intl.formatMessage({ id: 'msg.duration' })}: {duration}
-                </Col>
-                <Col>
-                    {intl.formatMessage({ id: 'msg.averageSpeed' })}: {average.toFixed(1)} km/h
-                </Col>
+                <table>
+                    <thead>
+                        <tr>
+                            <th style={{ width: '20%' }}>{intl.formatMessage({ id: 'msg.start' })}</th>
+                            <th style={{ width: '20%' }}>{intl.formatMessage({ id: 'msg.arrival' })}</th>
+                            <th style={{ width: '20%' }}>{intl.formatMessage({ id: 'msg.end' })}</th>
+                            <th style={{ width: '20%' }}>{intl.formatMessage({ id: 'msg.duration' })}</th>
+                            <th style={{ width: '20%' }}>{intl.formatMessage({ id: 'msg.averageSpeed' })}</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{formatTimeOnly(startFront)}</td>
+                            <td>{formatTimeOnly(arrivalFront)}</td>
+                            <td>{formatTimeOnly(arrivalBack)}</td>
+                            <td>{duration}</td>
+                            <td>{average.toFixed(1)} km/h</td>
+                        </tr>
+                    </tbody>
+                </table>
             </Row>
         </div>
     );
