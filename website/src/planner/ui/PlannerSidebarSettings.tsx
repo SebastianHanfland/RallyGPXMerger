5830ea2872bcbd144d54fbc441eae2311b886b8d
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-08-03 11:38:03 +0200

    #167: slim the other settings

--- website/src/planner/ui/PlannerSidebarSettings.tsx
+++ website/src/planner/ui/PlannerSidebarSettings.tsx
@@ -5,18 +5,20 @@ import { FormattedMessage } from 'react-intl';
 import { ConstructionSites } from '../settings/ConstructionSites.tsx';
 import { GapFinderParameters } from '../parameters/GapFinderParameters.tsx';
 
-export const PlannerSidebarSettings = () => {
+export const PlannerSidebarSettings = ({ all }: { all?: boolean }) => {
     return (
         <div className={'m-2'}>
-            <Accordion defaultActiveKey={'0'}>
-                <Accordion.Item eventKey="0">
-                    <Accordion.Header>
-                        <FormattedMessage id={'msg.trackSettings'} />
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        <TrackMergeParameters />
-                    </Accordion.Body>
-                </Accordion.Item>
+            <Accordion defaultActiveKey={all ? '0' : '1'}>
+                {all && (
+                    <Accordion.Item eventKey="0">
+                        <Accordion.Header>
+                            <FormattedMessage id={'msg.trackSettings'} />
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <TrackMergeParameters />
+                        </Accordion.Body>
+                    </Accordion.Item>
+                )}
                 <Accordion.Item eventKey="1">
                     <Accordion.Header>
                         <FormattedMessage id={'msg.gapSetting'} />
@@ -33,14 +35,16 @@ export const PlannerSidebarSettings = () => {
                         <ApiKeyInput />
                     </Accordion.Body>
                 </Accordion.Item>
-                <Accordion.Item eventKey="3">
-                    <Accordion.Header>
-                        <FormattedMessage id={'msg.constructions.title'} />
-                    </Accordion.Header>
-                    <Accordion.Body>
-                        <ConstructionSites />
-                    </Accordion.Body>
-                </Accordion.Item>
+                {all && (
+                    <Accordion.Item eventKey="3">
+                        <Accordion.Header>
+                            <FormattedMessage id={'msg.constructions.title'} />
+                        </Accordion.Header>
+                        <Accordion.Body>
+                            <ConstructionSites />
+                        </Accordion.Body>
+                    </Accordion.Item>
+                )}
             </Accordion>
         </div>
     );
