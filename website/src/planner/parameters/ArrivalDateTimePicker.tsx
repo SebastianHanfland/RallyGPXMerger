a56204238315ac5c7b7cfb21f273fb76b5b08480
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-08-03 09:23:52 +0200

    #167: Make arrival date also available on display

--- website/src/planner/parameters/ArrivalDateTimePicker.tsx
+++ website/src/planner/parameters/ArrivalDateTimePicker.tsx
@@ -2,21 +2,25 @@ import DatePicker from 'react-datepicker';
 import { useDispatch, useSelector } from 'react-redux';
 import { getArrivalDateTime, trackMergeActions } from '../store/trackMerge.reducer.ts';
 import 'react-datepicker/dist/react-datepicker.css';
-import { FormattedMessage } from 'react-intl';
+import { FormattedMessage, useIntl } from 'react-intl';
 import { triggerAutomaticCalculation } from '../logic/automaticCalculation.ts';
 import { AppDispatch } from '../store/store.ts';
 
-export function ArrivalDateTimePicker() {
+export function ArrivalDateTimePicker({ noHeader }: { noHeader?: boolean }) {
+    const intl = useIntl();
     const dispatch: AppDispatch = useDispatch();
     const arrivalDateTime = useSelector(getArrivalDateTime);
     return (
         <div className={'d-inline-block'}>
-            <h5 className="form-label m-3">
-                <FormattedMessage id={'msg.arrivalDate.title'} />
-            </h5>
+            {!noHeader && (
+                <h5 className="form-label m-3">
+                    <FormattedMessage id={'msg.arrivalDate.title'} />
+                </h5>
+            )}
             <DatePicker
                 className={'form-control'}
                 dateFormat={'dd.MM.yyyy HH:mm'}
+                placeholderText={intl.formatMessage({ id: 'msg.arrivalDate.title' })}
                 onChange={(date) => {
                     dispatch(trackMergeActions.setArrivalDateTime(date?.toISOString() ?? undefined));
                     dispatch(triggerAutomaticCalculation);
--- website/src/planner/ui/SimpleFileUploadSection.tsx
+++ website/src/planner/ui/SimpleFileUploadSection.tsx
@@ -1,10 +1,9 @@
 import { SimpleGpxSegments } from './SimpleGpxSegments.tsx';
 import { PlannerSidebarTrackInfo } from './PlannerSidebarTrackInfo.tsx';
-import { PlannerSidebarTrackFormDetails } from './PlannerSidebarTrackFormDetails.tsx';
 import { useDispatch, useSelector } from 'react-redux';
 import { getTrackCompositions } from '../store/trackMerge.reducer.ts';
 import { getEnrichedTrackStreetInfos } from '../logic/resolving/selectors/getEnrichedTrackStreetInfos.ts';
-import { Button } from 'react-bootstrap';
+import { Button, Col, Row } from 'react-bootstrap';
 import { downloadSinglePdfFiles } from '../streets/StreetFilesPdfMakeDownloader.tsx';
 import download from '../../assets/file-down.svg';
 import { useIntl } from 'react-intl';
@@ -12,6 +11,9 @@ import { AppDispatch } from '../store/store.ts';
 import { FileDownloader } from '../segments/FileDownloader.tsx';
 import { getCalculatedTracks } from '../store/calculatedTracks.reducer.ts';
 import { ExportStateJson } from '../io/ExportStateJson.tsx';
+import { TrackName } from './TrackName.tsx';
+import { ArrivalDateTimePicker } from '../parameters/ArrivalDateTimePicker.tsx';
+import { TrackPeople } from './TrackPeople.tsx';
 
 export function SimpleFileUploadSection() {
     const intl = useIntl();
@@ -28,25 +30,53 @@ export function SimpleFileUploadSection() {
     return (
         <div className={'p-2 shadow'} style={{ height: '95%', overflow: 'auto' }}>
             <h4>
-                <span className={'mx-2'}>{`${track.name}${distanceInfo}`}</span>
+                <Row>
+                    <Col>
+                        <span style={{ width: '300px' }}>
+                            <TrackName track={track} />
+                        </span>
+                    </Col>
+                    <Col>
+                        <span className={'mx-2'}>{`${distanceInfo}`}</span>
+                    </Col>
+                </Row>
             </h4>
-            <Button size={'sm'} className={'m-1'} onClick={() => dispatch(downloadSinglePdfFiles(intl, track.id))}>
-                <img src={download} alt="download file" className={'m-1'} color={'#ffffff'} />
-                PDF
-            </Button>
-            {calculatedTracks.length > 0 && (
-                <FileDownloader
-                    name={`${calculatedTracks[0].filename}.gpx`}
-                    content={calculatedTracks[0].content}
-                    id={calculatedTracks[0].id}
-                    label={'GPX'}
-                    onlyIcon={true}
-                    size={'sm'}
-                />
-            )}
-            <ExportStateJson label={intl.formatMessage({ id: 'msg.downloadPlanning' })} />
+            <Row>
+                <Col>
+                    <Button
+                        size={'sm'}
+                        className={'m-1'}
+                        onClick={() => dispatch(downloadSinglePdfFiles(intl, track.id))}
+                    >
+                        <img src={download} alt="download file" className={'m-1'} color={'#ffffff'} />
+                        PDF
+                    </Button>
+                </Col>
+                {calculatedTracks.length > 0 && (
+                    <Col>
+                        <FileDownloader
+                            name={`${calculatedTracks[0].filename}.gpx`}
+                            content={calculatedTracks[0].content}
+                            id={calculatedTracks[0].id}
+                            label={'GPX'}
+                            onlyIcon={true}
+                            size={'sm'}
+                        />
+                    </Col>
+                )}
+                <Col>
+                    <ExportStateJson label={intl.formatMessage({ id: 'msg.downloadPlanning' })} />
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <TrackPeople track={track} />
+                </Col>
+                <Col>
+                    <ArrivalDateTimePicker noHeader={true} />
+                </Col>
+            </Row>
             <PlannerSidebarTrackInfo trackInfo={matchedTrackInfo} />
-            <PlannerSidebarTrackFormDetails track={track} />
             <SimpleGpxSegments />
         </div>
     );
