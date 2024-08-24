cb624f0240e4d16d53369e94270e82de4f780345
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-08-24 15:06:37 +0200

    #168: have a minimal FE part of the storing of a planning

--- website/src/planner/ui/PlannerSidebar.tsx
+++ website/src/planner/ui/PlannerSidebar.tsx
@@ -7,6 +7,7 @@ import { PlannerSidebarSimpleNavigation } from './PlannerSidebarSimpleNavigation
 import { useDispatch, useSelector } from 'react-redux';
 import { getHasSingleTrack, getIsSidebarOpen, layoutActions } from '../store/layout.reducer.ts';
 import { ClosePlannerSidebar } from './ClosePlannerSidebar.tsx';
+import PocketBase from 'pocketbase';
 
 const getStyle = (showSidebar: boolean): CSSProperties => ({
     position: 'fixed',
@@ -43,20 +44,51 @@ function Content() {
     const hasSingleTrack = useSelector(getHasSingleTrack);
     return hasSingleTrack ? <SimpleContent /> : <ComplexContent />;
 }
+const pb = new PocketBase('http://127.0.0.1:8090');
 
 export function PlannerSidebar() {
     const showSidebar = useSelector(getIsSidebarOpen);
     const dispatch = useDispatch();
+    // const [password, setPassword] = useState('');
 
+    const id = 'abcabcabcabcabc';
+    const pw = 'pw';
+
+    const hash = (a: string, seed: string) => a;
+
+    const saveData = () => {
+        pb.collection('planning')
+            .create({ id, data: { a: 'a' } })
+            .then((planning) => {
+                pb.collection('permissions').create({
+                    planning_id: planning.id,
+                    password_hash: hash(pw, planning.id),
+                });
+            });
+    };
+    const updateData = () => {
+        pb.collection('planning').update(id, { data: { b: 'b' }, password_hash: hash(pw, id) });
+    };
+    const deleteData = () => {
+        pb.collection('planning').delete(id, { password_hash: hash(pw, id) });
+    };
+    const getData = () => {
+        pb.collection('planning').getOne(id);
+    };
     return (
         <>
             <ClosePlannerSidebar />
+
             <div
                 style={getStyle(showSidebar)}
                 className={'shadow'}
                 onClick={() => dispatch(layoutActions.setIsSidebarOpen(true))}
                 title={showSidebar ? undefined : 'See overview'}
             >
+                <button onClick={saveData}>Save</button>
+                <button onClick={updateData}>update</button>
+                <button onClick={deleteData}>delete</button>
+                <button onClick={getData}>get</button>
                 {showSidebar ? (
                     <div className={'mb-5'} style={{ height: '100vh' }}>
                         <Content />
