eab5f03dd086cf8d01fe23c3c2cf81570ebde135
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-08-27 12:49:46 +0200

    #168: use a simple sha256 hash to not store passwords

--- website/src/api/api.ts
+++ website/src/api/api.ts
@@ -1,10 +1,10 @@
 import PocketBase from 'pocketbase';
 import { State } from '../planner/store/types.ts';
+import { sha256 } from 'js-sha256';
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
-// TODO bcrypt
-const hash = (a: string, seed: string) => a + seed;
+const applySimpleHash = (a: string, salt: string) => sha256(salt + a);
 
 export const createPlanning = (data: State, password: string): Promise<string> => {
     return pb
@@ -13,16 +13,16 @@ export const createPlanning = (data: State, password: string): Promise<string> =
         .then((planning) => {
             pb.collection('permissions').create({
                 planning_id: planning.id,
-                password_hash: hash(password, planning.id),
+                password_hash: applySimpleHash(password, planning.id),
             });
             return planning.id;
         });
 };
 export const updatePlanning = (id: string, data: State, password: string) => {
-    pb.collection('planning').update(id, { data, password_hash: hash(password, id) });
+    pb.collection('planning').update(id, { data, password_hash: applySimpleHash(password, id) });
 };
 export const deletePlanning = (id: string, password: string) => {
-    pb.collection('planning').delete(id, { password_hash: hash(password, id) });
+    pb.collection('planning').delete(id, { password_hash: applySimpleHash(password, id) });
 };
 export const getData = (id: string) => {
     return pb.collection('planning').getOne(id);
