5da28b5d0d923912381bbcd4b8ba4aaa125abff7
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-11-22 17:15:16 +0100

    #171: calculate when custom speed is changed

--- website/src/planner/settings/SegmentSpeedCells.tsx
+++ website/src/planner/settings/SegmentSpeedCells.tsx
@@ -5,11 +5,13 @@ import { getSegmentSpeeds, gpxSegmentsActions } from '../store/gpxSegments.reduc
 import { getCount } from '../../utils/inputUtil.ts';
 import { GpxSegment } from '../../common/types.ts';
 import { useIntl } from 'react-intl';
+import { triggerAutomaticCalculation } from '../logic/automaticCalculation.ts';
+import { AppDispatch } from '../store/store.ts';
 
 export function SegmentSpeedCells({ gpxSegment }: { gpxSegment: GpxSegment }) {
     const intl = useIntl();
     const { id } = gpxSegment;
-    const dispatch = useDispatch();
+    const dispatch: AppDispatch = useDispatch();
     const averageSpeed = useSelector(getAverageSpeedInKmH);
     const segmentSpeeds = useSelector(getSegmentSpeeds);
 
@@ -30,7 +32,10 @@ export function SegmentSpeedCells({ gpxSegment }: { gpxSegment: GpxSegment }) {
                     placeholder={intl.formatMessage({ id: 'msg.customSpeed.placeholder' })}
                     title={intl.formatMessage({ id: 'msg.customSpeed.placeholder' })}
                     value={segmentSpeed?.toString() ?? ''}
-                    onChange={(value) => dispatch(gpxSegmentsActions.setSegmentSpeeds({ id, speed: getCount(value) }))}
+                    onChange={(value) => {
+                        dispatch(gpxSegmentsActions.setSegmentSpeeds({ id, speed: getCount(value) }));
+                        dispatch(triggerAutomaticCalculation);
+                    }}
                 />
             </td>
         </>
