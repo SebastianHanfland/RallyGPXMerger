abca7e1d8567ca9b7acc856f7906af13db971206
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-11-23 10:18:42 +0100

    #173: allow to switch between live and plan

--- website/src/versions/trackInfo/TrackInformationModalButton.tsx
+++ website/src/versions/trackInfo/TrackInformationModalButton.tsx
@@ -1,10 +1,12 @@
 import Button from 'react-bootstrap/Button';
 import { FormattedMessage } from 'react-intl';
-import { useDispatch } from 'react-redux';
-import { mapActions } from '../store/map.reducer.ts';
+import { useDispatch, useSelector } from 'react-redux';
+import { getIsLive, mapActions } from '../store/map.reducer.ts';
+import { setStartAndEndTime } from '../data/loadFilesHook.ts';
 
 export function TrackInformationModalButton() {
     const dispatch = useDispatch();
+    const isLive = useSelector(getIsLive);
 
     const setShowModal = (value: boolean) => dispatch(mapActions.setShowTrackInfo(value));
 
@@ -12,6 +14,18 @@ export function TrackInformationModalButton() {
         <>
             <Button onClick={() => setShowModal(true)}>
                 <FormattedMessage id={'msg.trackInfoShort'} />
+            </Button>
+            <Button
+                onClick={() => {
+                    if (isLive) {
+                        setStartAndEndTime(dispatch);
+                    }
+                    dispatch(mapActions.setIsLive(!isLive));
+                }}
+                variant={'info'}
+                style={{ marginLeft: '10px' }}
+            >
+                <FormattedMessage id={isLive ? 'msg.showPlan' : 'msg.showLive'} />
             </Button>
         </>
     );
