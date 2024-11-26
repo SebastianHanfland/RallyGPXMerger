8a3e33f4db7f55c214fc78250e44a1fee1f1d02e
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-11-26 22:17:14 +0100

    #180: make map less blurry

--- website/src/common/mapConfig.ts
+++ website/src/common/mapConfig.ts
@@ -1,3 +1,5 @@
+import { TileLayerOptions } from 'leaflet';
+
 interface Config {
     tileUrlTemplate: string;
     maxZoom?: number;
@@ -10,20 +12,20 @@ const generalConfig: Config = {
     tileUrlTemplate: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
     maxZoom: 20,
     minZoom: 2,
-    zoomOffset: -1,
+    zoomOffset: 0,
     startZoom: 10,
 };
 
 export function getMapConfiguration() {
     const { tileUrlTemplate, maxZoom, minZoom, zoomOffset, startZoom } = generalConfig;
 
-    function getOptions() {
+    function getOptions(): TileLayerOptions {
         return {
             attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
             maxZoom,
             minZoom,
             id: 'mapbox/streets-v11',
-            tileSize: 512,
+            tileSize: 256,
             zoomOffset,
         };
     }
