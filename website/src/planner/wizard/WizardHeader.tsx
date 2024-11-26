aceeafa039931a60fa62fb24a3a9ee84d4358864
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-11-26 21:25:06 +0100

    #180: add margin at top

--- website/src/planner/wizard/WizardHeader.tsx
+++ website/src/planner/wizard/WizardHeader.tsx
@@ -2,7 +2,7 @@ import { FormattedMessage } from 'react-intl';
 
 export function WizardHeader() {
     return (
-        <h3 className={'mb-5'}>
+        <h3 className={'my-5'}>
             <FormattedMessage id={'msg.appName'} />
         </h3>
     );
