89b98874a3d6ba7877bae5410beb0a404ca06dc2
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-11-30 09:39:24 +0100

    #186: fix build

--- website/src/planner/ui/MainPlannerUi.tsx
+++ website/src/planner/ui/MainPlannerUi.tsx
@@ -6,13 +6,10 @@ import { PlannerHomeButton } from './PlannerHomeButton.tsx';
 import { TimeSlider } from '../map/TimeSlider.tsx';
 import { CalculationIsRunning } from './CalculationIsRunning.tsx';
 import { CalculationOnTheFly } from './CalculationOnTheFly.tsx';
-import { useSelector } from 'react-redux';
-import { isPlanningInProgress } from '../store/planner.selector.ts';
 import { SharePlanningButton } from '../layout/SharePlanningButton.tsx';
 import { HelpingTip } from './HelpingTip.tsx';
 
 export const MainPlannerUi = () => {
-    const planningInProgress = useSelector(isPlanningInProgress);
     return (
         <>
             <div className={'canvas-wrapper'} style={{ left: 0, position: 'fixed', overflow: 'auto' }}>
