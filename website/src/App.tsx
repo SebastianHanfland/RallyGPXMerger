b88b9f8d5113870b54fd2f4426338531072eac3e
Sebastian Hanfland <sebastian.hanfland@rwth-aachen.de>
2024-12-03 16:55:46 +0100

    #181: copy map and introduce it when comparison is in the url

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -6,15 +6,20 @@ import { RallyVersionControl } from './versions/RallyVersionControl.tsx';
 import { RallyPlannerWrapper } from './planner/RallyPlanner.tsx';
 import { RallyDisplayWrapper } from './versions/RallyDisplayWrapper.tsx';
 import { RallyTableWrapper } from './versions/RallyTableWrapper.tsx';
+import { RallyComparison } from './versions/RallyComparison.tsx';
 
 export function App() {
     const urlParams = window.location.search;
     const hasExternalUrl = urlParams.includes('version=');
+    const hasComparisonUrl = urlParams.includes('comparison=');
     const hasDisplayUrl = urlParams.includes('display=');
     const hasTableUrl = urlParams.includes('table=');
 
     if (hasExternalUrl) {
         return <ErrorBoundary>{<RallyVersionControl />}</ErrorBoundary>;
+    }
+    if (hasComparisonUrl) {
+        return <ErrorBoundary>{<RallyComparison />}</ErrorBoundary>;
     }
     if (hasTableUrl) {
         return <ErrorBoundary>{<RallyTableWrapper />}</ErrorBoundary>;
