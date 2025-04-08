ee4b68f6996b19d56467e565daee92fe34468907
sebastian <sebastian.hanfland@rwth-aachen.de>
2025-04-08 16:41:29 +0200

    #200: use alert instead of a bad way to access the redux store

--- website/src/planner/logic/merge/helper/enrichGpxSegmentsWithTimeStamps.ts
+++ website/src/planner/logic/merge/helper/enrichGpxSegmentsWithTimeStamps.ts
@@ -2,8 +2,6 @@ import { generateTimeData } from '../speedSimulator.ts';
 
 import { GpxSegment, ResolvedGpxSegment } from '../../../../common/types.ts';
 import { getGpx } from '../../../../common/cache/gpxCache.ts';
-import { errorNotification } from '../../../store/toast.reducer.ts';
-import { store } from '../../../store/store.ts';
 
 export function enrichGpxSegmentsWithTimeStamps(
     gpxSegments: GpxSegment[],
@@ -28,10 +26,7 @@ export function enrichGpxSegmentsWithTimeStamps(
                 gpxContent.setEnd(end);
                 nextStartDate = track.points[track.points.length - 1].time.toISOString();
             } else {
-                errorNotification(
-                    store.dispatch,
-                    `Segment file with unfilled track: "${segment.filename}" (empty track: "${track.name}")`
-                );
+                alert(`Segment file with unfilled track: "${segment.filename}" (empty track: "${track.name}")`);
             }
         });
         return {
