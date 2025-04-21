568a0551839def6c761f825b72d0ac5f44476fee
sebastian <sebastian.hanfland@rwth-aachen.de>
2025-04-21 16:05:47 +0200

    #187: trying to make the table work

--- website/test/integration/TableDisplay.test.tsx
+++ website/test/integration/TableDisplay.test.tsx
@@ -1,15 +1,36 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router';
+import { RallyTableWrapper } from '../../src/versions/RallyTableWrapper';
+import { Mock, vi } from 'vitest';
+import { useGetUrlParam } from '../../src/utils/linkUtil';
+import { getLanguage } from '../../src/language';
+import { getData } from '../../src/api/api';
+import * as fs from 'node:fs';
+import { State } from '../../src/planner/store/types';
 
 export const CCom = () => {
     return <div>Dummy</div>;
 };
 
+vi.mock('../../src/utils/linkUtil');
+vi.mock('../../src/language');
+vi.mock('../../src/api/api');
+
+
 
 describe('dummy render test', () => {
     it('should find a node', () => {
         // given
-        render(<CCom></CCom>);
+        const buffer = '' + fs.readFileSync('./public/RideOfSilence2024.json');
+        const state = JSON.parse(buffer) as State;
 
-        screen.getByText('Dummy')
+
+        (getLanguage as Mock).mockImplementation(() => 'en');
+        (useGetUrlParam as Mock).mockImplementation(() => 'planning-id');
+        (getData as Mock).mockResolvedValue({data: state});
+        act(() => render(<BrowserRouter><RallyTableWrapper/></BrowserRouter>));
+
+        screen.getByRole('wtf')
+        screen.getByText('Loading')
     });
 });
