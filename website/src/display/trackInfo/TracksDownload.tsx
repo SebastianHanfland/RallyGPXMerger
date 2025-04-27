be18c90099dd00b922213a6128efb936fb657886
sebastian <sebastian.hanfland@rwth-aachen.de>
2025-04-27 06:19:05 +0200

    #187: resolve one todo, now easily to access

--- website/src/display/trackInfo/TracksDownload.tsx
+++ website/src/display/trackInfo/TracksDownload.tsx
@@ -4,17 +4,19 @@ import { Button } from 'react-bootstrap';
 import download from '../../assets/file-down.svg';
 import { downloadFilesInZip } from '../../planner/tracks/CalculatedFilesDownloader.tsx';
 import { getDisplayTracks } from '../store/displayTracksReducer.ts';
+import { getPlanningTitle } from '../../planner/store/trackMerge.reducer.ts';
 
 export const ZipFilesDownloader = () => {
     const intl = useIntl();
     const displayTracks = useSelector(getDisplayTracks);
+    const planningTitle = useSelector(getPlanningTitle);
+
     if (!displayTracks) {
         return null;
     }
     return (
         <Button
-            // TODO-187: planning title here
-            onClick={() => downloadFilesInZip(displayTracks, 'Sternfahrt-Muenchen-2024')}
+            onClick={() => downloadFilesInZip(displayTracks, planningTitle ?? 'Demonstration')}
             disabled={displayTracks.length === 0}
             title={intl.formatMessage({ id: 'msg.downloadTracks.hint' })}
         >
