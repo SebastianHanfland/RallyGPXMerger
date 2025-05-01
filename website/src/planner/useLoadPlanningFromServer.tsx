420f6079fa4e1a5562fd70a09f1be906d956e90d
sebastian <sebastian.hanfland@rwth-aachen.de>
2025-05-01 14:12:26 +0200

    #187: fill the parsed tracks initially

--- website/src/planner/useLoadPlanningFromServer.tsx
+++ website/src/planner/useLoadPlanningFromServer.tsx
@@ -5,8 +5,53 @@ import { useEffect } from 'react';
 import { getData } from '../api/api.ts';
 import { backendActions, getPlanningPassword } from './store/backend.reducer.ts';
 import { errorNotification, successNotification, toastsActions } from './store/toast.reducer.ts';
-import { calculateTracks } from './logic/automaticCalculation.ts';
 import { AppDispatch } from './store/planningStore.ts';
+import { parsedTracksActions } from './store/parsedTracks.reducer.ts';
+import { getCalculatedTracks } from './store/calculatedTracks.reducer.ts';
+import { ParsedTrack } from '../common/types.ts';
+import { getColorFromUuid } from '../utils/colorUtil.ts';
+import { SimpleGPX } from '../utils/SimpleGPX.ts';
+import { optionallyDecompress } from './store/compressHelper.ts';
+import { getConstructionSegments, getGpxSegments } from './store/gpxSegments.reducer.ts';
+import { Dispatch } from '@reduxjs/toolkit';
+import { State } from './store/types.ts';
+
+function createAndStoreReadablePoints(state: State, planningId: string, dispatch: Dispatch) {
+    const calculatedTracks = getCalculatedTracks(state);
+    const gpxSegments = getGpxSegments(state);
+    const constructionSegments = getConstructionSegments(state);
+    const parsedTracks = calculatedTracks.map(
+        (track): ParsedTrack => ({
+            id: track.id,
+            filename: track.filename,
+            peopleCount: track.peopleCount,
+            version: planningId,
+            color: getColorFromUuid(track.id),
+            points: SimpleGPX.fromString(optionallyDecompress(track.content)).getPoints(),
+        })
+    );
+    const parsedSegments = gpxSegments.map(
+        (segment): ParsedTrack => ({
+            id: segment.id,
+            filename: segment.filename,
+            version: planningId,
+            color: getColorFromUuid(segment.id),
+            points: SimpleGPX.fromString(optionallyDecompress(segment.content)).getPoints(),
+        })
+    );
+    const parsedConstructionSegments = constructionSegments?.map(
+        (segment): ParsedTrack => ({
+            id: segment.id,
+            filename: segment.filename,
+            version: planningId,
+            color: 'red',
+            points: SimpleGPX.fromString(optionallyDecompress(segment.content)).getPoints(),
+        })
+    );
+    dispatch(parsedTracksActions.setParsedTracks(parsedTracks));
+    dispatch(parsedTracksActions.setParsedSegments(parsedSegments));
+    dispatch(parsedTracksActions.addParsedConstructionSegments(parsedConstructionSegments ?? []));
+}
 
 export function useLoadPlanningFromServer() {
     const planningId = useGetUrlParam('planning=');
@@ -19,7 +64,9 @@ export function useLoadPlanningFromServer() {
         if (planningId) {
             getData(planningId)
                 .then((data) => {
-                    dispatch({ payload: data.data, type: 'wholeState' });
+                    const state = data.data;
+                    dispatch({ payload: state, type: 'wholeState' });
+
                     dispatch(backendActions.setPlanningId(planningId));
                     dispatch(toastsActions.clearToasts());
                     const passwordToSet = adminToken ?? planningPassword;
@@ -32,7 +79,7 @@ export function useLoadPlanningFromServer() {
                         intl.formatMessage({ id: 'msg.dataLoad.success.title' }),
                         intl.formatMessage({ id: 'msg.dataLoad.success.message' })
                     );
-                    dispatch(calculateTracks(false));
+                    createAndStoreReadablePoints(state, planningId, dispatch);
                 })
                 .catch(() => {
                     errorNotification(
