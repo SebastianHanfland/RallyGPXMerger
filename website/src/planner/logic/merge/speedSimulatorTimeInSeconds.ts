7b40784002a2bff2c4dfbe394e6f63d2a456b1d0
sebastian <sebastian.hanfland@rwth-aachen.de>
2025-05-31 20:07:42 +0200

    #223: add timing information to parsed gpx

---
+++ website/src/planner/logic/merge/speedSimulatorTimeInSeconds.ts
@@ -0,0 +1,65 @@
+import geoDistance from 'geo-distance-helper';
+import { ParsedPoint } from '../../new-store/types.ts';
+
+function slopeFactor(slope: number): number {
+    const max_slope = 100;
+    slope = Math.max(-max_slope, Math.min(max_slope, slope));
+
+    if (slope < -30) {
+        return 1.5;
+    } else if (slope < 0) {
+        return 1 + ((2 * 0.7) / 13) * slope + (0.7 / Math.pow(13, 2)) * Math.pow(slope, 2);
+    } else if (slope <= 20) {
+        return 1 + Math.pow(slope / 7, 2);
+    } else {
+        return 10;
+    }
+}
+
+function getLatLngFromParsedPoint(b: Omit<ParsedPoint, 't'>) {
+    return { lat: b.b, lng: b.l };
+}
+
+export function generateParsedPointsWithTimeInSeconds(avg: number, points: Omit<ParsedPoint, 't'>[]): ParsedPoint[] {
+    const alpha = 0.15;
+    let last_speed = avg;
+    let previousPoint: ParsedPoint | undefined = undefined;
+
+    return points.map((point, index, points) => {
+        if (index === 0) {
+            previousPoint = { ...point, t: 0 };
+            return { ...point, t: 0 };
+        }
+        const a = previousPoint as ParsedPoint;
+        const b = points[index];
+        const distInKm = geoDistance(getLatLngFromParsedPoint(b), getLatLngFromParsedPoint(a)) as number;
+
+        const slope = distInKm == 0 ? 0 : ((b.e - a.e) / (1000 * distInKm)) * 100;
+        const slope_factor = slopeFactor(slope);
+
+        const speed = alpha * (avg / slope_factor) + (1 - alpha) * last_speed;
+        const time = a.t + (60 * 60 * distInKm) / speed;
+        last_speed = speed;
+
+        previousPoint = { ...point, t: time };
+        return { ...point, t: time };
+    });
+}
+
+export function calculateSpeed(points: ParsedPoint[]): number {
+    const startPoint = { ...points[0] };
+    const endPoint = { ...points[points.length - 1] };
+
+    let distance = 0;
+    let lastPoint: ParsedPoint | null = null;
+    points.forEach((point) => {
+        if (lastPoint) {
+            distance += geoDistance(getLatLngFromParsedPoint(lastPoint), getLatLngFromParsedPoint(point)) as number;
+        }
+        lastPoint = point;
+    });
+
+    const timeInHours = (endPoint.t - startPoint.t) / 60 / 60;
+
+    return distance / timeInHours;
+}
--- website/src/planner/segments/segmentParsing.ts
+++ website/src/planner/segments/segmentParsing.ts
@@ -1,19 +1,20 @@
 import { ParsedGpxSegment, ParsedPoint } from '../new-store/types.ts';
 import { v4 as uuidv4 } from 'uuid';
 import { SimpleGPX } from '../../utils/SimpleGPX.ts';
+import { generateParsedPointsWithTimeInSeconds } from '../logic/merge/speedSimulatorTimeInSeconds.ts';
 
-function getPoints(gpxString: string): ParsedPoint[] {
+function getPoints(gpxString: string, averageSpeed: number): ParsedPoint[] {
     const points = SimpleGPX.fromString(gpxString).getPoints();
-    // TODO #223: resolve time and resolve streets
-    return points.map((point) => ({ l: point.lon, b: point.lat, e: point.ele, s: -1, t: 0 }));
+    const parsedPoints = points.map((point) => ({ l: point.lon, b: point.lat, e: point.ele, s: -1, t: 0 }));
+    return generateParsedPointsWithTimeInSeconds(averageSpeed, parsedPoints);
 }
 
-export async function toParsedGpxSegment(file: File): Promise<ParsedGpxSegment> {
+export async function toParsedGpxSegment(file: File, averageSpeed: number): Promise<ParsedGpxSegment> {
     return file.arrayBuffer().then((buffer) => ({
         id: uuidv4(),
         filename: file.name.replace('.gpx', ''),
         flipped: false,
         streetsResolved: false,
-        points: getPoints(new TextDecoder().decode(buffer)),
+        points: getPoints(new TextDecoder().decode(buffer), averageSpeed),
     }));
 }
