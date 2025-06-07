09ebb00e8c8a1816bff35ea38bdf0655b162df56
sebastian <sebastian.hanfland@rwth-aachen.de>
2025-06-07 13:47:31 +0200

    #223: actually implement logic for creating the lookups

--- website/src/planner/logic/resolving/street-new/enrichSegmentWithResolvedStreets.ts
+++ website/src/planner/logic/resolving/street-new/enrichSegmentWithResolvedStreets.ts
@@ -1,15 +1,32 @@
 import { ParsedGpxSegment, ResolvedPositions } from '../../../new-store/types.ts';
+import { toKey } from '../helper/pointKeys.ts';
 
 export function enrichSegmentWithResolvedStreets(
     segmentWithoutStreets: ParsedGpxSegment,
     allResolvedStreetNames: ResolvedPositions,
     streetResolveStart: number
 ): { segment: ParsedGpxSegment; streetLookUp: Record<number, string> } {
+    let indexCounter = streetResolveStart;
+    const streetLookUp: Record<number, string> = {};
+
     const segment = {
         ...segmentWithoutStreets,
         streetsResolved: true,
-        points: segmentWithoutStreets.points.map((point) => ({ ...point, s: streetResolveStart })),
+        points: segmentWithoutStreets.points.map((point) => {
+            const key = toKey({ lat: point.b, lon: point.l });
+            const resolvedStreetName = allResolvedStreetNames[key];
+            if (!resolvedStreetName) {
+                return point;
+            }
+            const foundInLookup = Object.entries(streetLookUp).find((entry) => entry[1] === resolvedStreetName);
+            if (foundInLookup) {
+                return { ...point, s: Number(foundInLookup[0]) };
+            } else {
+                indexCounter += 1;
+                streetLookUp[indexCounter] = resolvedStreetName;
+                return { ...point, s: indexCounter };
+            }
+        }),
     };
-    const streetLookUp = { [streetResolveStart]: Object.values(allResolvedStreetNames)[0] ?? '' };
     return { segment, streetLookUp: streetLookUp };
 }
