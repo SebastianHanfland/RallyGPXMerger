70472f0dbc32ca4ab552477e3d9622261277f7fe
sebastian <sebastian.hanfland@rwth-aachen.de>
2025-06-08 16:30:39 +0200

    #223: remove creation of parsed tracks because it is not necessary anymore

--- website/src/planner/useLoadPlanningFromServer.tsx
+++ website/src/planner/useLoadPlanningFromServer.tsx
@@ -7,41 +7,17 @@ import { backendActions, getPlanningPassword } from './store/backend.reducer.ts'
 import { errorNotification, successNotification, toastsActions } from './store/toast.reducer.ts';
 import { AppDispatch } from './store/planningStore.ts';
 import { parsedTracksActions } from './store/parsedTracks.reducer.ts';
-import { getCalculatedTracks } from './store/calculatedTracks.reducer.ts';
 import { ParsedTrack } from '../common/types.ts';
-import { getColorFromUuid } from '../utils/colorUtil.ts';
 import { SimpleGPX } from '../utils/SimpleGPX.ts';
 import { optionallyDecompress } from './store/compressHelper.ts';
-import { getConstructionSegments, getGpxSegments } from './store/gpxSegments.reducer.ts';
+import { getConstructionSegments } from './store/gpxSegments.reducer.ts';
 import { Dispatch } from '@reduxjs/toolkit';
 import { State } from './store/types.ts';
 import { trackMergeActions } from './store/trackMerge.reducer.ts';
 
 export const createAndStoreReadablePoints = (planningId: string) => (dispatch: Dispatch, getState: () => State) => {
     const state = getState();
-    const calculatedTracks = getCalculatedTracks(state);
-    const gpxSegments = getGpxSegments(state);
     const constructionSegments = getConstructionSegments(state);
-    const parsedTracks = calculatedTracks.map(
-        (track): ParsedTrack => ({
-            id: track.id,
-            filename: track.filename,
-            peopleCount: track.peopleCount,
-            version: planningId,
-            color: getColorFromUuid(track.id),
-            points: SimpleGPX.fromString(optionallyDecompress(track.content)).getPoints(),
-        })
-    );
-    const parsedSegments = gpxSegments.map((segment): ParsedTrack => {
-        const points = SimpleGPX.fromString(optionallyDecompress(segment.content)).getPoints();
-        return {
-            id: segment.id,
-            filename: segment.filename,
-            version: planningId,
-            color: getColorFromUuid(segment.id),
-            points: segment.flipped ? points.reverse() : points,
-        };
-    });
     const parsedConstructionSegments = constructionSegments?.map(
         (segment): ParsedTrack => ({
             id: segment.id,
@@ -51,8 +27,6 @@ export const createAndStoreReadablePoints = (planningId: string) => (dispatch: D
             points: SimpleGPX.fromString(optionallyDecompress(segment.content)).getPoints(),
         })
     );
-    dispatch(parsedTracksActions.setParsedTracks(parsedTracks));
-    dispatch(parsedTracksActions.setParsedSegments(parsedSegments));
     dispatch(parsedTracksActions.addParsedConstructionSegments(parsedConstructionSegments ?? []));
 };
 
