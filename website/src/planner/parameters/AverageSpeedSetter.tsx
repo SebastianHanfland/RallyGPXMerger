14a307f2ef03a8fb2410353086bbe3ebd4297d88
sebastian <sebastian.hanfland@rwth-aachen.de>
2025-10-18 10:29:24 +0200

    #239: extract component from average speed setting

--- website/src/planner/parameters/AverageSpeedSetter.tsx
+++ website/src/planner/parameters/AverageSpeedSetter.tsx
@@ -4,8 +4,29 @@ import { Form } from 'react-bootstrap';
 import { FormattedMessage } from 'react-intl';
 import { debounceConstructionOfTracks } from '../logic/automaticCalculation.ts';
 
-export function AverageSpeedSetter({ slim }: { slim?: boolean }) {
+function AverageSpeedRangeInput() {
     const dispatch = useDispatch();
+    const averageSpeed = useSelector(getAverageSpeedInKmH);
+
+    return (
+        <div className={'d-flex'}>
+            <span className={'mx-4'}>3&nbsp;km/h</span>
+            <Form.Range
+                min={3}
+                max={20}
+                step={0.1}
+                value={averageSpeed}
+                onChange={(event) => {
+                    dispatch(trackMergeActions.setAverageSpeed(Number(event.target.value)));
+                    debounceConstructionOfTracks(dispatch);
+                }}
+            />
+            <span className={'mx-4'}>20&nbsp;km/h</span>
+        </div>
+    );
+}
+
+export function AverageSpeedSetter({ slim }: { slim?: boolean }) {
     const averageSpeed = useSelector(getAverageSpeedInKmH);
 
     if (slim) {
@@ -17,20 +38,7 @@ export function AverageSpeedSetter({ slim }: { slim?: boolean }) {
                         <span className={'bg-info p-1'}>{averageSpeed.toFixed(1) + '\xa0km/h'}</span>
                     </h6>
                 </div>
-                <div className={'d-flex'}>
-                    <span className={'mx-4'}>3&nbsp;km/h</span>
-                    <Form.Range
-                        min={3}
-                        max={20}
-                        step={0.1}
-                        value={averageSpeed}
-                        onChange={(event) => {
-                            dispatch(trackMergeActions.setAverageSpeed(Number(event.target.value)));
-                            debounceConstructionOfTracks(dispatch);
-                        }}
-                    />
-                    <span className={'mx-4'}>20&nbsp;km/h</span>
-                </div>
+                <AverageSpeedRangeInput />
             </div>
         );
     }
@@ -40,20 +48,7 @@ export function AverageSpeedSetter({ slim }: { slim?: boolean }) {
             <h5 className="form-label m-3">
                 <FormattedMessage id={'msg.averageSpeed.title'} />
             </h5>
-            <div className={'d-flex'}>
-                <span className={'mx-4'}>3&nbsp;km/h</span>
-                <Form.Range
-                    min={3}
-                    max={20}
-                    step={0.1}
-                    value={averageSpeed}
-                    onChange={(event) => {
-                        dispatch(trackMergeActions.setAverageSpeed(Number(event.target.value)));
-                        debounceConstructionOfTracks(dispatch);
-                    }}
-                />
-                <span className={'mx-4'}>20&nbsp;km/h</span>
-            </div>
+            <AverageSpeedRangeInput />
             <h6 className="form-label m-3">
                 <FormattedMessage id={'msg.averageSpeed'} />
                 {': '}
