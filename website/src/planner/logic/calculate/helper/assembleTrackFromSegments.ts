8079813192901d664b1b49c86d51af275d88e57b
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-02-15 12:29:34 +0100

    #245: remove weird access to store which retriggered the calculation all the time

--- website/src/planner/logic/calculate/helper/assembleTrackFromSegments.ts
+++ website/src/planner/logic/calculate/helper/assembleTrackFromSegments.ts
@@ -1,45 +1,7 @@
-import { ParsedGpxSegment, ParsedPoint, PointOfInterestType, TrackComposition } from '../../../store/types.ts';
+import { ParsedGpxSegment, ParsedPoint, TrackComposition } from '../../../store/types.ts';
 import { instanceOfBreak } from '../../merge/types.ts';
 import { resolveGpxSegments } from './solvingHelper.ts';
 import { CalculatedTrack2 } from '../../../../common/types.ts';
-import geoDistance from 'geo-distance-helper';
-import { planningStore } from '../../../store/planningStore.ts';
-import { pointsActions } from '../../../store/points.reducer.ts';
-import { v4 as uuidv4 } from 'uuid';
-import { getLatLng } from '../../../../utils/pointUtil.ts';
-import { formatNumber } from '../../../../utils/numberUtil.ts';
-import { DEFAULT_GAP_TOLERANCE, getGapToleranceInKm } from '../../../store/trackMerge.reducer.ts';
-
-function checkForGap(
-    lastPoint: ParsedPoint,
-    segment: ParsedGpxSegment,
-    track: TrackComposition,
-    lastSegmentName: string
-) {
-    const endOfNextSegment = segment.points[segment.points.length - 1];
-    const distanceBetweenSegments = geoDistance(getLatLng(lastPoint), getLatLng(endOfNextSegment)) as number;
-    const gapToleranceInKm = getGapToleranceInKm(planningStore.getState()) ?? DEFAULT_GAP_TOLERANCE;
-    if (distanceBetweenSegments > gapToleranceInKm) {
-        const message = `Here is something too far away: track ${track.name}, between '${
-            segment.filename
-        }' and '${lastSegmentName}: distance is ${formatNumber(distanceBetweenSegments, 2)} km'`;
-        planningStore.dispatch(
-            pointsActions.addGapPoint({
-                id: uuidv4(),
-                type: PointOfInterestType.GAP,
-                radiusInM: (distanceBetweenSegments * 1000) / 2,
-                description: message,
-                title: `Too big gap on ${track.name}`,
-                lat: (lastPoint.b + endOfNextSegment.b) / 2,
-                lng: (lastPoint.l + endOfNextSegment.l) / 2,
-            })
-        );
-    }
-}
-
-function clearAllGaps() {
-    planningStore.dispatch(pointsActions.removeAllGapPoint());
-}
 
 function getPointsEndingAtTime(gpxOrBreak: ParsedGpxSegment, endTime: number): ParsedPoint[] {
     const points = gpxOrBreak.points;
@@ -51,11 +13,9 @@ function getPointsEndingAtTime(gpxOrBreak: ParsedGpxSegment, endTime: number): P
 export function assembleTrackFromSegments(track: TrackComposition, gpxSegments: ParsedGpxSegment[]): CalculatedTrack2 {
     let arrivalTimeForPreviousSegment = track.delayAtEndInSeconds ?? 0;
     let trackPoints: ParsedPoint[] = [];
-    clearAllGaps();
 
     const gpxSegmentContents = resolveGpxSegments(track, gpxSegments);
-    let lastPoint: ParsedPoint | undefined = undefined;
-    let lastSegmentName: string = '';
+
     gpxSegmentContents
         .reverse()
         .filter((entry) => entry !== undefined)
@@ -63,16 +23,9 @@ export function assembleTrackFromSegments(track: TrackComposition, gpxSegments: 
             if (instanceOfBreak(gpxOrBreak)) {
                 arrivalTimeForPreviousSegment = arrivalTimeForPreviousSegment - gpxOrBreak.minutes * 60;
             } else {
-                if (!!lastPoint) {
-                    checkForGap(lastPoint, gpxOrBreak, track, lastSegmentName);
-                }
-
                 const shiftedPoint = getPointsEndingAtTime(gpxOrBreak, arrivalTimeForPreviousSegment);
                 arrivalTimeForPreviousSegment = shiftedPoint[0].t;
                 trackPoints = [...shiftedPoint, ...trackPoints];
-
-                lastPoint = shiftedPoint[0];
-                lastSegmentName = gpxOrBreak.filename;
             }
         });
 
