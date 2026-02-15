6040e8abfd4cee00283e4b8abbc06efd9d94644e
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-02-15 15:27:53 +0100

    #243: fix compiler errors

--- website/src/planner/logic/resolving/selectors/getEnrichedTrackStreetInfos.ts
+++ website/src/planner/logic/resolving/selectors/getEnrichedTrackStreetInfos.ts
@@ -1,9 +1,6 @@
 import {
     getDistrictReplacementWayPoints,
-    getResolvedDistricts,
-    getResolvedPostCodes,
     getStreetNameReplacementWayPoints,
-    getTrackStreetInfos,
 } from '../../../store/geoCoding.reducer.ts';
 import { TrackStreetInfo, TrackWayPointType } from '../types.ts';
 import { createSelector } from '@reduxjs/toolkit';
@@ -11,18 +8,13 @@ import { getWayPointKey } from '../helper/pointKeys.ts';
 import { overwriteWayPoints } from '../aggregate/overwriteWayPoints.ts';
 
 export const getEnrichedTrackStreetInfos = createSelector(
-    getTrackStreetInfos,
-    getResolvedPostCodes,
-    getResolvedDistricts,
     getDistrictReplacementWayPoints,
     getStreetNameReplacementWayPoints,
-    (
-        trackStreetInfos,
-        resolvedPostCodes,
-        resolvedDistricts,
-        districtReplacements,
-        streetReplacements
-    ): TrackStreetInfo[] => {
+    (districtReplacements, streetReplacements): TrackStreetInfo[] => {
+        const trackStreetInfos: TrackStreetInfo[] = [];
+        const resolvedPostCodes = null;
+        const resolvedDistricts = null;
+
         const overwrites: Record<string, string> = {};
         districtReplacements?.forEach((replacement) => {
             overwrites[getWayPointKey(replacement).postCodeKey] = replacement.district;
