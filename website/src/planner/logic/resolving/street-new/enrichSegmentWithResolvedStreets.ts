a0aeb8e8773ace500acf17e6b59c8beb836ca5e9
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-02-16 09:15:05 +0100

    #250: have segments better split, they always have new numbers if they are not connected

--- website/src/planner/logic/resolving/street-new/enrichSegmentWithResolvedStreets.ts
+++ website/src/planner/logic/resolving/street-new/enrichSegmentWithResolvedStreets.ts
@@ -1,6 +1,14 @@
 import { toKey } from '../helper/pointKeys.ts';
 import { ParsedGpxSegment, ParsedPoint, ResolvedPositions } from '../../../store/types.ts';
 
+function getHighestFound(streetLookUp: Record<number, string>, resolvedStreetName: string): [string, string] | null {
+    const entriesWithSameStreetName = Object.entries(streetLookUp).filter((entry) => entry[1] === resolvedStreetName);
+    if (entriesWithSameStreetName.length === 0) {
+        return null;
+    }
+    return entriesWithSameStreetName[entriesWithSameStreetName.length - 1];
+}
+
 export function enrichSegmentWithResolvedStreets(
     segmentWithoutStreets: ParsedGpxSegment,
     allResolvedStreetNames: ResolvedPositions,
@@ -17,10 +25,18 @@ export function enrichSegmentWithResolvedStreets(
             points.push(point);
             return;
         }
-        const foundInLookup = Object.entries(streetLookUp).find((entry) => entry[1] === resolvedStreetName);
+        const foundInLookup = getHighestFound(streetLookUp, resolvedStreetName);
+
         if (foundInLookup) {
-            points.push({ ...point, s: Number(foundInLookup[0]) });
-            return;
+            if (points.length > 0 && points[points.length - 1].s === Number(foundInLookup[0])) {
+                points.push({ ...point, s: Number(foundInLookup[0]) });
+                return;
+            } else {
+                indexCounter += 1;
+                streetLookUp[indexCounter] = resolvedStreetName;
+                points.push({ ...point, s: indexCounter });
+                return;
+            }
         } else {
             indexCounter += 1;
             streetLookUp[indexCounter] = resolvedStreetName;
