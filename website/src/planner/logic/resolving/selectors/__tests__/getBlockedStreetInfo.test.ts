50c11475fdb4ed75c4cfff9a843224988d19e927
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-02-25 05:54:16 +0100

    #72: adjust test

--- website/src/planner/logic/resolving/selectors/__tests__/getBlockedStreetInfo.test.ts
+++ website/src/planner/logic/resolving/selectors/__tests__/getBlockedStreetInfo.test.ts
@@ -1,7 +1,7 @@
 import { getBlockedStreetInfo } from '../getBlockedStreetInfo.ts';
 import { Mock } from 'vitest';
-import { BlockedStreetInfo, TrackStreetInfo } from '../../types.ts';
-import { State } from '../../../../store/types.ts';
+import { BlockedStreetInfo, TrackStreetInfo, TrackWayPointType } from '../../types.ts';
+import { State, TrackComposition } from '../../../../store/types.ts';
 import { getTrackStreetInfos } from '../../../../calculation/getTrackStreetInfos.ts';
 
 vi.mock('../../../../calculation/getTrackStreetInfos.ts');
@@ -10,34 +10,72 @@ describe('getBlockedStreetInfo', () => {
     it('should work with empty lists', () => {
         // given
         const trackStreetInfos: TrackStreetInfo[] = [];
+        const trackCompositions: TrackComposition[] = [];
         (getTrackStreetInfos as unknown as Mock).mockReturnValue(trackStreetInfos);
 
         // when
-        const blockedStreetInfo = getBlockedStreetInfo({} as State);
+        const blockedStreetInfo = getBlockedStreetInfo({
+            trackMerge: { trackCompositions: trackCompositions },
+        } as State);
 
         // then
         expect(blockedStreetInfo).toEqual([]);
     });
 
-    it('should take earliest from and latest to date for a street with the same name', () => {
+    it('should take earliest from and latest to date for a street with the same name and not double count people', () => {
         // given
         const earliestStart = '2022-02-02T02:00:00.000Z';
         const latestEnd = '2022-02-02T02:20:00.000Z';
         const trackStreetInfos: TrackStreetInfo[] = [
             {
-                wayPoints: [{ streetName: 'A', frontArrival: '2022-02-02T02:10:00.000Z', backPassage: latestEnd }],
+                id: '1',
+                wayPoints: [
+                    {
+                        streetName: 'A',
+                        frontArrival: '2022-02-02T02:10:00.000Z',
+                        backPassage: '2022-02-02T02:12:00.000Z',
+                        type: TrackWayPointType.Track,
+                    },
+                    {
+                        streetName: 'A',
+                        frontArrival: '2022-02-02T02:14:00.000Z',
+                        backPassage: latestEnd,
+                        type: TrackWayPointType.Track,
+                    },
+                ],
             },
             {
-                wayPoints: [{ streetName: 'A', frontArrival: earliestStart, backPassage: '2022-02-02T02:10:00.000Z' }],
+                id: '2',
+                wayPoints: [
+                    {
+                        streetName: 'A',
+                        frontArrival: earliestStart,
+                        backPassage: '2022-02-02T02:10:00.000Z',
+                        type: TrackWayPointType.Track,
+                    },
+                ],
             },
         ] as TrackStreetInfo[];
+        const trackCompositions: TrackComposition[] = [
+            { id: '1', peopleCount: 10 } as TrackComposition,
+            { id: '2', peopleCount: 20 } as TrackComposition,
+        ];
+
         (getTrackStreetInfos as unknown as Mock).mockReturnValue(trackStreetInfos);
         const expectedBlockedStreets: BlockedStreetInfo[] = [
-            { streetName: 'A', frontArrival: earliestStart, backPassage: latestEnd } as BlockedStreetInfo,
+            {
+                streetName: 'A',
+                frontArrival: earliestStart,
+                backPassage: latestEnd,
+                peopleCount: 30,
+                tracksIds: ['1', '2'],
+            } as BlockedStreetInfo,
         ];
 
         // when
-        const blockedStreetInfo = getBlockedStreetInfo({} as State);
+        const blockedStreetInfo = getBlockedStreetInfo({
+            trackMerge: { trackCompositions: trackCompositions },
+        } as State);
 
         // then
         expect(blockedStreetInfo).toEqual(expectedBlockedStreets);
