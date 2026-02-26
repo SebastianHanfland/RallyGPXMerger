c32f41b4da21e8a2273ec835e377e445023cd02d
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-02-26 05:38:21 +0100

    #130: allow to set any value for a break

--- website/src/planner/map/BreakDialogContent.tsx
+++ website/src/planner/map/BreakDialogContent.tsx
@@ -1,35 +1,19 @@
 import { Form } from 'react-bootstrap';
 import { TrackBreak } from '../store/types.ts';
 import wc from '../../assets/wc.svg';
-import Select, { SingleValue } from 'react-select';
 import { FormattedMessage, useIntl } from 'react-intl';
+import { getCount } from '../../utils/inputUtil.ts';
+import { ChangeEvent } from 'react';
 
 interface Props {
     values: Partial<TrackBreak>;
     setValues: (point: Partial<TrackBreak>) => void;
 }
 
-const breaks = [
-    { value: 1, label: '+ 1 min' },
-    { value: 2, label: '+ 2 min' },
-    { value: 5, label: '+ 5 min' },
-    { value: 10, label: '+ 10 min' },
-    { value: 15, label: '+ 15 min' },
-    { value: 20, label: '+ 20 min' },
-    { value: 25, label: '+ 25 min' },
-    { value: 30, label: '+ 30 min' },
-    { value: -1, label: '- 1 min' },
-    { value: -2, label: '- 2 min' },
-    { value: -3, label: '- 3 min' },
-    { value: -4, label: '- 4 min' },
-    { value: -5, label: '- 5 min' },
-    { value: -10, label: '- 10 min' },
-];
-
 export const BreakDialogContent = ({ values, setValues }: Props) => {
     const intl = useIntl();
-    const addSegmentToTrack = (newValue: SingleValue<{ value: number }>) => {
-        setValues({ ...values, minutes: newValue?.value });
+    const addSegmentToTrack = (event: ChangeEvent<HTMLInputElement>) => {
+        setValues({ ...values, minutes: getCount(event) });
     };
     return (
         <div>
@@ -37,13 +21,11 @@ export const BreakDialogContent = ({ values, setValues }: Props) => {
                 <Form.Label>
                     <FormattedMessage id={'msg.minutes'} />
                 </Form.Label>
-                <Select
-                    name="segmentSelect"
-                    value={breaks.find((option) => option.value === values.minutes)}
-                    placeholder={intl.formatMessage({ id: 'msg.minutes.details' })}
-                    options={breaks}
-                    className="basic-multi-select"
-                    classNamePrefix="select"
+                <Form.Control
+                    type="number"
+                    step={1}
+                    title={intl.formatMessage({ id: 'msg.minutes.details' })}
+                    value={values.minutes ?? 0}
                     onChange={addSegmentToTrack}
                 />
             </Form.Group>
@@ -61,19 +43,20 @@ export const BreakDialogContent = ({ values, setValues }: Props) => {
                 />
             </Form.Group>
             <Form.Group>
-                <Form.Label>
-                    <FormattedMessage id={'msg.toiletAtPause'} />
-                </Form.Label>
+                <Form.Label>{<FormattedMessage id={'msg.toilets'} />}</Form.Label>
                 <Form.Check
                     type={'checkbox'}
                     label={
                         <span>
-                            <img className={'mx-1'} src={wc} alt={'wc'} />
+                            <span>
+                                <img className={'mx-1'} src={wc} alt={'wc'} />
+                            </span>
+                            <FormattedMessage id={'msg.toiletAtPause'} />
                         </span>
                     }
                     placeholder={intl.formatMessage({ id: 'msg.toiletAtPause' })}
-                    checked={values.hasToilet}
-                    onClick={() => setValues({ ...values, hasToilet: !values.hasToilet })}
+                    checked={values.hasToilet ?? false}
+                    onChange={() => setValues({ ...values, hasToilet: !values.hasToilet })}
                 ></Form.Check>
             </Form.Group>
         </div>
