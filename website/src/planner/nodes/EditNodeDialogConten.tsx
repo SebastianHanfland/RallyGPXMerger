89e167421fb22d82bcf50d4053af3c4d48203249
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-03-10 00:38:32 +0100

    #286: extract content into own component

---
+++ website/src/planner/nodes/EditNodeDialogConten.tsx
@@ -0,0 +1,164 @@
+import { useSelector } from 'react-redux';
+import { FormattedMessage, useIntl } from 'react-intl';
+import Button from 'react-bootstrap/Button';
+import { getTrackCompositions } from '../store/trackMerge.reducer.ts';
+import { getNodeEditInfo } from '../store/nodes.reducer.ts';
+import { Form, ProgressBar } from 'react-bootstrap';
+import { useEffect, useState } from 'react';
+import { listAllNodesOfTracks } from '../../common/calculation/nodes/nodeFinder.ts';
+import { getBranchesAtNode, getBranchTracks } from './getBranchesAtNode.ts';
+import { getColor } from '../../utils/colorUtil.ts';
+import { NodeSpecification } from '../store/types.ts';
+import { getCount } from '../../utils/inputUtil.ts';
+import {
+    getBranchId,
+    getBranchNumbersSelector,
+} from '../../common/calculation/calculated-tracks/nodeSpecResultingBranchSize.ts';
+import { EditNodeDialogBranchTitle } from './EditNodeDialogBranchTitle.tsx';
+
+function getNodeDelayValue(numberValue: number, branchParticipants: number, total: number) {
+    const maximum = (total ?? 0) - branchParticipants;
+    if (numberValue > maximum) {
+        return maximum;
+    }
+    if (numberValue < 0) {
+        return 0;
+    }
+    return numberValue;
+}
+
+export const EditNodeDialogContent = () => {
+    const intl = useIntl();
+    const nodeEditInfo = useSelector(getNodeEditInfo);
+    const trackCompositions = useSelector(getTrackCompositions);
+    const branchesAtNode = useSelector(getBranchesAtNode);
+    const branchNumbers = useSelector(getBranchNumbersSelector);
+
+    const [nodeSpecs, setNodeSpecs] = useState<NodeSpecification | undefined>(undefined);
+    const direction = intl.formatMessage({ id: 'msg.direction' });
+
+    const trackNodes = listAllNodesOfTracks(trackCompositions);
+
+    const foundTrackNode = trackNodes.find((node) => node.segmentIdAfterNode === nodeEditInfo?.segmentAfterId);
+
+    const aggregated = false;
+
+    useEffect(() => {
+        if (branchesAtNode) {
+            setNodeSpecs({ totalCount: branchesAtNode.totalCount, trackOffsets: branchesAtNode?.trackOffsets });
+        } else {
+            setNodeSpecs(undefined);
+        }
+    }, [branchesAtNode]);
+
+    if (!nodeEditInfo || !foundTrackNode || !branchesAtNode || !nodeSpecs || !nodeEditInfo.segmentAfterId) {
+        return null;
+    }
+    const branchTracks = getBranchTracks(nodeEditInfo.segmentAfterId, trackCompositions);
+    if (!branchTracks) {
+        return null;
+    }
+
+    let total = 0;
+    Object.values(branchTracks).forEach((bTracks) => {
+        total += branchNumbers[getBranchId(bTracks.map(({ id }) => id))];
+    });
+    console.log({ total });
+
+    return (
+        <>
+            <div>{`<= ${direction}`}</div>
+            {Object.entries(branchTracks).map(([segmentId, tracks]) => {
+                const shiftOffset = (offSet: number) => () => {
+                    const newValue = getNodeDelayValue(
+                        (nodeSpecs?.trackOffsets[segmentId] ?? 0) + offSet,
+                        branchNumbers[getBranchId(tracks.map(({ id }) => id))],
+                        total
+                    );
+                    setNodeSpecs({
+                        ...nodeSpecs,
+                        trackOffsets: { ...nodeSpecs?.trackOffsets, [segmentId]: newValue },
+                    });
+                };
+                return (
+                    <>
+                        <EditNodeDialogBranchTitle
+                            segmentId={segmentId}
+                            tracks={tracks}
+                            peopleOffset={nodeSpecs.trackOffsets[segmentId]}
+                        />
+                        <div
+                            key={segmentId + '2'}
+                            style={{ display: 'flex', justifyContent: 'row', alignItems: 'flex-end' }}
+                        >
+                            <div key={segmentId + '3'}>
+                                <Button size={'sm'} onClick={shiftOffset(-100000000)}>
+                                    {'<-'}
+                                </Button>
+                            </div>
+                            <ProgressBar key={segmentId} className={'flex-fill mx-2'} style={{ height: '30px' }}>
+                                <ProgressBar
+                                    now={(nodeSpecs.trackOffsets[segmentId] / total) * 100}
+                                    variant={'gray'}
+                                    className={'bg-transparent'}
+                                    style={{ height: '20px' }}
+                                    visuallyHidden
+                                    key={0}
+                                />
+                                {aggregated && (
+                                    <ProgressBar
+                                        now={
+                                            ((branchNumbers[getBranchId(tracks.map(({ id }) => id))] ?? 0) / total) *
+                                            100
+                                        }
+                                        style={{ cursor: 'pointer', background: getColor({ id: segmentId }) }}
+                                    />
+                                )}
+
+                                {!aggregated &&
+                                    tracks.map((track) => (
+                                        <ProgressBar
+                                            now={((track.peopleCount ?? 0) / total) * 100}
+                                            title={`${track.name}: ${track.peopleCount ?? 0} ${intl.formatMessage({
+                                                id: 'msg.trackPeople',
+                                            })}`}
+                                            key={track.id}
+                                            style={{ cursor: 'pointer', background: getColor(track) }}
+                                        />
+                                    ))}
+                            </ProgressBar>
+                            <div key={segmentId + '4'}>
+                                <Button size={'sm'} onClick={shiftOffset(100000000)}>
+                                    {'->'}
+                                </Button>
+                            </div>
+                            <div className={'mx-2'}>
+                                <Form.Group>
+                                    <Form.Label>
+                                        <FormattedMessage id={'msg.nodeOffset'} />
+                                    </Form.Label>
+                                    <Form.Control
+                                        type="text"
+                                        placeholder={intl.formatMessage({ id: 'msg.trackPeople' })}
+                                        value={nodeSpecs.trackOffsets[segmentId]}
+                                        onChange={(value) => {
+                                            const newValue = getNodeDelayValue(
+                                                getCount(value) ?? 0,
+                                                branchNumbers[getBranchId(tracks.map(({ id }) => id))],
+                                                total
+                                            );
+                                            setNodeSpecs({
+                                                ...nodeSpecs,
+                                                trackOffsets: { ...nodeSpecs?.trackOffsets, [segmentId]: newValue },
+                                            });
+                                        }}
+                                    />
+                                </Form.Group>
+                            </div>
+                        </div>
+                    </>
+                );
+            })}
+        </>
+    );
+};
