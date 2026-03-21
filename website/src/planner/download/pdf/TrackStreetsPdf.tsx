2532cce6f6d43ca0a79e5d771de38e926ea7156c
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-03-21 13:28:27 +0100

    #274: remove unused elements

--- website/src/planner/download/pdf/TrackStreetsPdf.tsx
+++ website/src/planner/download/pdf/TrackStreetsPdf.tsx
@@ -1,21 +1,12 @@
 import { Document, Page, StyleSheet, Text, View } from '@react-pdf/renderer';
 import { IntlShape } from 'react-intl';
 import { TrackStreetInfo } from '../../logic/resolving/types.ts';
-import { TDocumentDefinitions } from 'pdfmake/interfaces';
-import { createInfoTable } from './infoTablePdf.ts';
-import { createEntryPointOverviewTable } from './entryPointOverviewPdf.ts';
-import { createBreakOverviewTable } from './breakOverviewPdf.ts';
-import { createNodeOverviewTable } from './nodeOverviewTablePdf.ts';
-import { createStreetTable } from './trackStreetTablePdf.ts';
-import { createPdf } from 'pdfmake/build/pdfmake';
-import { styles as stylesOld } from './pdfUtil.ts';
 import { InfoTablePdf } from './InfoTablePdf.tsx';
 import { EntryPointOverviewPdf } from './EntryPointOverviewPdf.tsx';
 import { BreakOverviewPdf } from './BreakOverviewPdf.tsx';
 import { NodeOverviewTablePdf } from './NodeOverviewTablePdf.tsx';
 import { TrackStreetTablePdf } from './TrackStreetTablePdf.tsx';
 
-// Create styles
 const styles = StyleSheet.create({
     page: {
         flexDirection: 'column',
@@ -28,20 +19,6 @@ const styles = StyleSheet.create({
         flexGrow: 1,
     },
 });
-
-// Create Document Component
-export const MyDocument = () => (
-    <Document>
-        <Page size="A4" style={styles.page} orientation={'landscape'}>
-            <View style={styles.section}>
-                <Text>Section #1</Text>
-            </View>
-            <View style={styles.section}>
-                <Text>Section #2</Text>
-            </View>
-        </Page>
-    </Document>
-);
 
 interface Props {
     intl: IntlShape;
@@ -72,36 +49,3 @@ export const TrackStreetsPdf = ({ trackStreets, intl, planningLabel }: Props) =>
         </Page>
     </Document>
 );
-
-export const createTrackStreetPdf = (intl: IntlShape, planningLabel?: string) => (trackStreets: TrackStreetInfo) => {
-    const docDefinition: TDocumentDefinitions = {
-        pageOrientation: 'landscape',
-        content: [
-            '',
-            { text: trackStreets.name.replaceAll('.gpx', ''), style: 'titleStyle' },
-            '\n\n',
-            planningLabel ? `${planningLabel}` : ' ',
-            ' ',
-            { text: intl.formatMessage({ id: 'msg.trackInfo' }), style: 'titleStyle' },
-            ' ',
-            createInfoTable(trackStreets, intl),
-            ' ',
-            ' ',
-            ...createEntryPointOverviewTable(trackStreets, intl),
-            ' ',
-            ' ',
-            ...createBreakOverviewTable(trackStreets, intl),
-            // DONE
-            ' ',
-            ' ',
-            ...createNodeOverviewTable(trackStreets, intl),
-            ' ',
-            ' ',
-            { text: intl.formatMessage({ id: 'msg.streetOverview' }), style: 'titleStyle' },
-            ' ',
-            createStreetTable(trackStreets, intl),
-        ],
-        styles: stylesOld,
-    };
-    return createPdf(docDefinition);
-};
