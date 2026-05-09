a1bb9891a3e9684d0c798e1fd6eb9c37bb606ee2
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-05-09 18:08:02 +0200

    #325: take speed of replaced segment into account

--- website/src/planner/segments/FileChangeWithUploadButton.tsx
+++ website/src/planner/segments/FileChangeWithUploadButton.tsx
@@ -8,7 +8,7 @@ import { executeGpxSegmentReplacementWithUpload } from './fileReplaceWithUploadT
 import { AppDispatch } from '../store/planningStore.ts';
 import { ReplaceFileDisplay } from './ReplaceFileDisplay.tsx';
 import { FormattedMessage, useIntl } from 'react-intl';
-import { getReplaceProcess, segmentDataActions } from '../store/segmentData.redux.ts';
+import { getReplaceProcess, getSegmentSpeeds, segmentDataActions } from '../store/segmentData.redux.ts';
 import { toParsedGpxSegment } from './segmentParsing.ts';
 import { enrichGpxSegmentsWithStreetNames } from '../logic/resolving/streets/mapMatchingStreetResolver.ts';
 import { getAverageSpeedInKmH } from '../store/settings.reducer.ts';
@@ -24,6 +24,8 @@ export function FileChangeWithUploadButton({ id, name }: Props) {
     const [showModal, setShowModal] = useState(false);
     const replaceProcess = useSelector(getReplaceProcess);
     const averageSpeed = useSelector(getAverageSpeedInKmH);
+    const segmentSpeeds = useSelector(getSegmentSpeeds);
+    const segmentSpeed = replaceProcess ? (segmentSpeeds[replaceProcess!.targetSegment] ?? averageSpeed) : averageSpeed;
 
     const [isLoading, setIsLoading] = useState(false);
 
@@ -39,7 +41,7 @@ export function FileChangeWithUploadButton({ id, name }: Props) {
     };
 
     const handleChange = (newFiles: File | File[]) => {
-        Promise.all([...(newFiles as File[])].map((file) => toParsedGpxSegment(file, averageSpeed))).then(
+        Promise.all([...(newFiles as File[])].map((file) => toParsedGpxSegment(file, segmentSpeed))).then(
             (replacementSegments) => {
                 dispatch(enrichGpxSegmentsWithStreetNames(replacementSegments)).then(() =>
                     dispatch(enrichGpxSegmentsWithPostCodesAndDistricts)
