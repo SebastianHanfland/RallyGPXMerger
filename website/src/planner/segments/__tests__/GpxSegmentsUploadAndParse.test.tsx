f1d1e8a4e63a1e37a0500fb1aaac98dcde9c83ac
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-07-19 12:59:45 +0200

    #339: trying to get the log

--- website/src/planner/segments/__tests__/GpxSegmentsUploadAndParse.test.tsx
+++ website/src/planner/segments/__tests__/GpxSegmentsUploadAndParse.test.tsx
@@ -34,6 +34,7 @@ describe('GpxUpload and parse', () => {
 
         await ui.uploadGpxSegment('segment1');
 
+        console.log({ seg1: getParsedGpxSegments(store.getState()) });
         await waitFor(() => expect(getParsedGpxSegments(store.getState())).toHaveLength(1), { timeout: 2000 });
         const points = getParsedGpxSegments(store.getState())[0].points;
         console.log({ points, seg: getParsedGpxSegments(store.getState()) });
