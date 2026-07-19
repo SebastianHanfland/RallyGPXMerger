2971ee4116e77d6e234bb8e8f51e17b91913520a
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-07-19 12:05:42 +0200

    #339: remove log

--- website/src/migrate/types.ts
+++ website/src/migrate/types.ts
@@ -4,6 +4,5 @@ import { StateOld } from '../planner/store/typesOld.ts';
 export type StateVersion1 = StateOld;
 export type StateVersion2 = State;
 export const isOldState = (state: State | StateOld): state is StateOld => {
-    console.log('Check for old state', state);
     return (state as StateOld).gpxSegments !== undefined;
 };
