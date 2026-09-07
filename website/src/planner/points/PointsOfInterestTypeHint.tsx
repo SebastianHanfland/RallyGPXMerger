03aef0da4efceb4b21e1a1c118a8c1a4a2605bfd
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-09-07 17:05:09 +0200

    #346: remove gaps from the street name check

---
+++ website/src/planner/points/PointsOfInterestTypeHint.tsx
@@ -0,0 +1,30 @@
+import { FormattedMessage } from 'react-intl';
+import { getLanguage } from '../../language.ts';
+import { useSelector } from 'react-redux';
+import hand from '../../assets/hand.svg';
+import { getParsedGpxSegments } from '../store/segmentData.redux.ts';
+import { WarningIcon } from '../../utils/icons/WarningIcon.tsx';
+
+export function GpxCreationHint() {
+    const language = getLanguage();
+    const link = language === 'de' ? 'https://gpx.studio/de' : 'https://gpx.studio/';
+    const hasNoGPXSegments = useSelector(getParsedGpxSegments).length === 0;
+
+    return (
+        <p style={hasNoGPXSegments ? { border: '2px solid transparent', borderColor: 'red' } : undefined}>
+            {hasNoGPXSegments && <WarningIcon />}
+            <FormattedMessage id={'msg.createGpx.part1'} />{' '}
+            <a href={'http://brouter.de/brouter-web'} target={'_blank'} referrerPolicy={'no-referrer'}>
+                <img src={hand} className="m-1" alt="warning" style={{ width: `${15}px`, height: `${15}px` }} />
+                brouter
+            </a>
+            <span>
+                <FormattedMessage id={'msg.createGpx.part2'} />
+            </span>
+            <a href={link} target={'_blank'} referrerPolicy={'no-referrer'}>
+                <img src={hand} className="m-1" alt="warning" style={{ width: `${15}px`, height: `${15}px` }} />
+                Gpx Studio
+            </a>
+        </p>
+    );
+}
