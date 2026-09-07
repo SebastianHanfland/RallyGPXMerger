730b352c7922804290b9f97c8b6483d3c1e1cece
sebastian <sebastian.hanfland@rwth-aachen.de>
2026-09-07 18:23:47 +0200

    #346: Have the general appearance right

--- website/src/planner/points/PointsOfInterestHeader.tsx
+++ website/src/planner/points/PointsOfInterestHeader.tsx
@@ -9,15 +9,22 @@ export function PointsOfInterestHeader() {
     const points = useSelector(getPoints) ?? [];
     const intl = useIntl();
     const pointsLabel = intl.formatMessage({ id: 'msg.points' });
+    const pointsOnPublicMapLabel = intl.formatMessage({ id: 'msg.pointsOnPublicMap' });
 
     const problemPoints = points.filter((point) =>
         [PointOfInterestType.IMPEDIMENT, PointOfInterestType.TODO].includes(point.type)
+    );
+    const publicPoints = points.filter((point) =>
+        [PointOfInterestType.PUBLIC_COMMENT, PointOfInterestType.GATHERING, PointOfInterestType.TOILET].includes(
+            point.type
+        )
     );
     if (problemPoints.length === 0) {
         return (
             <div>
                 <CheckIcon />
                 <FormattedMessage id={'msg.noOpenPoints'} /> {` (${points.length} ${pointsLabel})`}
+                {` (${publicPoints.length} ${pointsOnPublicMapLabel})`}
             </div>
         );
     }
@@ -27,6 +34,7 @@ export function PointsOfInterestHeader() {
             <WarningIcon />
             {points.length} <FormattedMessage id={'msg.openPoints'} />
             {` (${points.length} ${pointsLabel})`}
+            {` (${publicPoints.length} ${pointsOnPublicMapLabel})`}
         </div>
     );
 }
